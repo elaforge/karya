0f0daa0321c81eea0e193921d5a3820390d82151
qdunkan <qdunkan@gmail.com>
2008-01-28 00:32:18 +0000

    fix f_util formatting

--- ui/f_util.cc
+++ ui/f_util.cc
@@ -10,81 +10,74 @@
 #include "f_util.h"
 
 
-enum { DAMAGE_ZOOM = FL_DAMAGE_USER1 };
+// enum { DAMAGE_ZOOM = FL_DAMAGE_USER1 };
 
 
 char *
 show_event(int ev)
 {
-	static char buf[1024];
-	char *s;
-	switch (ev) {
-	case FL_NO_EVENT: return "nothing";
-	case FL_PUSH: case FL_DRAG: case FL_RELEASE: case FL_MOVE:
-		if (ev==FL_PUSH) s = "push";
-		else if (ev == FL_DRAG) s = "drag";
-		else if (ev == FL_RELEASE) s = "release";
-		else if (ev == FL_MOVE) s = "move";
-		sprintf(buf, "%s: %d (%d %d)", s, Fl::event_button(),
-			Fl::event_x(), Fl::event_y());
-		return buf;
-	case FL_ENTER: return "enter";
-	case FL_LEAVE: return "leave";
-	case FL_FOCUS: return "focus";
-	case FL_UNFOCUS: return "unfocus";
-	
-	case FL_KEYDOWN: case FL_KEYUP: case FL_SHORTCUT:
-		if (ev == FL_KEYDOWN) s = "down";
-		else if (ev == FL_KEYUP) s = "up";
-		else s = "shortcut";
-		sprintf(buf, "key%s: %.*s", s, Fl::event_length(), Fl::event_text());
-		return buf;
-	
-	case FL_DEACTIVATE: return "deactivate";
-	case FL_ACTIVATE: return "activate";
-	case FL_HIDE: return "hide";
-	case FL_SHOW: return "show";
-	default:
-		sprintf(buf, "unknown event: %d", ev);
-		return buf;
-	}
-}
+    static char buf[1024];
+    char *s;
+    switch (ev) {
+    case FL_NO_EVENT: return "nothing";
+    case FL_PUSH: case FL_DRAG: case FL_RELEASE: case FL_MOVE:
+        if (ev==FL_PUSH) s = "push";
+        else if (ev == FL_DRAG) s = "drag";
+        else if (ev == FL_RELEASE) s = "release";
+        else if (ev == FL_MOVE) s = "move";
+        sprintf(buf, "%s: %d (%d %d)", s, Fl::event_button(),
+            Fl::event_x(), Fl::event_y());
+        return buf;
+    case FL_ENTER: return "enter";
+    case FL_LEAVE: return "leave";
+    case FL_FOCUS: return "focus";
+    case FL_UNFOCUS: return "unfocus";
 
+    case FL_KEYDOWN: case FL_KEYUP: case FL_SHORTCUT:
+        if (ev == FL_KEYDOWN) s = "down";
+        else if (ev == FL_KEYUP) s = "up";
+        else s = "shortcut";
+        sprintf(buf, "key%s: %.*s", s, Fl::event_length(), Fl::event_text());
+        return buf;
 
-void
-print_event(int evt)
-{
-    printf("event: %s\n", show_event(evt));
+    case FL_DEACTIVATE: return "deactivate";
+    case FL_ACTIVATE: return "activate";
+    case FL_HIDE: return "hide";
+    case FL_SHOW: return "show";
+    default:
+        sprintf(buf, "unknown event: %d", ev);
+        return buf;
+    }
 }
 
 
 char *
 show_damage(uchar d)
 {
-	static char buf[1024];
-	memset(buf, '\0', sizeof buf);
-	if (d & FL_DAMAGE_ALL)
-		strcat(buf, "all, ");
-	if (d & FL_DAMAGE_CHILD)
-		strcat(buf, "child, ");
-	if (d & FL_DAMAGE_EXPOSE)
-		strcat(buf, "expose, ");
-	if (d & FL_DAMAGE_OVERLAY)
-		strcat(buf, "overlay, ");
-	if (d & DAMAGE_ZOOM)
-		strcat(buf, "zoom, ");
-	buf[strlen(buf)-2] = '\0';
-	return buf;
+    static char buf[1024];
+    memset(buf, '\0', sizeof buf);
+    if (d & FL_DAMAGE_ALL)
+        strcat(buf, "all, ");
+    if (d & FL_DAMAGE_CHILD)
+        strcat(buf, "child, ");
+    if (d & FL_DAMAGE_EXPOSE)
+        strcat(buf, "expose, ");
+    if (d & FL_DAMAGE_OVERLAY)
+        strcat(buf, "overlay, ");
+    // if (d & DAMAGE_ZOOM)
+    //  strcat(buf, "zoom, ");
+    buf[strlen(buf)-2] = '\0';
+    return buf;
 }
 
 
 char *
 show_widget(const Fl_Widget *w)
 {
-	static char buf[64];
-	Rect r = rect(*w);
-	snprintf(buf, sizeof buf, "(%d %d %d %d) %s \"%s\"", r.x, r.y, r.w, r.h,
-		typeid(*w).name(), w->label());
+    static char buf[64];
+    Rect r = rect(*w);
+    snprintf(buf, sizeof buf, "(%d %d %d %d) %s \"%s\"", r.x, r.y, r.w, r.h,
+        typeid(*w).name(), w->label());
     return buf;
 }
 
@@ -92,30 +85,30 @@ show_widget(const Fl_Widget *w)
 void
 print_widget(const Fl_Widget *w)
 {
-		// for some reason c++ style io silently prints nothing
-		// std::cout << "c" << i << ' ' << rect_from_widget(*c)
-		// 	<< typeid(*c).name() << " \"" << c->label() << "\"\n";
-	Rect r = rect(*w);
-	printf("(%d %d %d %d) %s \"%s\"\n", r.x, r.y, r.w, r.h,
-		typeid(*w).name(), w->label());
+        // for some reason c++ style io silently prints nothing
+        // std::cout << "c" << i << ' ' << rect_from_widget(*c)
+        //  << typeid(*c).name() << " \"" << c->label() << "\"\n";
+    Rect r = rect(*w);
+    printf("(%d %d %d %d) %s \"%s\"\n", r.x, r.y, r.w, r.h,
+        typeid(*w).name(), w->label());
 }
 
 
 void
 print_children(const Fl_Widget *w, int nlevels, int recurse)
 {
-	print_widget(w);
-	if (nlevels-- == 0)
-		return;
-	const Fl_Group *g = dynamic_cast<const Fl_Group *>(w);
-	if (g) {
-		for (int i = 0; i < g->children(); i++) {
-			if (!g->child(i)->visible())
-				continue;
-			for (int r = recurse; r; r--)
-				printf("    ");
-			printf("c%d: ", i);
-			print_children(g->child(i), nlevels, recurse+1);
-		}
-	}
+    print_widget(w);
+    if (nlevels-- == 0)
+        return;
+    const Fl_Group *g = dynamic_cast<const Fl_Group *>(w);
+    if (g) {
+        for (int i = 0; i < g->children(); i++) {
+            if (!g->child(i)->visible())
+                continue;
+            for (int r = recurse; r; r--)
+                printf("    ");
+            printf("c%d: ", i);
+            print_children(g->child(i), nlevels, recurse+1);
+        }
+    }
 }
--- ui/f_util.h
+++ ui/f_util.h
@@ -3,31 +3,40 @@
 #ifndef __F_UTIL_H
 #define __F_UTIL_H
 
+#include <FL/Fl.H>
 #include <FL/fl_draw.H>
 #include <FL/Fl_Widget.H>
 
 #include "util.h"
 
 
+// use more convenient types with standard fltk functions
+
 inline Rect
 rect(const Fl_Widget &w) { return Rect(w.x(), w.y(), w.w(), w.h()); }
 inline Rect
 rect(const Fl_Widget *w) { return rect(*w); }
-
-char *show_event(int ev);
-void print_event(int ev);
-
-char *show_damage(uchar d);
-
-char *show_widget(const Fl_Widget *w);
-void print_widget(const Fl_Widget *w);
-
-void print_children(const Fl_Widget *w, int nlevels=-1, int recurse = 0);
 
 inline Fl_Color
 color_to_fl(const Color &c)
 {
     return fl_rgb_color(c.r, c.g, c.b);
 }
+
+inline Point
+mouse_pos()
+{
+    return Point(Fl::event_x(), Fl::event_y());
+}
+
+
+// printing stuff for debugging
+
+char *show_event(int ev);
+char *show_damage(uchar d);
+char *show_widget(const Fl_Widget *w);
+
+void print_widget(const Fl_Widget *w);
+void print_children(const Fl_Widget *w, int nlevels=-1, int recurse = 0);
 
 #endif
