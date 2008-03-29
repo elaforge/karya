1e1def5b36834c4d9b241247d831fca3f246e692
qdunkan <qdunkan@gmail.com>
2008-03-29 22:33:31 +0000

    more logging functions

--- Util/Log.hs
+++ Util/Log.hs
@@ -14,46 +14,17 @@ a given pattern may go to the status bar.
 -}
 
 module Util.Log (
-    debug, notice, warn, error
-    , log, write, Msg
+    Msg, Prio(..), msg, msg_io, log, write
+    , debug, notice, warn, error
     , hex -- debugging, remove me later
 ) where
 import Prelude hiding (error, log)
 import qualified Data.Word as Word
+import qualified Data.Time as Time
 import Text.Printf (printf)
 
-
--- Later have versions that log context, log to a logging monad, etc.
--- TODO how can I automatically get FileContext?
--- TODO system should be dynamically scoped, 'Log.with_system sys (code)'
-log :: Prio -> String -> IO ()
-log prio text = write (msg prio text)
-default_show msg = printf "%s - %s\n" (prio_stars (msg_prio msg)) (msg_text msg)
-
-write :: Msg -> IO ()
-write msg = putStrLn (default_show msg)
-
-debug = log Debug
-notice = log Notice
-warn = log Warn
-error = log Error
-
-msg :: Prio -> String -> Msg
-msg prio text = Msg () prio text []
-
-data Prio = Debug -- ^ Lots of msgs produced by code level.  Users don't look
-        -- at this during normal use, but can be useful for debugging.
-    | Notice -- ^ Informational msgs that the user will want to see.  Progress
-        -- messages in e.g. derivation and play status are included here.
-    | Warn -- ^ Something went wrong in e.g. derivation.  User definitely wants
-        -- to see this.
-    | Error -- ^ Code error in the app, which may quit after printing this.
-    deriving (Show, Enum, Eq, Ord)
-
-prio_stars prio = replicate (fromEnum prio + 1) '*'
-
 data Msg = Msg
-    { msg_date :: () -- Datetime
+    { msg_date :: Maybe Time.UTCTime
     , msg_prio :: Prio
     -- Named system, higher level than just the filename.
     -- , msg_system :: System
@@ -66,6 +37,40 @@ data Msg = Msg
     -- , msg_attrs :: [(String, String)]
     } deriving (Show, Eq)
 
+data Prio = Debug -- ^ Lots of msgs produced by code level.  Users don't look
+        -- at this during normal use, but can be useful for debugging.
+    | Notice -- ^ Informational msgs that the user will want to see.  Progress
+        -- messages in e.g. derivation and play status are included here.
+    | Warn -- ^ Something went wrong in e.g. derivation.  User definitely wants
+        -- to see this.
+    | Error -- ^ Code error in the app, which may quit after printing this.
+    deriving (Show, Enum, Eq, Ord)
+
+msg :: Prio -> String -> Msg
+msg prio text = Msg Nothing prio text []
+msg_io :: Prio -> String -> IO Msg
+msg_io prio text = do
+    utc <- Time.getCurrentTime
+    return ((msg prio text) {msg_date = Just utc})
+
+-- Later have versions that log context, log to a logging monad, etc.
+-- TODO how can I automatically get FileContext?
+-- TODO system should be dynamically scoped, 'Log.with_system sys (code)'
+log :: Prio -> String -> IO ()
+log prio text = write (msg prio text)
+default_show msg = printf "%s - %s" (prio_stars (msg_prio msg)) (msg_text msg)
+
+write :: Msg -> IO ()
+write msg = putStrLn (default_show msg)
+
+debug = log Debug
+notice = log Notice
+warn = log Warn
+error = log Error
+
+prio_stars prio = replicate (fromEnum prio + 1) '*'
+
+{-
 data System
     = App -- ^ app level stuff, goes to stderr
     | Playback
@@ -78,6 +83,7 @@ data FileContext = FileContext
     { file_name :: String
     , file_lineno :: Int
     } deriving (Show, Eq)
+-}
 
 -- | More specific data for the msg.  A derivation would send Progress
 -- reports at Info, an error in derivation would send Block at Warn, and
