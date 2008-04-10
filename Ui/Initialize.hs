8b3fbc25674c18454a46bd0017eca38c80ed0293
qdunkan <qdunkan@gmail.com>
2008-04-10 00:31:41 +0000

    log ui thread exceptions

--- Ui/Initialize.hs
+++ Ui/Initialize.hs
@@ -26,18 +26,16 @@ acts_mvar = unsafePerformIO (MVar.newMVar [])
 -- since some UIs don't work properly unless run from the main thread.
 -- When 'app' exits, the ui loop will be aborted.
 initialize :: (STM.TChan UiMsg.UiMsg -> IO ()) -> IO ()
-initialize app = do
+initialize app = Exception.handle ui_handler $ do
     msg_chan <- STM.newTChanIO
-    Thread.start_os_thread "app" $ app_wrapper acts_mvar (app msg_chan)
-    th_id <- Concurrent.myThreadId
-    MVar.putMVar ui_thread_id th_id
-    poll_loop acts_mvar msg_chan
-    `Exception.finally`
-    (Log.notice "main thread quitting")
+    quit_request <- MVar.newMVar ()
+    Thread.start_os_thread "app" $ Exception.handle app_handler $
+        app msg_chan >> kill_ui_thread quit_request
+    c_initialize
+    poll_loop quit_request acts_mvar msg_chan
 
-app_wrapper acts_mvar app = do
-    Exception.catch app (\exc -> Log.error ("caught exception: " ++ show exc))
-    kill_ui_thread acts_mvar
+ui_handler exc = Log.error ("ui thread died from exception: " ++ show exc)
+app_handler exc = Log.error ("app thread died from  exception: " ++ show exc)
 
 -- | Send the UI to the ui thread and run it, returning its result.
 -- send_action :: MVar.MVar [IO ()] -> Send
@@ -50,22 +48,20 @@ send_action act = do
 add_act acts_mvar x = MVar.modifyMVar_ acts_mvar (return . (x:))
 
 -- | The ui's polling cycle.
-poll_loop acts_mvar msg_chan = do
-    c_initialize
-    Monad.forever $ do
-        wait
-        -- I think that fltk will wake up once for every call to awake, so I
-        -- shouldn't have to worry about another awake call coming in right
-        -- here.
-        handle_actions acts_mvar
-        ui_msgs <- UiMsg.take_ui_msgs
-        STM.atomically (mapM_ (STM.writeTChan msg_chan) ui_msgs)
+poll_loop quit_request acts_mvar msg_chan = do
+    wait
+    -- I think that fltk will wake up once for every call to awake, so I
+    -- shouldn't have to worry about another awake call coming in right
+    -- here.
+    handle_actions acts_mvar
+    ui_msgs <- UiMsg.take_ui_msgs
+    STM.atomically (mapM_ (STM.writeTChan msg_chan) ui_msgs)
+    quit_requested <- MVar.isEmptyMVar quit_request
+    Monad.when (not quit_requested) $
+        poll_loop quit_request acts_mvar msg_chan
 
-kill_ui_thread acts_mvar = do
-    th_id <- MVar.readMVar ui_thread_id
-    -- Send a kill over to be handled so I know it'll be awake, but don't wait
-    -- for a return val.
-    add_act acts_mvar (Concurrent.killThread th_id)
+kill_ui_thread quit_request = do
+    MVar.tryTakeMVar quit_request
     awake -- get it out of wait
 
 foreign import ccall "initialize" c_initialize :: IO ()
