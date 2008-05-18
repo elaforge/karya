1d83c7d4e9640abd975878a8c98efe9429652876
qdunkan <qdunkan@gmail.com>
2008-05-18 02:26:43 +0000

    convert Signal to TrackPos

--- Perform/Signal.hs
+++ Perform/Signal.hs
@@ -1,12 +1,13 @@
 module Perform.Signal where
+import qualified Control.Arrow as Arrow
 import qualified Data.Map as Map
 
 import qualified Util.Data
 
+import Ui.Types
 import qualified Perform.Timestamp as Timestamp
 
-
-import Debug.Trace
+-- import Debug.Trace
 
 
 -- * construction
@@ -15,11 +16,11 @@ import Debug.Trace
 -- signals
 
 data Signal = Signal {
-    signal_map :: Map.Map Timestamp.Timestamp (Method, Val)
+    signal_map :: Map.Map TrackPos (Method, Val)
     } deriving (Show, Eq)
 
-signal :: [(Timestamp.Timestamp, Method, Val)] -> Signal
-signal = Signal . Map.fromList . map (\(ts, meth, val) -> (ts, (meth, val)))
+signal :: [(TrackPos, Method, Val)] -> Signal
+signal = Signal . Map.fromList . map (\(pos, meth, val) -> (pos, (meth, val)))
 
 type Val = Double
 
@@ -46,62 +47,70 @@ data Method =
 -- 'srate'.
 -- TODO: supply srate as a signal
 -- TODO filter dups
-sample :: Signal -> Timestamp.Timestamp -> Timestamp.Timestamp
-    -> [(Timestamp.Timestamp, Val)]
-sample sig start_ts end_ts = _sample srate previous (Map.assocs post) end_ts
+sample :: Signal -> TrackPos -> TrackPos -> [(TrackPos, Val)]
+sample sig start_pos end_pos = _sample srate previous (Map.assocs post) end_pos
     where
-    (pre, post) = Util.Data.split_map start_ts (signal_map sig)
+    (pre, post) = Util.Data.split_map start_pos (signal_map sig)
     previous = if Map.null pre
-        then (Timestamp.Timestamp 0, 0)
-        else (\(ts, (meth, val)) -> (ts, val)) (Map.findMax pre)
-    -- TODO hardcoded for the moment
-    srate = Timestamp.seconds 0.1
+        then (TrackPos 0, 0)
+        else (\(pos, (meth, val)) -> (pos, val)) (Map.findMax pre)
+    -- TODO hardcoded for the moment, this should be 0.1 second at the
+    -- hardcoded pos->ts conversion.
+    srate = TrackPos 100
+
+sample_timestamp :: Signal -> Timestamp.Timestamp -> Timestamp.Timestamp
+    -> [(Timestamp.Timestamp, Val)]
+sample_timestamp sig start_ts end_ts =
+    map (Arrow.first Timestamp.from_track_pos) $
+        sample sig (Timestamp.to_track_pos start_ts)
+            (Timestamp.to_track_pos end_ts)
 
 -- TODO use DList instead of ++
 _sample _ _ [] _ = []
-_sample srate (prev_ts, prev_val) ((ts, (meth, val)):rest) end_ts
-    | ts > end_ts = []
+_sample srate (prev_pos, prev_val) ((pos, (meth, val)):rest) end_pos
+    | pos > end_pos = []
     -- Discontinuities in the input can make interpolate produce NaNs, and
     -- will produce extra samples, so skip them.
-    | prev_ts == ts = _sample srate (ts, val) rest end_ts
+    | prev_pos == pos = _sample srate (pos, val) rest end_pos
     | otherwise = case meth of
-        Set -> (ts, val) : _sample srate (ts, val) rest end_ts
+        Set -> (pos, val) : _sample srate (pos, val) rest end_pos
         Jump (jmeth, jval) ->
-            _sample srate (prev_ts, prev_val) [(ts, (jmeth, jval))] ts
-            ++ ( _sample srate (ts, val) rest end_ts)
-        _ -> let smps = sample_from srate prev_ts ts
+            _sample srate (prev_pos, prev_val) [(pos, (jmeth, jval))] pos
+            ++ ( _sample srate (pos, val) rest end_pos)
+        _ -> let smps = sample_from srate prev_pos pos
             in zip smps
-                (map (interpolate meth (prev_ts, prev_val) (ts, val)) smps)
-            ++ _sample srate (ts, val) rest end_ts
+                (map (interpolate meth (prev_pos, prev_val) (pos, val)) smps)
+            ++ _sample srate (pos, val) rest end_pos
 
-sample_from srate start_ts end_ts
-    | start_ts > end_ts = []
-    | otherwise = start_ts : sample_from srate (start_ts + srate) end_ts
+sample_from srate start_pos end_pos
+    | start_pos > end_pos = []
+    | otherwise = start_pos : sample_from srate (start_pos + srate) end_pos
 
 -- | Get the value of a signal at the given timestamp.
-at :: Signal -> Timestamp.Timestamp -> Val
-at sig ts = interpolate next_meth (prev_ts, prev_val) (next_ts, next_val) ts
+at :: Signal -> TrackPos -> Val
+at sig pos = interpolate next_meth (prev_pos, prev_val) (next_pos, next_val) pos
     where
-    (pre_, at, post) = Map.splitLookup ts (signal_map sig)
+    (pre_, at, post) = Map.splitLookup pos (signal_map sig)
     -- Since 'interpolate' interpolates from the previous value, put an exact
     -- match in pre to ensure that I return its value.
     pre = case at of
         Nothing -> pre_
-        Just v -> Map.insert ts v pre_
+        Just v -> Map.insert pos v pre_
 
-    (prev_ts, (_, prev_val)) = if Map.null pre
-        then ((Timestamp.Timestamp 0), (Set, 0))
+    (prev_pos, (_, prev_val)) = if Map.null pre
+        then ((TrackPos 0), (Set, 0))
         else Map.findMax pre
-    (next_ts, (next_meth, next_val)) = if Map.null post
-        then (ts, (Set, 0))
+    (next_pos, (next_meth, next_val)) = if Map.null post
+        then (pos, (Set, 0))
         else Map.findMin post
 
--- interpolate meth prev next ts =
---     trace ("\n*->" ++ show (meth, prev, next, ts) ++ "\n")
---     (interpolate' meth prev next ts)
+-- interpolate meth prev next pos =
+--     trace ("\n*->" ++ show (meth, prev, next, pos) ++ "\n")
+--     (interpolate' meth prev next pos)
 
-interpolate meth (prev_ts, prev_val) (next_ts, next_val) ts
-    | prev_ts == next_ts = next_val
+interpolate :: Method -> (TrackPos, Val) -> (TrackPos, Val) -> TrackPos -> Val
+interpolate meth (prev_pos, prev_val) (next_pos, next_val) pos
+    | prev_pos == next_pos = next_val
     | otherwise = prev_val + ival
     where
     ival = case meth of
@@ -110,5 +119,4 @@ interpolate meth (prev_ts, prev_val) (next_ts, next_val) ts
         Linear -> amount * (next_val - prev_val)
         Exp n -> let exp = if n >= 0 then n else (1 / abs n)
             in amount**exp * (next_val - prev_val)
-    amount = Timestamp.to_seconds (ts - prev_ts)
-        / Timestamp.to_seconds (next_ts - prev_ts)
+    amount = fromIntegral (pos - prev_pos) / fromIntegral (next_pos - prev_pos)
