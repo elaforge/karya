6bd9541c42f40ea6abb1ad183d68280d0e6aa3f2
qdunkan <qdunkan@gmail.com>
2008-06-18 21:01:22 +0000

    make pprinted vals wrap at 80 cols

--- Util/PPrint.hs
+++ Util/PPrint.hs
@@ -1,3 +1,7 @@
+{- | This is stolen from gleb.alexeev@gmail.com's ipprint package on hackage.
+I'm not just using it directly because I want to pass custom formatting flags
+because my terminal is 80 chars wide.
+-}
 module Util.PPrint where
 import qualified Data.Char as Char
 
@@ -5,13 +9,9 @@ import Language.Haskell.Parser
 import Language.Haskell.Pretty
 import Language.Haskell.Syntax
 import Text.Read
-
--- gleb.alexeev@gmail.com's ipprint package from hackage
-import qualified IPPrint
+import qualified Text.PrettyPrint as PrettyPrint
 
 
-pshow :: Show a => a -> String
-pshow = dedent . IPPrint.pshow
 pprint :: Show a => a -> IO ()
 pprint = putStrLn . pshow
 
@@ -21,13 +21,31 @@ is_str (HsModule _ _ _ _ [HsPatBind _ _ (HsUnGuardedRhs rhs) _]) = case rhs of
     _ -> Nothing
 is_str _ = Nothing
 
+pp_style = PrettyPrint.style
+    { PrettyPrint.ribbonsPerLine = 1, PrettyPrint.lineLength = 80 }
+
+pprint_mode = prettyPrintStyleMode pp_style defaultMode
+
+-- Yay for copy and paste.  TODO clean this up a bit.
+
+-- | Pretty show.
+pshow :: Show a => a -> String
+pshow v = dedent $ case parseModule ("value = " ++ s) of
+        ParseOk m -> tidy (pprint_mode m)
+        ParseFailed _ _   -> s
+    where
+    s = show v
+    tidy x = case readPrec_to_S skipBoring 0 x of
+        [((), tail)] -> "   " ++ tail
+        _            -> s
+
 -- | Pretty print the given value, unless it's a string, in which case return
 -- it unchanged.
 str_pshow :: Show a => a -> String
 str_pshow v = dedent $ case parseModule ("value = " ++ s) of
         ParseOk m -> case is_str m of
             Just s -> s
-            Nothing -> tidy $ prettyPrint m
+            Nothing -> tidy (pprint_mode m)
         ParseFailed _ _   -> s
     where
     s = show v
