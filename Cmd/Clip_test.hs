14653cab83601aed9be7d5e931575509f05d95d1
qdunkan <qdunkan@gmail.com>
2009-08-01 07:55:01 +0000

    add Cmd.CmdTest, utils for Cmd tests

--- Cmd/Clip_test.hs
+++ Cmd/Clip_test.hs
@@ -13,6 +13,7 @@ import qualified Ui.TestSetup as TestSetup
 import qualified Cmd.Cmd as Cmd
 import qualified Cmd.Clip as Clip
 import qualified Cmd.Simple as Simple
+import qualified Cmd.CmdTest as CmdTest
 
 import qualified App.Config as Config
 
@@ -32,9 +33,6 @@ run = TestSetup.run State.empty
 track1_events = [(0, 2, "e11"), (4, 2, "e12"), (8, 2, "e13")]
 track2_events = [(1, 2, "e21"), (5, 2, "e22")]
 
-track2_simple = to_simple track2_events
-track1_simple = to_simple track1_events
-
 to_simple :: [(TrackPos, TrackPos, String)] -> [Simple.Event]
 to_simple = map (\(pos, dur, text) -> (realToFrac pos, realToFrac dur, text))
 
@@ -48,24 +46,11 @@ clip_id = Block.BlockId (Id.id clip_ns Config.clip_block_name)
 with_clip state cstate = State.error_either "with_clip" $
     State.exec state (Clip.state_to_namespace cstate clip_ns)
 
--- | Run a cmd and return everything you could possibly be interested in.
--- Will be Nothing if the cmd aborted.
-run_cmd :: State.State -> Cmd.State -> Cmd.CmdT Identity.Identity a
-    -> Maybe (Either State.StateError
-        (a, Cmd.State, State.State, [Log.Msg]))
-run_cmd ui_state cmd_state cmd = case Cmd.run_id ui_state cmd_state cmd of
-    (cmd_state2, _midi_msgs, logs, result) -> case result of
-        Left err -> Just (Left err)
-        Right (val, ui_state2, _updates) -> case val of
-            Nothing -> Nothing
-            Just v -> Just (Right (v, cmd_state2, ui_state2, logs))
+run_clip ustate cmd = CmdTest.run ustate default_cmd_state cmd
 
-run_clip ui_state cmd = run_cmd ui_state default_cmd_state cmd
+extract_block block_id (val, ustate, _cstate, logs) = (val, block, logs)
+    where Right block = State.eval ustate (Simple.dump_block block_id)
 
-extract_block block_id (val, _cmd_state, ui_state2, logs) = (val, block, logs)
-    where Right block = State.eval ui_state2 (Simple.dump_block block_id)
-
-result_val (val, _, _) = val
 state_val (_, state, _) = state
 
 default_cmd_state = Cmd.empty_state
@@ -73,29 +58,31 @@ default_cmd_state = Cmd.empty_state
     , Cmd.state_focused_view = Just TestSetup.default_view_id
     }
 
+-- TODO there's no reason for this to be in IO
 run_io :: State.State -> Cmd.CmdT Identity.Identity a
-          -> IO (a, Cmd.State, State.State, [Log.Msg])
-run_io state m = do
-    res <- case run_clip state m of
-        Just (Right res) -> return res
-        Just (Left err) -> error $ "state error: " ++ show err
-        Nothing -> error "abort"
-    return res
+          -> IO (a, State.State, Cmd.State, [Log.Msg])
+run_io ustate m = do
+    return $ case run_clip ustate m of
+        Right (res, ustate, cstate, msgs) -> case res of
+            Nothing -> error "abort"
+            Just val -> (val, ustate, cstate, msgs)
+        Left err -> error $ "state error: " ++ show err
 
 extract_events (_, _, tracks) = map (\(_, _, a) -> a) tracks
 mksel a b c d = Just (Block.Selection a b c d)
 
-run_sel state sel cmd = run_io state $ do
+run_sel ustate sel cmd = run_io ustate $ do
     State.set_selection TestSetup.default_view_id Config.insert_selnum sel
     cmd
 
-set_sel sel = State.set_selection TestSetup.default_view_id Config.insert_selnum sel
+set_sel sel = State.set_selection
+    TestSetup.default_view_id Config.insert_selnum sel
 
 -- * copy
 
-run_copy state sel cmd = fmap
+run_copy ustate sel cmd = fmap
     (extract_events . state_val . extract_block clip_id)
-    (run_sel state sel cmd)
+    (run_sel ustate sel cmd)
 
 test_cmd_copy_selection = do
     let state = events_state
@@ -114,7 +101,7 @@ test_cmd_copy_selection = do
         ]
 
 test_namespace_ops = do
-    let run state cmd = do fmap (\(_, _, state, _) -> state) (run_io state cmd)
+    let run state cmd = do fmap (\(_, state, _, _) -> state) (run_io state cmd)
 
     state <- run events_state $ do
         set_sel $ mksel 1 1 2 4
@@ -146,7 +133,7 @@ test_cmd_paste_overwrite = do
     res <- run (mksel 1 1 1 1) Clip.cmd_paste_overwrite
     equal res
         [ [(0, 1, "e11"), (1, 2, "c1"), (5, 2, "c2"), (8, 2, "e13")]
-        , track2_simple
+        , track2_events
         ]
 
     -- Second track isn't overwritten because clip has no second track.
@@ -154,21 +141,21 @@ test_cmd_paste_overwrite = do
     res <- run (mksel 1 1 2 1) Clip.cmd_paste_overwrite
     equal res
         [ [(0, 1, "e11"), (1, 2, "c1"), (5, 2, "c2"), (8, 2, "e13")]
-        , track2_simple
+        , track2_events
         ]
 
     -- Only replace the second event, since clipboard is clipped to sel.
     res <- run (mksel 1 1 1 4) Clip.cmd_paste_overwrite
     equal res
         [ [(0, 1, "e11"), (1, 2, "c1"), (4, 2, "e12"), (8, 2, "e13")]
-        , track2_simple
+        , track2_events
         ]
 
     -- Pasted events are clipped to the selection.
     res <- run (mksel 1 2 1 7) Clip.cmd_paste_overwrite
     equal res
         [ [(0, 2, "e11"), (2, 2, "c1"), (6, 1, "c2"), (8, 2, "e13")]
-        , track2_simple
+        , track2_events
         ]
 
 test_cmd_paste_merge = do
@@ -179,21 +166,21 @@ test_cmd_paste_merge = do
     equal res
         [ [(0, 1, "e11"), (1, 2, "c1"), (4, 1, "e12"), (5, 2, "c2"),
             (8, 2, "e13")]
-        , track2_simple
+        , track2_events
         ]
     -- Not much more to test here since it's all the same as
     -- cmd_paste_overwrite.
 
     -- They all overlap.
     res <- run (mksel 1 1 1 1) Clip.cmd_paste_soft_merge
-    equal res [track1_simple, track2_simple]
+    equal res [track1_events, track2_events]
 
     -- This time they make it in.
     res <- run (mksel 1 2 1 2) Clip.cmd_paste_soft_merge
     equal res
         [ [(0, 2, "e11"), (2, 2, "c1"), (4, 2, "e12"), (6, 2, "c2"),
             (8, 2, "e13")]
-        , track2_simple
+        , track2_events
         ]
 
 test_cmd_paste_insert = do
@@ -205,12 +192,12 @@ test_cmd_paste_insert = do
     equal res
         [ [(0, 1, "e11"), (1, 2, "c1"), (5, 2, "c2"),
             (10, 2, "e12"), (14, 2, "e13")]
-        , track2_simple
+        , track2_events
         ]
 
     -- selection pushes by selection length
     res <- run (mksel 1 1 1 3) Clip.cmd_paste_insert
     equal res
         [ [(0, 1, "e11"), (1, 2, "c1"), (6, 2, "e12"), (10, 2, "e13")]
-        , track2_simple
+        , track2_events
         ]
---
+++ Cmd/CmdTest.hs
@@ -0,0 +1,79 @@
+-- | Utilities for cmd tests.
+module Cmd.CmdTest where
+import qualified Control.Monad.Identity as Identity
+
+import qualified Util.Log as Log
+import qualified Midi.Midi as Midi
+
+import qualified Ui.Key as Key
+import qualified Ui.State as State
+import qualified Ui.UiMsg as UiMsg
+import qualified Ui.TestSetup as TestSetup
+
+import qualified Cmd.Simple as Simple
+import qualified Cmd.Cmd as Cmd
+import qualified Cmd.Msg as Msg
+
+import qualified Perform.Timestamp as Timestamp
+
+import qualified App.Config as Config
+
+default_block_id = TestSetup.default_block_id
+default_view_id = TestSetup.default_view_id
+
+-- | Run a cmd and return everything you could possibly be interested in.
+-- Will be Nothing if the cmd aborted.
+run :: State.State -> Cmd.State -> Cmd.CmdT Identity.Identity a
+    -> (Either State.StateError
+        (Maybe a, State.State, Cmd.State, [Log.Msg]))
+run ustate cstate cmd = case Cmd.run_id ustate cstate cmd of
+    (cmd_state2, _midi_msgs, logs, result) -> case result of
+        Left err -> Left err
+        Right (val, ui_state2, _updates) ->
+            Right (val, ui_state2, cmd_state2, logs)
+
+with_sel sel cmd = do
+    State.set_selection TestSetup.default_view_id Config.insert_selnum sel
+    Cmd.modify_state $ \st ->
+        st { Cmd.state_focused_view = Just TestSetup.default_view_id }
+    cmd
+
+-- | Run cmd with the given tracks, and return the resulting tracks.
+run_tracks :: [TestSetup.TrackSpec] -> Cmd.CmdT Identity.Identity a
+    -> Either String (Maybe a, [(String, [Simple.Event])], [String])
+run_tracks track_specs cmd =
+    case run ustate cstate cmd of
+        Right (val, ustate2, cstate2, logs) ->
+            Right (val, extract_tracks ustate2, map Log.msg_text logs)
+        Left err -> Left (show err)
+    where
+    cstate = cmd_state
+    (_, ustate) = TestSetup.run_mkview track_specs
+
+extract_tracks ustate = map (\(_, title, events) -> (title, events)) tracks
+    where
+    ((_, _, tracks), _) =
+        TestSetup.run ustate (Simple.dump_block default_block_id)
+
+cmd_state = Cmd.empty_state
+    { Cmd.state_focused_view = Just default_view_id
+    }
+
+
+-- * msg
+
+empty_context = UiMsg.Context Nothing Nothing Nothing
+
+make_key :: Bool -> Key.Key -> Msg.Msg
+make_key down k = Msg.Ui
+    (UiMsg.UiMsg empty_context (UiMsg.MsgEvent (UiMsg.Kbd state k)))
+    where state = if down then UiMsg.KeyDown else UiMsg.KeyUp
+
+key_down = make_key True . Key.KeyChar
+key_up = make_key False . Key.KeyChar
+backspace = make_key True Key.Backspace
+
+make_midi :: Midi.ChannelMessage -> Msg.Msg
+make_midi chan_msg = Msg.Midi $
+    Midi.ReadMessage (Midi.ReadDevice "test") Timestamp.immediately
+        (Midi.ChannelMessage 0 chan_msg)
