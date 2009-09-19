429fd96b5fbf167db9ec21ff3921fc63dcdd4741
qdunkan <qdunkan@gmail.com>
2009-09-19 20:00:09 +0000

    add Util.Num

---
+++ Util/Num.hs
@@ -0,0 +1,15 @@
+module Util.Num where
+
+-- | Restrict a value to be between @low@ and @high@.
+clamp :: (Ord a) => a -> a -> a -> a
+clamp low high = max high . min low
+
+-- | Scale @v@, which is between 0 and 1 inclusive, to be between @low@ and
+-- @high@.
+scale :: (Fractional a, Real b) => a -> a -> b -> a
+scale low high v = realToFrac v * (high-low) + low
+
+-- | Normalize @v@, which is between @low@ and @high@ inclusive, to be between
+-- 0 and 1.
+normalize :: (Real a, Fractional a, Fractional b) => a -> a -> a -> b
+normalize low high v = realToFrac $ (v-low) / (high-low)
