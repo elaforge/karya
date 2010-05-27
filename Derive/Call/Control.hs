f0e634030a4fcbe90ce058ab99f87bd4513ae57f
qdunkan <qdunkan@gmail.com>
2010-05-27 07:28:52 +0000

    implement exponential interpolator

--- Derive/Call/Control.hs
+++ Derive/Call/Control.hs
@@ -8,7 +8,7 @@ import Ui
 import qualified Derive.Call as Call
 import qualified Derive.Derive as Derive
 import qualified Derive.TrackLang as TrackLang
-import Derive.TrackLang (required)
+import Derive.TrackLang (required, optional)
 
 import qualified Perform.Pitch as Pitch
 import qualified Perform.PitchSignal as PitchSignal
@@ -28,6 +28,7 @@ control_calls = Derive.make_calls
     [ ("=", Call.c_equal Derive.no_control)
     , ("", c_set)
     , ("i", c_linear)
+    , ("e", c_exponential)
     ]
 
 c_set :: Derive.ControlCall
@@ -40,20 +41,26 @@ c_linear :: Derive.ControlCall
 c_linear = Derive.generate_one $ \args _ _ _ ->
     case TrackLang.passed_vals args of
         [] -> case TrackLang.passed_prev_val args of
-            Nothing -> return $
-                Derive.throw "can't set to previous val when there was none"
+            Nothing -> return $ Derive.throw
+                "can't set to previous val when there was none"
             Just (_, prev_y) -> return $ do
                 pos <- Derive.score_to_real 0
                 return $ Signal.signal [(pos, prev_y)]
-        _ -> control_interpolate id args
+        _ -> TrackLang.call1 args (required "val") $ \val ->
+            control_interpolate id val args
+
+c_exponential :: Derive.ControlCall
+c_exponential = Derive.generate_one $ \args _ _ _ ->
+    TrackLang.call2 args (required "val", optional "exp" 2) $ \val exp ->
+        control_interpolate (expon exp) val args
 
 -- | Create samples according to an interpolator function.  The function is
 -- passed values from 0--1 representing position in time and is expected to
 -- return values from 0--1 representing the Y position at that time.  So linear
 -- interpolation is simply @id@.
-control_interpolate :: (Double -> Signal.Y) -> TrackLang.PassedArgs Signal.Y
-    -> Either TrackLang.TypeError Derive.ControlDeriver
-control_interpolate f args = TrackLang.call1 args (required "val") $ \val -> do
+control_interpolate :: (Double -> Signal.Y) -> Signal.Y
+    -> TrackLang.PassedArgs Signal.Y -> Derive.ControlDeriver
+control_interpolate f val args = do
     cur <- Derive.score_to_real 0
     srate <- Derive.require_val TrackLang.v_srate
     case TrackLang.passed_prev_val args of
@@ -99,11 +106,17 @@ c_note_linear = Derive.generate_one $ \args _ _ _ ->
                 scale <- Derive.require_val TrackLang.v_scale
                 return $ PitchSignal.signal (Pitch.scale_id scale)
                     [(pos, prev_y)]
-        _ -> pitch_interpolate id args
+        _ -> TrackLang.call1 args (required "note") $ \note ->
+            pitch_interpolate id note args
 
-pitch_interpolate :: (Double -> Double) -> TrackLang.PassedArgs PitchSignal.Y
-    -> Either TrackLang.TypeError Derive.PitchDeriver
-pitch_interpolate f args = TrackLang.call1 args (required "note") $ \note -> do
+c_note_exponential :: Derive.PitchCall
+c_note_exponential = Derive.generate_one $ \args _ _ _ ->
+    TrackLang.call2 args (required "note", optional "exp" 2) $ \note exp ->
+        pitch_interpolate (expon exp) note args
+
+pitch_interpolate :: (Double -> Signal.Y) -> Pitch.Note
+    -> TrackLang.PassedArgs PitchSignal.Y -> Derive.PitchDeriver
+pitch_interpolate f note args = do
         cur <- Derive.score_to_real 0
         scale <- Derive.require_val TrackLang.v_scale
         srate <- Derive.require_val TrackLang.v_srate
@@ -130,6 +143,14 @@ interpolate :: RealTime -> (y -> y -> Double -> y) -> (Double -> Double)
 interpolate srate scale f x0 y0 x1 y1 =
     zip xs (map (scale y0 y1 . f . Num.normalize x0 x1) xs)
     where xs = range_until x0 x1 srate
+
+-- | Negative exponents produce a curve that jumps from the "starting point"
+-- which doesn't seem too useful, so so hijack the negatives as an easier way
+-- to write 1/n.  That way n is smoothly departing, while -n is smoothly
+-- approaching.
+expon :: Double -> Double -> Double
+expon n x = x**exp
+    where exp = if n >= 0 then n else (1 / abs n)
 
 -- | Enumerate a half-open range, except this one omits the first value and
 -- includes the final one.  Uses multiplication instead of successive addition
