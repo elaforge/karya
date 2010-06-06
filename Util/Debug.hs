8ae67ff9c6ec75d18ab6580e5315ffc6bc91b4e7
qdunkan <qdunkan@gmail.com>
2010-06-06 17:02:44 +0000

    Util.Debug consistently prepends prefix and pretty-prints values

--- Util/Debug.hs
+++ Util/Debug.hs
@@ -1,22 +1,36 @@
-module Util.Debug where
+module Util.Debug (
+    trace, traces, tracef, trace_ret, tracem, traceM
+) where
 
 import qualified Debug.Trace as Trace
+import qualified Util.PPrint as PPrint
+import qualified Util.Seq as Seq
 
 
 trace :: String -> b -> b
-trace = Trace.trace
+trace s = Trace.trace (prefix ++ s)
 
+-- | Print a showable value.
 traces :: (Show a) => a -> b -> b
-traces a b = trace (show a) b
+traces a b = trace (pshow a) b
 
-trace_ret :: (Show a, Show b) => a -> b -> b
-trace_ret a ret = trace (show a ++ " -> " ++ show ret) ret
-
-tracev :: (Show a) => a -> a
-tracev x = Trace.trace ("**trace: " ++ show x) x
-
+-- | Print a value prefixed with a msg.
 tracem :: (Show a) => String -> a -> a
-tracem msg x = Trace.trace ("**" ++ msg ++ ": " ++ show x) x
+tracem msg x = trace (msg ++ ": " ++ pshow x) x
 
+-- | Print a value in a monad.  The monad will force it to be printed.
 traceM :: (Show a, Monad m) => a -> m ()
-traceM msg = trace ("** " ++ show msg) (return ())
+traceM val = trace (pshow val) (return ())
+
+-- | Print a value after applying a function to it.
+tracef :: (Show b) => (a -> b) -> a -> a
+tracef f val = trace (pshow (f val)) val
+
+-- | Trace input and output of a function.
+trace_ret :: (Show a, Show b) => a -> b -> b
+trace_ret a ret = trace (pshow a ++ " -> " ++ pshow ret) ret
+
+prefix = "** "
+
+pshow :: (Show a) => a -> String
+pshow = Seq.strip . PPrint.pshow
