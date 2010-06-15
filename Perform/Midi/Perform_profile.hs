53fcbf5bb4332100608f17b1f2236fa291cff8df
qdunkan <qdunkan@gmail.com>
2010-06-15 05:17:37 +0000

    add Perform.Midi.Perform_profile

---
+++ Perform/Midi/Perform_profile.hs
@@ -0,0 +1,69 @@
+module Perform.Midi.Perform_profile where
+import qualified Control.DeepSeq as DeepSeq
+import qualified Data.Map as Map
+import Util.Test
+
+import Ui
+
+import qualified Midi.Midi as Midi
+
+import qualified Derive.Score as Score
+
+import qualified Perform.Midi.Control as Control
+import qualified Perform.Midi.Perform as Perform
+import qualified Perform.Midi.Instrument as Instrument
+
+import qualified Instrument.MidiDb as MidiDb
+
+import qualified Perform.Signal as Signal
+
+
+-- perform :: MidiDb.LookupMidiInstrument
+--     -> Instrument.Config -> [Event] -> ([Midi.WriteMessage], [Warning.Warning])
+
+profile_perform = do
+    let f evts = (length msgs, logs)
+            where (msgs, logs) = Perform.perform test_lookup inst_config evts
+    let len = 100000
+    let sig = force $ Signal.signal (zip [0, 0.25 .. len] (cycle vals))
+        vals = map (/10) ([0..10] ++ [10, 9 .. 1])
+    let (msgs, logs) = f
+            [(mkevent 0 len [(Control.Control "cc1", sig)] Signal.empty)]
+    -- Forcing the msgs first leads to a big space drag.  I'm not sure why...
+    -- shouldn't forcing the logs result in a much larger unevaluated msgs?
+    pprint logs
+    pprint msgs
+    -- pprint $ f [(mkevent 0 len [(Control.Control "cc1", sig)] Signal.empty)]
+
+profile_perform_control = do
+    let len = 300000
+    let sig = force $ Signal.signal (zip [0, 0.25 .. len] (cycle vals))
+        vals = map (/10) ([0..10] ++ [10, 9 .. 1])
+    let f end  = Perform.perform_control Control.empty_map 0 0 end
+            (Control.Control Control.c_mod, sig)
+    let (msg, warns) = f len
+    pprint warns
+    pprint (length msg)
+
+
+force val = DeepSeq.deepseq val val
+
+
+mkevent :: RealTime -> RealTime -> [(Control.Control, Signal.Control)]
+    -> Signal.NoteNumber -> Perform.Event
+mkevent start dur controls pitch_sig =
+    Perform.Event inst1 start dur (Map.fromList controls) pitch_sig []
+
+
+test_lookup :: MidiDb.LookupMidiInstrument
+test_lookup _ (Score.Instrument inst)
+    | inst == "inst1" = Just $ inst1
+    | otherwise = Nothing
+
+inst1 = mkinst "inst1"
+mkinst name = Instrument.instrument "synth" name Nothing Control.empty_map
+    (-1, 1)
+
+inst_config = Instrument.config
+    [ (Score.Instrument "inst1", [(dev, 0), (dev, 1)]) ]
+    where dev = Midi.WriteDevice "dev1"
