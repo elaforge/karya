7a120097519909ef5f06078102ce02408c15e616
qdunkan <qdunkan@gmail.com>
2010-09-01 17:22:33 +0000

    fix printf bug

--- App/Main.hs
+++ App/Main.hs
@@ -237,7 +237,7 @@ make_write_midi :: Map.Map Midi.WriteDevice Midi.WriteDevice
     -> MidiImp.WriteMap -> Midi.WriteMessage -> IO ()
 make_write_midi wdev_map write_map (Midi.WriteMessage wdev ts msg) = do
     let real_wdev = Map.get wdev wdev wdev_map
-    Printf.printf "PLAY %s->%s %s: %s\n" (Midi.un_write_device wdev)
+    Printf.printf "PLAY %s->%s %d: %s\n" (Midi.un_write_device wdev)
         (Midi.un_write_device real_wdev) (Timestamp.to_millis ts) (show msg)
     case Map.lookup real_wdev write_map of
         Nothing -> Log.error $ show real_wdev ++ " not in devs: "
