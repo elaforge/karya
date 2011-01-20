2d76e5dc32a11b82eec462c5c0d2b7a4c723bb0a
qdunkan <qdunkan@gmail.com>
2011-01-20 01:34:50 +0000

    fix hspp to do unqualified replacements more correctly

--- tools/hspp.hs
+++ tools/hspp.hs
@@ -3,7 +3,21 @@
 --
 -- I don't use cpp because it doesn't like dots in symbols.
 --
--- TODO this screws up \\ string continuation
+-- I need to replace only function calls, but can't know what those are without
+-- actually parsing the source.  I think replacement of qualified names is
+-- pretty safe since those don't really occur otherwise, but replacing
+-- unqualified names in the same module is tricky.
+--
+-- Known problems:
+--
+-- - \"where\" in the module comment will make macros in the module export list
+-- be replaced
+--
+-- - mangles comments and replaces macros in them, but I don't really care
+-- about that
+--
+-- - Since annotations are per-line, @x = x@ won't replace @x@, but that's
+-- a silly definition anyway.
 import qualified Data.List as List
 import qualified Data.Char as Char
 import qualified Data.Maybe as Maybe
@@ -37,6 +51,7 @@ test_macros = map (SrcposMacro Nothing)
     , "check_msg"]
 
 
+main :: IO ()
 main = do
     args <- System.Environment.getArgs
     [orig_fn, src_fn, dest_fn] <- if length args == 3 then return args
@@ -44,24 +59,39 @@ main = do
     input <- readFile src_fn
     let macros = if "_test.hs" `List.isSuffixOf` orig_fn
             then test_macros else global_macros
-    writeFile dest_fn (process orig_fn macros input)
+    writeFile dest_fn (process (clean_fn orig_fn) macros input)
+
+clean_fn :: FilePath -> FilePath
+clean_fn fn
+    -- ghc tends to pass filenames with leading ./
+    | "./" `List.isPrefixOf` fn = drop 2 fn
+    | otherwise = fn
 
 process :: FilePath -> [Macro] -> String -> String
 process fn macros = unlines . (line_pragma:)
-    . map (\(i, func, line) -> replace_ids (make_replace fn i func macros) line)
+    . map (\(line, annot) -> replace_ids (replace macros fn annot) line)
     . annotate . smart_lines
     where line_pragma = "{-# LINE 1 \"" ++ fn ++ "\" #-}"
 
-make_replace :: FilePath -> Integer -> Maybe String -> [Macro]
-    -> (String -> String)
-make_replace fn lineno func_name macros tok =
-    case find_macro func_name (fn_to_module fn) tok macros of
+-- | Replace a token with the version applied to the srcpos.
+--
+-- This is tricky because when I am in other modules, I want to replace only
+-- the qualified function call \"A.f\".  If I am replacing a function in the
+-- same module as it is defined, I want to replace non-qualified, but must
+-- be careful to only replace function calls.  Not definitions, not export
+-- lists!
+replace :: [Macro] -> FilePath -> Annotation -> String -> String
+replace macros fn annot tok
+    | a_declaration annot || not (a_post_header annot) = tok
+    | otherwise = case find_macro macros func_name (fn_to_module fn) tok of
         Nothing -> tok
         Just repl -> "(" ++ repl ++ " (" ++ srcpos ++ "))"
-    where srcpos = make_srcpos fn func_name lineno
+    where
+    func_name = a_func_name annot
+    srcpos = make_srcpos fn (a_func_name annot) (a_line annot)
 
-find_macro :: Maybe String -> String -> String -> [Macro] -> Maybe String
-find_macro func_name mod token macros
+find_macro :: [Macro] -> Maybe String -> String -> String -> Maybe String
+find_macro macros func_name mod token
     | any matches macros = Just (token ++ "_srcpos")
     | otherwise = Nothing
     where
@@ -81,8 +111,7 @@ fn_to_module :: FilePath -> String
 fn_to_module = map repl . FilePath.dropExtension
     where repl c = if c == '/' then '.' else c
 
-
-make_srcpos :: FilePath -> Maybe String -> Integer -> String
+make_srcpos :: FilePath -> Maybe String -> Int -> String
 make_srcpos fn func_name lineno = show (Just (fn, func_name, lineno))
 
 replace_ids :: (String -> String) -> String -> String
@@ -96,22 +125,38 @@ replace_ids replace contents = spaces ++ tok2 ++ replace_ids replace after_tok
     tok2 = replace tok
 
 
--- | Annotate lines with their line numbers and func names.
-annotate :: [String] -> [(Integer, Maybe String, String)]
-annotate lines = zip3 [1..] (annotate_func_names lines) lines
+data Annotation = Annotation {
+    a_line :: Int
+    -- | If function name if this token is inside a function.
+    , a_func_name :: Maybe String
+    -- | True if left of @::@ or in an import list.
+    , a_declaration :: Bool
+    -- | Line is after the module's @where@
+    , a_post_header :: Bool
+    } deriving (Show)
 
-annotate_func_names lines = tail (scanl scan Nothing lines)
+annotate :: [String] -> [(String, Annotation)]
+annotate lines =
+    zip lines (tail (scanl scan_line (Annotation 0 Nothing False False) lines))
 
-scan last_func line = case func_name line of
-    Nothing -> last_func
-    Just x -> Just x
+scan_line :: Annotation -> String -> Annotation
+scan_line old_annot line = Annotation
+    (a_line old_annot + 1)
+    (maybe (a_func_name old_annot) Just (line_to_func line))
+    (Regex.matches declaration line || "import " `List.isPrefixOf` line)
+    -- fooled by 'where' in comment or inside another word
+    -- doesn't work at all for modules without a name or imports
+    (a_post_header old_annot || "where" `List.isInfixOf` line
+        || "import" `List.isPrefixOf` line)
 
-func_name :: String -> Maybe String
-func_name line = case Regex.find_groups reg line of
+line_to_func :: String -> Maybe String
+line_to_func line = case Regex.find_groups definition line of
         [] -> Nothing
         (_, [fname]) : _ -> Just fname
         groups -> error $ "unexpected groups: " ++ show groups
-    where reg = Regex.make "^([a-z0-9_][A-Za-z0-9_]*) .*="
+
+definition = Regex.make "^([a-z0-9_][A-Za-z0-9_]*) .*="
+declaration = Regex.make "^[a-z0-9_][A-Za-z0-9_, ]* *::"
 
 
 -- * parsing
@@ -144,7 +189,7 @@ smart_lines text = line : smart_lines rest
 
 test_find_macro = do
     let f mod token macro_mod = Maybe.isJust $
-            find_macro (Just "qq") mod token [SrcposMacro macro_mod "f"]
+            find_macro [SrcposMacro macro_mod "f"] (Just "qq") mod token
     -- If module isn't set, look for 'f'.
     check (not (f "A.B" "B.f" Nothing))
     check (f "A.B" "f" Nothing)
@@ -156,6 +201,10 @@ test_find_macro = do
     -- In same module, look for 'f'
     check (f "A.B" "f" (Just "A.B"))
     check (not (f "A.B" "B.f" (Just "A.B")))
+
+test_annotate = do
+    pprint $ annotate ["module X (", "foo", ") where",
+        "foo = bar", "bar, buz :: Baz", "bar a b = a b", "x = 10"]
 
 test_smart_lines = do
     let f = smart_lines
