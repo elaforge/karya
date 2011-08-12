7a65128794b6107e3b1db60f2a5cb45d73cc7072
qdunkan <qdunkan@gmail.com>
2011-08-12 19:01:33 +0000

    documentation and cleanup for MsgCollector.h

--- fltk/MsgCollector.h
+++ fltk/MsgCollector.h
@@ -4,80 +4,45 @@
 #include "Block.h"
 #include "Track.h"
 
-/*
-Msgs:
+/* UiMsgs are how the fltk layer communicates with the haskell layer.
 
-Keystrokes:
-If you click on an input (block or track title), it gets visually highlighted.
-Keystrokes go to that input until you click outside or hit return or tab.
+   Some events are handled directly by fltk widgets, but the rest are
+   sent as fltk events.  There is also a set of msgs that are not fltk
+   events and carry their own event-specific data with them.
 
-When focus leaves an input, it emits (InputChanged, BlockView, Maybe TrackNum)
+   Various fltk event handlers will call the various MsgCollector msgs
+   to put a msg in the global queue.  Every time the haskell side of the UI
+   event loop cycles, it will drain the queue and forward those to the Cmd
+   event loop (responder).
 
-Otherwise, keystrokes are emitted as UiMsgs.  The mouse position shouldn't
-matter for these.
+    Msgs:
 
-Mouse:
-Clicks, drags, and releases on tracks are reported as UiMsgs.  The app can
-use this to set selections or whatever.  Scrolling or zooming via the GUI
-widgets is reported as SetZoom or SetTrackScroll.
+    Keystrokes:
+    If you click on an input (block or track title), it gets visually
+    highlighted.  Keystrokes go to that input until you click outside or hit
+    return or tab.
 
-Dragging a track boundary or the ruler edge is not reported.
+    When focus leaves an input, it emits (InputChanged, BlockView, Maybe
+    TrackNum)
 
-Trying to close the window is reported as CloseBlock.
+    Otherwise, keystrokes are emitted as UiMsgs.  The mouse position shouldn't
+    matter for these.
 
+    Mouse:
+    Clicks, drags, and releases on tracks are reported as UiMsgs.  The app can
+    use this to set selections or whatever.  Scrolling or zooming via the GUI
+    widgets is reported as SetZoom or SetTrackScroll.
 
-This is much simpler than fltk's model since the only widgets that get events
-are inputs, and there is no keyboard navigation.
+    Dragging a track boundary or the ruler edge is not reported.
 
+    Trying to close the window is reported as CloseBlock.
 
-click:
-Find the innermost widget under the pointer.  Offer it focus, and clear
-existing focus if it doesn't accept.  Offer it FL_PUSH, and set pushed() on it
-if it accepts.
-If the widget didn't accept FL_PUSH, call a global handler.
-
-drag:
-If widget accepted FL_PUSH (i.e. is pushed()), send directly to that widget.
-Otherwise, call the global handler.
-
-kbd:
-If a widget has focus, send directly to that widget.
-If the widget didn't accept it, call the global handler.
-
-All other events are ignored.
-
-I also want to disable all of that keyboard arrow key navigation stuff, so
-I think groups would just be responsible for finding which of their children
-gets focus and pushed on FL_PUSH.
-
-So I thought if I overrode Fl_Group::handle to only handle FL_PUSH
-
-
-TODO:
-blur() function sets focus to the window
-window has a handle that collects KBD
-
-window accepts push and drag if no one else did, and collects
-
-if a child accepted PUSH, window won't get it and won't collect
-
-make MoveTile not accept PUSH if it has a modifier, so I can expand dividers
-with ^click or something
-
+    This is much simpler than fltk's model since the only widgets that get
+    events are inputs, and there is no keyboard navigation.
 */
 
-
 // This struct is simple so it can be serialized to haskell.
-//
-// It's actually a union and the meaning of the various fields change
-// depending on the 'type' field.
-//
-// TODO it would be easier to understand if I made bits actual unions, but
-// I'm not totally sure what the FFI would think of that, and it's not so
-// bad if the only code that cares is fltk/MsgCollector.cc and
-// UiMsg/UiMsgC.hsc.
 struct UiMsg {
-    // It's always initialized manually, but STL needs a default constructor.
     UiMsg() {}
 
     // This is like the destructor, but isn't since this structure owns
@@ -107,8 +72,7 @@ struct UiMsg {
         return names;
     }
 
-    // Type tag.
-    MsgType type;
+    MsgType type; // Type tag.
 
     // Every msg has context.
     struct Context {
@@ -118,7 +82,9 @@ struct UiMsg {
 
         // View with focus.
         BlockViewWindow *focus;
-        // View to which this event applies, if any.
+        // View to which this event applies, if any.  In haskell this is
+        // given to the UiUpdates, but it's more of a hassle to nest types
+        // in c++, so make it an optional field here.
         BlockViewWindow *view;
         // Mouse was over this track.
         // Actually a bool, but haskell FFI doesn't support bools.
@@ -130,11 +96,12 @@ struct UiMsg {
         ScoreTime pos;
     } context;
 
+    // WARNING:
+    // Union members can't have default constructors, so make extra sure all
+    // fields are initialized.
+
     // Fields from the various fltk event_*() functions, used for 'msg_event'.
     struct Event {
-        // Event() : event(0), button(0), clicks(0), x(0), y(0), is_click(0),
-        //     key(0), modifier_state(0), is_repeat(0)
-        // {}
         int event;
         int button, clicks, x, y;
         char is_click;
@@ -144,35 +111,29 @@ struct UiMsg {
     };
 
     struct Resize {
-        // Resize() : rect(0), visible_track(0), visible_time(0) {}
         IRect *rect;
         int visible_track, visible_time;
     };
 
     struct Zoom {
-        // Zoom() : zoom(0) {}
         ZoomInfo *zoom;
     };
 
     struct TrackWidth {
-        // TrackWidth() : width(0) {}
         int width;
     };
 
     struct TrackScroll {
-        // TrackScroll() : scroll(0) {}
         int scroll;
     };
 
     // If context.has_track, this is a track title update, otherwise it's
     // the block title.
     struct Input {
-        // Input() : text(0) {}
         char *text;
     };
 
     struct ScreenSize {
-        // ScreenSize() : rect(0), screen(0), screens(0) {}
         IRect *rect;
         int screen, screens;
     };
@@ -191,7 +152,6 @@ struct UiMsg {
 std::ostream &operator<<(std::ostream &os, const UiMsg &m);
 std::ostream &operator<<(std::ostream &os, const UiMsg::Context &m);
 std::ostream &operator<<(std::ostream &os, const UiMsg::Event &m);
-
 
 class MsgCollector {
 public:
@@ -212,10 +172,7 @@ public:
     // 'view' and 'w' are const, but adding it causes const trickle.
     void block_update(Fl_Widget *w, UiMsg::MsgType type);
     void block_update(Fl_Widget *w, UiMsg::MsgType type, int tracknum);
-
     void window_update(BlockViewWindow *view, UiMsg::MsgType type);
-    // void window_update(BlockViewWindow *view, UiMsg::MsgType type,
-    //         int tracknum);
 
     // Send one msg_screen_size msg for each screen.
     //
