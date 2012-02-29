28a4a189d8beb95299643d8b55f05f572dfdf614
qdunkan <qdunkan@gmail.com>
2012-02-29 01:31:34 +0000

    fix bug where double click wouldn't open a block call on a collapsed track

--- Cmd/BlockConfig.hs
+++ Cmd/BlockConfig.hs
@@ -77,10 +77,11 @@ cmd_open_block = do
     let call_of = NoteTrack.block_call ns
     sel <- Selection.events
     forM_ sel $ \(_, _, events) -> forM_ events $ \(_, event) ->
-        when_just (call_of (Event.event_string event)) $ \block_id -> do
-            views <- State.get_views_of block_id
-            maybe (Create.fitted_view block_id >> return ())
-                ViewConfig.bring_to_front (Seq.head (Map.keys views))
+        when_just (call_of (Event.event_string event)) $ \block_id ->
+            whenM (Maybe.isJust <$> State.lookup_block block_id) $ do
+                views <- State.get_views_of block_id
+                maybe (Create.fitted_view block_id >> return ())
+                    ViewConfig.bring_to_front (Seq.head (Map.keys views))
 
 -- * track
 
