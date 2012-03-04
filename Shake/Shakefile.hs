71de81265f6dccc049f8cf89d63913429412ceba
qdunkan <qdunkan@gmail.com>
2012-03-04 19:06:57 +0000

    shakefile: remove unused midiDriver flags

--- Shake/Shakefile.hs
+++ Shake/Shakefile.hs
@@ -82,7 +82,6 @@ oDir = (</> "obj") . buildDir
 data Flags = Flags {
     define :: [String]
     , midiLibs :: [String]
-    , midiDriver :: [String]
     , cInclude :: [String]
     , ccFlags :: [String]
     , fltkCc :: [String]
@@ -92,11 +91,11 @@ data Flags = Flags {
     } deriving (Show)
 
 instance Monoid.Monoid Flags where
-    mempty = Flags [] [] [] [] [] [] [] [] []
-    mappend (Flags a1 b1 c1 d1 e1 f1 g1 h1 i1)
-            (Flags a2 b2 c2 d2 e2 f2 g2 h2 i2) =
+    mempty = Flags [] [] [] [] [] [] [] []
+    mappend (Flags a1 b1 c1 d1 e1 f1 g1 h1)
+            (Flags a2 b2 c2 d2 e2 f2 g2 h2) =
         Flags (a1<>a2) (b1<>b2) (c1<>c2) (d1<>d2) (e1<>e2) (f1<>f2) (g1<>g2)
-            (h1<>h2) (i1<>i2)
+            (h1<>h2)
 
 -- * binaries
 
@@ -267,7 +266,6 @@ configure = do
             { define = ["-DCORE_MIDI"]
             , midiLibs = words $ "-framework CoreFoundation "
                 ++ "-framework CoreMIDI -framework CoreAudio"
-            , midiDriver = ["-DCORE_MIDI"]
             }
         "linux" -> mempty
         unknown -> error $ "unknown os: " ++ show unknown
