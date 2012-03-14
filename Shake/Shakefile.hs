7940de561a93b811b6e2b99cacfb226eea2b31fe
qdunkan <qdunkan@gmail.com>
2012-03-14 05:09:23 +0000

    shakefile: fix bug in writeConfigH

--- Shake/Shakefile.hs
+++ Shake/Shakefile.hs
@@ -389,8 +389,8 @@ writeConfigH :: IO ()
 writeConfigH = do
     useRepl <- fmap (("repl" `elem`) . map fst) Environment.getEnvironment
     useHint <- fmap (("hint" `elem`) . map fst) Environment.getEnvironment
-    if useRepl then mangle [hint] [ghc]
-        else if useHint then mangle [ghc] [hint]
+    if useRepl then mangle [ghc] [hint]
+        else if useHint then mangle [hint] [ghc]
         else mangle [] [ghc, hint] -- use stub
     where
     mangle = CcDeps.enableDefines "hsconfig.h"
