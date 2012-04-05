be123ed8dbb82a3695095a32904a96bdd1ff67f6
qdunkan <qdunkan@gmail.com>
2012-04-05 04:58:24 +0000

    fix broken Util.Pretty_test

--- Util/Pretty_test.hs
+++ Util/Pretty_test.hs
@@ -1,29 +1,29 @@
 module Util.Pretty_test where
 import qualified Data.Map as Map
-import qualified Text.PrettyPrint as PP
 
 import Util.Control
-import Util.Pretty
+import qualified Util.Pretty as Pretty
+import Util.Pretty (format)
+
 import qualified Perform.RealTime as RealTime
 
 
 -- Some hand tests for the pretty printer.  Not sure how to automate these...
 
-pprint :: (Pretty a) => a -> IO ()
-pprint = putStrLn . render 30 . format
+pformat :: (Pretty.Pretty a) => a -> IO ()
+pformat = putStrLn . Pretty.render 30 . format
 
 test_list = do
     let max = 10 :: Int
-        pp :: Pretty a => a -> IO ()
+        pp :: (Pretty.Pretty a) => a -> IO ()
         pp val = forM_ [40, 35, 30, 15] $ \width ->
-            putStrLn $ render width $ format val
-    putStrLn $ pretty [0..max]
+            putStrLn $ Pretty.render width $ format val
+    putStrLn $ Pretty.pretty [0..max]
     pp [0..max]
-    -- pp "some string"
 
 test_record = do
     let num = 30 :: Int
-    let f = pprint . record (PP.text "Rec")
+    let f = pformat . Pretty.record (Pretty.text "Rec")
     -- fit on one line
     f [("hi", format "there")]
     f (replicate 3 ("hi", format "there"))
@@ -32,10 +32,10 @@ test_record = do
     f (replicate 2 ("really long label", format "really long data value"))
 
 test_map = do
-    let f = pprint . Map.fromList
-    f [(k, "abcdef") | k <- ['a'..'z']]
+    let f = pformat . Map.fromList
+    f [(k, "abcdef") | k <- ['a'..'d']]
 
 test_tuple = do
-    pprint ("hi", "there", "really long string and stuff")
-    pprint (RealTime.seconds 3, RealTime.seconds 4)
-    putStrLn $ pretty ('a', 'b')
+    pformat ("hi", "there", "really long string and stuff")
+    pformat (RealTime.seconds 3, RealTime.seconds 4)
+    putStrLn $ Pretty.pretty ('a', 'b')
