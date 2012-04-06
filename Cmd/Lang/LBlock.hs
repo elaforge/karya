408306bac955ed662d057326b15d92fe60ec093e
qdunkan <qdunkan@gmail.com>
2012-04-06 05:17:23 +0000

    Cmd.Lang.LBlock.replace replaces calls on control tracks too

--- Cmd/Lang/LBlock.hs
+++ Cmd/Lang/LBlock.hs
@@ -14,13 +14,15 @@ import Types
 --
 -- It doesn't update TrackIds so they may still be named under their old block,
 -- but track id names aren't supposed to carry meaning anyway.
---
--- TODO throughout the entire piece?
 rename :: BlockId -> BlockId -> Cmd.CmdL ()
-rename from to = do
-    Create.rename_block from to
+rename from to = Create.rename_block from to
+
+-- | Rename block calls in a single block.
+replace :: BlockId -> BlockId -> Cmd.CmdL ()
+replace from to = do
     block_id <- Cmd.get_focused_block
-    ModifyEvents.map_note_tracks block_id (Just . replace_block_call from to)
+    ModifyEvents.block_tracks block_id $ \_ events ->
+        return $ Just $ map (replace_block_call from to) events
 
 replace_block_call :: BlockId -> BlockId -> Events.PosEvent -> Events.PosEvent
 replace_block_call from to pevent@(pos, evt)
