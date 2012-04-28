28779f17f4a92adc67b98736c920379a87e4ec26
qdunkan <qdunkan@gmail.com>
2012-04-28 05:16:39 +0000

    add Cmd.Lang.LState.rename_project

--- Cmd/Lang/LState.hs
+++ Cmd/Lang/LState.hs
@@ -1,12 +1,15 @@
--- | Lang cmds providing UI state or block level operations.
+-- | Lang cmds providing general UI state operations.
 module Cmd.Lang.LState where
 import qualified Control.Monad.Trans as Trans
 import qualified System.IO as IO
 
 import qualified Util.PPrint as PPrint
+import qualified Ui.Id as Id
 import qualified Ui.State as State
 import qualified Ui.UiTest as UiTest
+
 import qualified Cmd.Cmd as Cmd
+import qualified Cmd.Create as Create
 import Types
 
 
@@ -22,3 +25,8 @@ save_test_block fname block_id = do
     st <- State.get
     Trans.liftIO $ IO.writeFile fname $
         PPrint.pshow [UiTest.block_to_spec st block_id]
+
+rename_project :: String -> Cmd.CmdL ()
+rename_project ns = case Id.namespace ns of
+    Nothing -> Cmd.throw $ "invalid namespace: " ++ show ns
+    Just ns -> Create.rename_project ns
