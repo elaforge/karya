1bd6d4a673cc0ebffd8ab3161fc000e0e2957c04
qdunkan <qdunkan@gmail.com>
2012-06-11 00:36:00 +0000

    add Cmd.Lang.LState.enable_lilypond

--- Cmd/Lang/LState.hs
+++ Cmd/Lang/LState.hs
@@ -1,6 +1,7 @@
 -- | Lang cmds providing general UI state operations.
 module Cmd.Lang.LState where
 import qualified Control.Monad.Trans as Trans
+import qualified Data.Map as Map
 import qualified System.IO as IO
 
 import qualified Util.PPrint as PPrint
@@ -10,6 +11,7 @@ import qualified Ui.UiTest as UiTest
 
 import qualified Cmd.Cmd as Cmd
 import qualified Cmd.Create as Create
+import qualified Perform.Lilypond.Lilypond as Lilypond
 import Types
 
 
@@ -30,3 +32,17 @@ rename_project :: String -> Cmd.CmdL ()
 rename_project ns = case Id.namespace ns of
     Nothing -> Cmd.throw $ "invalid namespace: " ++ show ns
     Just ns -> Create.rename_project ns
+
+
+-- * lilypond
+
+-- | Turn on lilypond derivation for this block.
+enable_lilypond :: String -> String -> String -> BlockId -> Cmd.CmdL ()
+enable_lilypond clef time_sig dur1 block_id = do
+    State.modify_block_meta block_id $ Map.union $ Map.fromList
+        [ (Lilypond.meta_ly, "true")
+        , (Lilypond.meta_title, Id.ident_string block_id)
+        , (Lilypond.meta_clef, clef)
+        , (Lilypond.meta_time_signature, time_sig)
+        , (Lilypond.meta_duration1, dur1)
+        ]
