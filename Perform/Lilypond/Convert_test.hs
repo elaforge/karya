e4f80ebb86d1f560cea6760029de877cb27ea0c2
qdunkan <qdunkan@gmail.com>
2012-06-22 00:40:25 +0000

    add Perform.Lilypond.Convert_test

---
+++ Perform/Lilypond/Convert_test.hs
@@ -0,0 +1,18 @@
+module Perform.Lilypond.Convert_test where
+import Util.Test
+import qualified Derive.DeriveTest as DeriveTest
+import qualified Derive.LEvent as LEvent
+import qualified Derive.Score as Score
+
+import qualified Perform.Lilypond.Convert as Convert
+import qualified Perform.Lilypond.Lilypond as Lilypond
+
+
+test_convert = do
+    let f quarter = map (fmap extract) . Convert.convert quarter
+            . map (LEvent.Event . mkevent)
+        mkevent (start, dur, pitch) =
+            DeriveTest.mkevent (start, dur, pitch, [], Score.default_inst)
+        extract (Lilypond.Event start dur pitch _) = (start, dur, pitch)
+    equal (f 0.05 [(0, 0.05, "c"), (0.05, 0.1, "d")])
+        [LEvent.Event (0, 32, "d'"), LEvent.Event (32, 64, "ds'")]
