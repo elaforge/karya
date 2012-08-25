f32faf314dfbec082e3c78b3ac96847f034a3846
qdunkan <qdunkan@gmail.com>
2012-08-25 00:24:59 +0000

    fix Cmd.Perf_test

--- Cmd/Perf_test.hs
+++ Cmd/Perf_test.hs
@@ -1,20 +1,20 @@
 module Cmd.Perf_test where
 import Util.Test
+import qualified Ui.UiTest as UiTest
 import qualified Cmd.Cmd as Cmd
 import qualified Cmd.CmdTest as CmdTest
 import qualified Cmd.Perf as Perf
 
-import qualified Derive.PitchSignal as PitchSignal
 import qualified Derive.Scale.Twelve as Twelve
 import qualified Perform.Pitch as Pitch
 
 
 test_note_to_pitch = do
-    let f = Perf.note_to_pitch Twelve.scale_id . Pitch.Note
-    let run = CmdTest.extract (fmap PitchSignal.pitch_nn)
-            . CmdTest.run_tracks []
-    pprint (run (f "4c"))
-    equal (run (f "4c")) $ Right (Just (Right (Right 60)), [])
+    let f = Perf.note_to_pitch Twelve.scale_id UiTest.default_block_id
+            (UiTest.mk_tid 1) 1 . Pitch.Note
+    let run = CmdTest.extract id . CmdTest.run_tracks []
+    equal (run (f "4c")) $ Right (Just (Right 60), [])
+    -- TODO test a fancier scale that retunes?
 
 run :: Cmd.CmdId val -> Either String (Maybe val, [String])
 run = CmdTest.extract id . CmdTest.run_tracks []
