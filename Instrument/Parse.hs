86705931a074d2a5cea70eff0bd09ee868ea72ec
qdunkan <qdunkan@gmail.com>
2012-11-11 02:30:12 +0000

    sysex: remove the old sysex parser

--- Instrument/Parse.hs
+++ Instrument/Parse.hs
@@ -1,20 +1,14 @@
-{- | Functions to help parse MIDI patch files.
--}
+-- | Functions to parse MIDI patch files.
 module Instrument.Parse where
-import Data.Bits as Bits
-import qualified Data.ByteString as ByteString
 import qualified Data.List as List
 import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
-import Data.Word (Word8)
 
 import qualified Numeric
 import qualified Text.ParserCombinators.Parsec as Parsec
 import Text.ParserCombinators.Parsec ((<|>), (<?>))
-import qualified Text.ParserCombinators.Parsec.Pos as Parsec.Pos
 
 import Util.Control hiding ((<|>))
-import qualified Util.File as File
 import qualified Midi.Midi as Midi
 import qualified Derive.Score as Score
 import qualified Perform.Midi.Control as Control
@@ -164,108 +158,3 @@ p_nat = do
         (n, _) : _ -> return n
         _ -> Parsec.pzero -- this should never happen
     <?> "natural int"
-
-
--- * sysex
-
--- | Parse a sysex file as a stream of Word8s.
--- TODO this should be ByteString, but it's surprisingly not easy to do this.
--- Attoparsec doesn't keep track of the byte offset at all so the errors
--- are unhelpful.  Parsec can now read ByteStrings, but it's still Char
--- oriented so I still have to write combinators for bytes, and Parsec.token
--- still wants a 'tok_pos' function, which implies the byte number still has
--- to be in the token stream.
-type ByteParser = Parsec.GenParser (Parsec.Pos.SourcePos, Word8) ()
-
-parse_sysex_dir :: ByteParser Instrument.Patch -> FilePath
-    -> IO [Instrument.Patch]
-parse_sysex_dir parser dir = do
-    fns <- File.list_dir dir
-    warn_parses =<< mapM (parse_sysex_file parser) fns
-
-warn_parses :: [Either Parsec.ParseError a] -> IO [a]
-warn_parses parses = fmap Maybe.catMaybes $ forM parses $ \p -> case p of
-    Left err -> do
-        -- This is only run by make_db, so I guess prints are ok.
-        putStrLn $ "error parsing sysex: " ++ show err
-        return Nothing
-    Right parse -> return (Just parse)
-
-parse_sysex_file :: ByteParser Instrument.Patch -> FilePath
-    -> IO (Either Parsec.ParseError Instrument.Patch)
-parse_sysex_file parser fn = do
-    bytes <- File.read_binary fn
-    return $ add_sysex bytes . Sysex.add_file fn <$> parse_sysex parser fn bytes
-
-parse_sysex :: ByteParser a -> FilePath -> [Word8]
-    -> Either Parsec.ParseError a
-parse_sysex parser fn bytes = Parsec.parse parser fn (annotate bytes)
-    where
-    annotate bytes =
-        [(Parsec.Pos.newPos fn 1 n, byte) | (n, byte) <- zip [1..] bytes]
-
--- | Tack the sysex on to the patch's initialize field.
-add_sysex :: [Word8] -> Instrument.Patch -> Instrument.Patch
-add_sysex bytes patch =
-    patch { Instrument.patch_initialize = make_sysex_init bytes }
-
-make_sysex_init :: [Word8] -> Instrument.InitializePatch
-make_sysex_init bytes = Instrument.InitializeMidi
-        [Midi.CommonMessage (Midi.SystemExclusive manuf rest)]
-    where
-    -- If the msg is broken there's not much I can do here.
-    manuf = bytes !! 1
-    rest = ByteString.pack (drop 2 bytes)
-
-byte_tok :: (Word8 -> Maybe a) -> ByteParser a
-byte_tok f = Parsec.token show_tok tok_pos test_tok
-    where
-    show_tok (_, n) = hex n
-    tok_pos (pos, _) = pos
-    test_tok (_, n) = f n
-
-hex :: (Show a, Integral a) => a -> String
-hex n = Numeric.showHex n ""
-
-byte_sat :: (Word8 -> Bool) -> ByteParser Word8
-byte_sat f = byte_tok $ \b -> if f b then Just b else Nothing
-
-byte :: Word8 -> ByteParser Word8
-byte b = byte_sat (==b) <?> ("byte " ++ hex b)
-
-match_bytes :: [Word8] -> ByteParser ()
-match_bytes = mapM_ byte
-
-n_bytes :: Int -> ByteParser [Word8]
-n_bytes n = Parsec.count n any_byte
-
-one_byte :: ByteParser Word8
-one_byte = fmap head (n_bytes 1)
-
-any_byte :: ByteParser Word8
-any_byte = byte_sat (const True)
-
-start_sysex :: Word8 -> ByteParser Word8
-start_sysex manuf = byte Midi.sox_byte >> byte manuf
-
-end_sysex :: ByteParser ()
-end_sysex = byte Midi.eox_byte >> Parsec.eof
-
-to_eox :: ByteParser [Word8]
-to_eox = Parsec.many (byte_sat (/=Midi.eox_byte))
-
--- * decode sysex bytes
-
-to_string :: [Word8] -> String
-to_string = map (toEnum . fromIntegral)
-
-from_signed_7bit :: Word8 -> Int
-from_signed_7bit b = fromIntegral b .&. 0x3f - fromIntegral b .&. 0x40
-
-from_signed_8bit :: (Integral a) => a -> Int
-from_signed_8bit b = fromIntegral b .&. 0x7f - fromIntegral b .&. 0x80
--- 76543210
--- 10000000 0x80
--- x1000000 0x40
--- x0111111 0x3f
--- x0100000 0x20
