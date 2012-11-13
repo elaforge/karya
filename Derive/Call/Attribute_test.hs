58d3f9206987ffbe30b54816a6022a393998ca46
qdunkan <qdunkan@gmail.com>
2012-11-13 09:16:02 +0000

    fix tests

--- Derive/Call/Attribute_test.hs
+++ Derive/Call/Attribute_test.hs
@@ -10,7 +10,7 @@ test_legato = do
             where
             ((s, d, p), a) = (DeriveTest.e_note2 e, DeriveTest.e_attributes e)
     let (evts, logs) = run
-            [ (">", [(1, 3, "{ .1")])
+            [ (">", [(1, 3, "( .1")])
             , (">", [(n, 1, "") | n <- Seq.range 0 4 1])
             , ("*", [(n, 0, p) | (n, p) <-
                 zip (Seq.range_ 0 1) ["4a", "4b", "4c", "4d", "4e"]])
@@ -26,7 +26,7 @@ test_legato = do
 
     -- Legato events are extended to always overlap.
     let (evts, logs) = run
-            [ (">", [(0, 4, "{ .1")])
+            [ (">", [(0, 4, "( .1")])
             , (">", [(0, 1, ""), (2, 1, ""), (4, 1, "")])
             ]
     equal logs []
