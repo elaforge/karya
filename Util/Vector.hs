281f1c5f833e6783bac29b0c1a4076d7e366189e
qdunkan <qdunkan@gmail.com>
2012-11-22 18:47:45 +0000

    trivial alpha-renaming in Util.Vector

--- Util/Vector.hs
+++ Util/Vector.hs
@@ -1,21 +1,21 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, MultiParamTypeClasses #-}
 -- | Vector utilities.
 module Util.Vector where
-import qualified Data.Vector.Generic as Vector
+import qualified Data.Vector.Generic as Generic
 import qualified Data.Vector.Unboxed as Unboxed
 
 
-count :: (Vector.Vector v a) => (a -> Bool) -> v a -> Int
-count f = Vector.foldl' (\c a -> if f a then succ c else c) 0
+count :: (Generic.Vector v a) => (a -> Bool) -> v a -> Int
+count f = Generic.foldl' (\c a -> if f a then succ c else c) 0
 
-fold_abort :: (Vector.Vector v a) => (accum -> a -> Maybe accum) -> accum
+fold_abort :: (Generic.Vector v a) => (accum -> a -> Maybe accum) -> accum
     -> v a -> accum
 fold_abort f accum vec = go 0 accum
-    where go i accum = maybe accum (go (i+1)) $ f accum =<< vec Vector.!? i
+    where go i accum = maybe accum (go (i+1)) $ f accum =<< vec Generic.!? i
 
 -- | Find the index of the last value whose running sum is still below the
 -- given number.
-find_before :: (Vector.Vector v Int) => Int -> v Int -> Int
+find_before :: (Generic.Vector v Int) => Int -> v Int -> Int
 find_before n = fst . fold_abort go (0, 0)
     where
     go (i, total) a
@@ -23,7 +23,7 @@ find_before n = fst . fold_abort go (0, 0)
         | otherwise = Nothing
 
 bracketing :: Unboxed.Vector Double -> Double -> Maybe (Int, Double, Double)
-bracketing vec a = case Vector.findIndex (>=a) vec of
+bracketing vec a = case Generic.findIndex (>=a) vec of
     Just i
         | Unboxed.unsafeIndex vec i == a -> Just (i, a, a)
         | i > 0 ->
