360bdcb9795251d5df92629062acac37c16dd354
qdunkan <qdunkan@gmail.com>
2012-12-06 02:08:34 +0000

    update Midi.StubMidi to the new signature

--- Midi/StubMidi.hs
+++ Midi/StubMidi.hs
@@ -4,13 +4,17 @@ import qualified Control.Concurrent.STM as STM
 import qualified Data.Time as Time
 
 import qualified Midi.Interface as Interface
+import qualified Midi.Midi as Midi
 import qualified Perform.RealTime as RealTime
 
 
-initialize :: String -> (Either String Interface.Interface -> IO a) -> IO a
-initialize _app_name app = app . Right =<< interface
+initialize :: String
+    -> (Midi.Message -> Bool) -- ^ read msgs that return false are filtered
+    -> (Either String (Interface.RawInterface Midi.WriteMessage) -> IO a)
+    -> IO a
+initialize _app_name _want_message app = app . Right =<< interface
 
-interface :: IO Interface.Interface
+interface :: IO (Interface.RawInterface a)
 interface = do
     chan <- STM.newTChanIO
     return $ Interface.Interface
