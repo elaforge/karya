5b4b8748a309e857526199a36a6253b49ec10932
qdunkan <qdunkan@gmail.com>
2012-12-17 07:52:41 +0000

    soup up App.Send with features to send multiple commands, and time them

--- App/Send.hs
+++ App/Send.hs
@@ -4,20 +4,51 @@
     It's used to talk over the seq_language socket.
 -}
 module App.Send where
+import qualified Control.DeepSeq as DeepSeq
 import Control.Monad
+import qualified Data.Time as Time
+import qualified System.Console.GetOpt as GetOpt
 import qualified System.Environment as Environment
+import qualified Text.Printf as Printf
 
-import qualified Util.Seq as Seq
 import qualified App.SendCmd as SendCmd
 
+
+data Flag = Timing
+    deriving (Eq, Show)
+
+options :: [GetOpt.OptDescr Flag]
+options =
+    [ GetOpt.Option ['t'] [] (GetOpt.NoArg Timing) "show cmd completion time"
+    ]
 
 main :: IO ()
 main = SendCmd.initialize $ do
     args <- Environment.getArgs
-    msg <- if null args
-        then getContents
-        else return $ Seq.join " " args ++ "\n"
-    putStrLn $ "sending " ++ show msg
-    response <- SendCmd.send msg
-    unless (null response) $
-        putStrLn response
+    (flags, args) <- case GetOpt.getOpt GetOpt.Permute options args of
+        (flags, args, []) -> return (flags, args)
+        (_, _, errs) -> error $ "flag errors:\n" ++ unlines errs
+    print flags
+    msgs <- if null args
+        then fmap lines getContents
+        else return [unwords args]
+    forM_ msgs $ \msg -> do
+        putStrLn $ "send " ++ show msg
+        if Timing `elem` flags then do
+            (response, time) <- timed $ SendCmd.send (msg ++ "\n")
+            Printf.printf "%s - %.3f\n" response time
+        else do
+            response <- SendCmd.send (msg ++ "\n")
+            unless (null response) $
+                putStrLn response
+
+timed :: IO String -> IO (String, Double)
+timed action = do
+    start <- now
+    result <- action
+    () <- return $ DeepSeq.rnf result
+    end <- now
+    return (result, realToFrac (end - start))
+
+now :: IO Time.DiffTime
+now = fmap Time.utctDayTime Time.getCurrentTime
