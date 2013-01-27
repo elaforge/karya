62d267523ba4d9b4f6b638e08813029decc2056c
qdunkan <qdunkan@gmail.com>
2013-01-27 23:47:20 +0000

    add *_full variants to Util.Debug

--- Util/Debug.hs
+++ Util/Debug.hs
@@ -1,13 +1,14 @@
 module Util.Debug (
     -- * forced by evaluation
-    trace, tracep, traces, traceps
+    trace, tracep, tracep_full, traces, traceps
     , tracef, trace_ret
     -- * forced by monad
-    , traceM, tracepM, tracesM
+    , traceM, tracepM, tracepM_full, tracesM
     -- in IO
-    , puts, put, putp
+    , puts, put, putp, putp_full
 ) where
 import qualified Control.Monad.Trans as Trans
+import qualified Data.Monoid as Monoid
 import qualified Debug.Trace as Trace
 import qualified System.IO as IO
 
@@ -25,6 +26,12 @@ trace msg val = Trace.trace (with_msg msg (pshow val)) val
 -- | Pretty print the returned value.
 tracep :: (Pretty.Pretty a) => String -> a -> a
 tracep msg val = Trace.trace (with_msg msg (Pretty.formatted val)) val
+
+tracep_full :: (Pretty.Pretty a, Monoid.Monoid a, Eq a) =>
+    String -> a -> a
+tracep_full msg val
+    | val == Monoid.mempty = val
+    | otherwise = tracep msg val
 
 -- | Print a string.
 traces :: String -> a -> a
@@ -55,6 +62,12 @@ traceM msg val = Trace.trace (with_msg msg (pshow val)) (return ())
 tracepM :: (Pretty.Pretty a, Monad m) => String -> a -> m ()
 tracepM msg val = Trace.trace (with_msg msg (Pretty.formatted val)) (return ())
 
+tracepM_full :: (Pretty.Pretty a, Monoid.Monoid a, Eq a, Monad m) =>
+    String -> a -> m ()
+tracepM_full msg val
+    | val == Monoid.mempty = return ()
+    | otherwise = tracepM msg val
+
 tracesM :: (Monad m) => String -> m ()
 tracesM msg = Trace.trace msg (return ())
 
@@ -69,6 +82,13 @@ put msg = put_line . with_msg msg . pshow
 
 putp :: (Trans.MonadIO m, Pretty.Pretty a) => String -> a -> m ()
 putp msg = put_line . with_msg msg . Pretty.formatted
+
+-- | Put, but only if it's not mempty.
+putp_full :: (Trans.MonadIO m, Pretty.Pretty a, Monoid.Monoid a, Eq a) =>
+    String -> a -> m ()
+putp_full msg val
+    | val == Monoid.mempty = return ()
+    | otherwise = putp msg val
 
 put_line :: (Trans.MonadIO m) => String -> m ()
 put_line s = Trans.liftIO $ do
