62758c967da82c8edf8f21f966f58488cf7fbd71
qdunkan <qdunkan@gmail.com>
2013-04-23 17:27:58 +0000

    add Util.LazyVector
    I'm not using it yet and may never use it, but at least it's in source
    control now.

---
+++ Util/LazyVector.hs
@@ -0,0 +1,148 @@
+{-# LANGUAGE FlexibleContexts, MultiParamTypeClasses, RankNTypes #-}
+{-# LANGUAGE BangPatterns, GeneralizedNewtypeDeriving #-}
+module Util.LazyVector (
+    Lazy, lazyChunks, toStrict
+    -- * builder
+    , Builder, build, singleton, fromList
+) where
+import qualified Control.Monad.ST as ST
+import qualified Control.Monad.ST.Unsafe as ST.Unsafe
+import qualified Data.Monoid as Monoid
+import qualified Data.Vector as Vector
+import qualified Data.Vector.Mutable as Mutable
+
+import qualified Util.Pretty as Pretty
+
+
+newtype Lazy a = Lazy [Vector.Vector a]
+    deriving (Show, Pretty.Pretty)
+
+instance Functor Lazy where
+    fmap f (Lazy chunks) = Lazy (map (Vector.map f) chunks)
+
+lazyChunks :: Lazy a -> [Vector.Vector a]
+lazyChunks (Lazy chunks) = chunks
+
+toStrict :: Lazy a -> Vector.Vector a
+toStrict = Monoid.mconcat . lazyChunks
+
+-- cons :: Vector.Vector a -> Lazy a -> Lazy a
+-- cons v (Lazy vs)
+--     | Vector.null v = Lazy vs
+--     | otherwise = Lazy (v : vs)
+
+-- * builder
+
+newtype Builder a = Builder {
+    -- Invariant: The lists include no null Vectors.
+    runBuilder :: forall s. (Buffer s a -> ST.ST s [Vector.Vector a])
+         -> Buffer s a -> ST.ST s [Vector.Vector a]
+    }
+
+instance (Show a) => Show (Builder a) where
+    show = show . build
+
+instance (Pretty.Pretty a) => Pretty.Pretty (Builder a) where
+    format = Pretty.format . build
+
+instance Monoid.Monoid (Builder a) where
+    mempty = empty
+    mappend = append
+
+build :: Builder a -> Lazy a
+build builder = Lazy $ ST.runST $ do
+    buf <- newBuffer chunkSize
+    runBuilder (builder `append` flush) (const (return [])) buf
+
+empty :: Builder a
+empty = Builder (\cont buf -> cont buf)
+
+append :: Builder a -> Builder a -> Builder a
+append (Builder f) (Builder g) = Builder (f . g)
+
+singleton :: a -> Builder a
+singleton val = writeN 1 $ \mvec offset ->
+    Mutable.unsafeWrite mvec offset val
+
+fromList :: [a] -> Builder a
+fromList xs = Builder build
+    where
+    build cont (Buffer mvec offset left) = loop mvec offset left xs
+        where
+        loop !mvec !offset !left [] = cont (Buffer mvec offset left)
+        loop mvec offset left (x:xs)
+            | left == 0 = do
+                chunk <- Vector.unsafeFreeze mvec
+                mvec <- Mutable.new chunkSize
+                chunks <- ST.Unsafe.unsafeInterleaveST $
+                    loop mvec 0 chunkSize (x:xs)
+                return $! chunk : chunks
+            | otherwise = do
+                Mutable.unsafeWrite mvec offset x
+                loop mvec (offset+1) (left-1) xs
+
+flush :: Builder a
+flush = Builder f
+    where
+    f cont buf@(Buffer mvec used left)
+        | used == 0 = cont buf
+        | otherwise = do
+            vec <- Vector.unsafeFreeze mvec
+            -- TODO why is it I can use mvec after freezing?
+            -- I think because the offset writes after the frozen point.
+            -- But in any case, ensureFree calls const newBuffer afterwards.
+            -- Why not do the newBuffer in flush?
+            let !buf2 = Buffer mvec used left
+                -- The buffer may have unused space on the end, so only take
+                -- the part that has data.
+                !chunk = Vector.take used vec
+            -- Text uses inlineInterleaveST and says it's faster
+            chunks <- ST.Unsafe.unsafeInterleaveST (cont buf2)
+            return $! chunk : chunks
+
+
+-- ** buffer
+
+data Buffer s a = Buffer
+    {-# UNPACK #-} !(Mutable.STVector s a)
+    {-# UNPACK #-} !Int -- elements used
+    {-# UNPACK #-} !Int -- space left
+
+writeAtMost :: Int -> (forall s. Mutable.MVector s a -> Int -> ST.ST s Int)
+    -> Builder a
+writeAtMost n f = ensureFree n `append` withBuffer (writeBuffer f)
+
+writeN :: Int -> (forall s. Mutable.MVector s a -> Int -> ST.ST s ())
+    -> Builder a
+writeN n f = writeAtMost n (\mvec offset -> f mvec offset >> return n)
+
+ensureFree :: Int -> Builder a
+ensureFree !n = withSize $ \left ->
+    if n <= left then empty
+        else flush `append` withBuffer (const (newBuffer (max n chunkSize)))
+
+writeBuffer :: (Mutable.MVector s a -> Int -> ST.ST s Int) -> Buffer s a
+    -> ST.ST s (Buffer s a)
+writeBuffer f (Buffer mvec used left) = do
+    written <- f mvec used
+    return $! Buffer mvec (used + written) (left - written)
+
+withBuffer :: (forall s. Buffer s a -> ST.ST s (Buffer s a)) -> Builder a
+withBuffer f = Builder $ \cont buf -> cont =<< f buf
+
+withSize :: (Int -> Builder a) -> Builder a
+withSize f = Builder $ \cont buf@(Buffer _ _ left) ->
+    runBuilder (f left) cont buf
+
+newBuffer :: Int -> ST.ST s (Buffer s a)
+newBuffer size = do
+    mvec <- Mutable.new size
+    return $! Buffer mvec 0 size
+
+-- 8 bytes for an int, so 4k = 512 elts, 1k = 128 elts
+chunkSize :: Int
+chunkSize = 128 - chunkOverhead
+
+-- | Overhead of one chunk, in words.
+chunkOverhead :: Int
+chunkOverhead = 4 -- not sure what MVector is, but should be this more or less
---
+++ Util/LazyVector_test.hs
@@ -0,0 +1,24 @@
+module Util.LazyVector_test where
+import Util.Control
+import Util.Test
+import qualified Util.LazyVector as LazyVector
+import qualified Data.Vector as Vector
+import qualified Test.QuickCheck as Q
+
+
+-- property: if you construct out of mixed singletons and fromLists, it will
+-- always be the same as using list and (++).
+
+test_builder = do
+    let f = Vector.toList . LazyVector.toStrict . LazyVector.build
+        chunks = map Vector.toList . LazyVector.lazyChunks . LazyVector.build
+    let s = LazyVector.singleton
+        list = LazyVector.fromList
+    equal (f (s 1)) [1]
+    equal (f (s 1 <> s 2)) [1, 2]
+    equal (f (s 1 <> s 2 <> list [3, 4, 5])) [1..5]
+    equal (f (s 1 <> s 2 <> list [3..1024])) [1..1024]
+    equal (f ((mempty :: LazyVector.Builder Int) <> mempty)) []
+
+    equal (chunks (mempty <> s 1 <> s 2 <> mempty)) [[1, 2]]
+    equal (chunks (s 1 <> mempty <> s 2)) [[1, 2]]
