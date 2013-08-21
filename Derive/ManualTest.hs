be8631d80fceca3db1728872d896bc1a14b52431
qdunkan <qdunkan@gmail.com>
2013-08-21 06:57:35 +0000

    add Derive.ManualTest

---
+++ Derive/ManualTest.hs
@@ -0,0 +1,27 @@
+-- Copyright 2013 Evan Laforge
+-- This program is distributed under the terms of the GNU General Public
+-- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
+
+-- | Skeleton for one-off tests, presumably using the output of
+-- LDebug.dump_block.
+module Derive.ManualTest where
+import Util.Test
+import qualified Ui.UiTest as UiTest
+import qualified Derive.DeriveTest as DeriveTest
+import qualified Local.Instrument.Kontakt as Kontakt
+
+
+run :: IO ()
+run = do
+    let res = DeriveTest.derive_dump dump (UiTest.bid "thani-s")
+        (pevents, msgs, logs) = DeriveTest.perform_dump synths dump res
+    prettyp $ DeriveTest.extract id res
+    prettyp pevents
+    prettyp msgs
+    -- prettyp logs
+
+synths = Kontakt.synth_descs
+
+
+dump :: UiTest.Dump
+dump = undefined
