25c59f1e5f9128b1e12db370928ac64f64670313
qdunkan <qdunkan@gmail.com>
2013-08-31 18:03:08 +0000

    fix BaliScales_test, broken by Wayang changes

--- Derive/Scale/BaliScales_test.hs
+++ Derive/Scale/BaliScales_test.hs
@@ -12,9 +12,9 @@ test_ombak = do
     let run trans pitch = DeriveTest.extract Score.initial_nn $
             DeriveTest.derive_tracks
                 [("> " ++ trans, [(0, 1, "")]), ("*wayang", [(0, 0, pitch)])]
-    equal (run "" "1") ([Just 67.57], [])
-    equal (run "" "tuning = 'umbang' | 1") ([Just 67.57], [])
-    equal (run "" "tuning = 'isep' | 1") ([Just 67.26], [])
+    equal (run "" "3e") ([Just 67.57], [])
+    equal (run "" "tuning = 'umbang' | 3e") ([Just 67.57], [])
+    equal (run "" "tuning = 'isep' | 3e") ([Just 67.26], [])
 
-    equalf 0.01 (run "| %ombak = 5" "1") ([Just 67.306], [])
-    equalf 0.01 (run "| %ombak = 10" "1") ([Just 67.198], [])
+    equalf 0.01 (run "| %ombak = 5" "3e") ([Just 67.306], [])
+    equalf 0.01 (run "| %ombak = 10" "3e") ([Just 67.198], [])
