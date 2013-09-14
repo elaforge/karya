804f7567826fe9ba3e6c95428af4cef9854838c0
qdunkan <qdunkan@gmail.com>
2013-09-14 20:02:14 +0000

    forgot to add Derive.Scale.Util_test

---
+++ Derive/Scale/Util_test.hs
@@ -0,0 +1,60 @@
+module Derive.Scale.Util_test where
+import Util.Control
+import Util.Test
+import qualified Derive.Scale.Theory as Theory
+import qualified Derive.Scale.Util as Util
+
+
+test_ascii_kbd_pitch = do
+    let f per_oct oct pc = extract $ Util.ascii_kbd_pitch per_oct $
+            Theory.Pitch oct (Theory.Note pc 0)
+        extract (Theory.Pitch oct (Theory.Note pc _)) = (oct, pc)
+    -- Octave smaller than the row.
+    equal [f 5 0 pc | pc <- [0..9]]
+        [ (0, 0), (0, 1), (0, 2), (0, 3), (0, 4)
+        , (1, 0), (1, 1), (1, 2), (1, 3), (1, 4)
+        ]
+    equal [f 5 1 pc | pc <- [0..9]]
+        [ (1, 0), (1, 1), (1, 2), (1, 3), (1, 4)
+        , (2, 0), (2, 1), (2, 2), (2, 3), (2, 4)
+        ]
+
+    -- Octave larger than the row.
+    equal [f 12 0 pc | pc <- [0..9]]
+        [ (0, 0), (0, 1), (0, 2), (0, 3), (0, 4)
+        , (0, 5), (0, 6), (0, 7), (0, 8), (0, 9)
+        ]
+    equal [f 12 1 pc | pc <- [0..9]]
+        [ (0, 10), (0, 11), (1, 0), (1, 1), (1, 2)
+        , (1, 3), (1, 4), (1, 5), (1, 6), (1, 7)
+        ]
+
+test_piano_kbd_pitch = do
+    let f per_oct oct pc = extract <$> Util.piano_kbd_pitch 0 per_oct
+            (Theory.Pitch oct (Theory.Note pc 0))
+        extract (Theory.Pitch oct (Theory.Note pc _)) = (oct, pc)
+    -- Octave smaller than the row.
+    equal (map (f 5 0) [0..6])
+        [ Just (0, 0), Just (0, 1), Just (0, 2), Just (0, 3), Just (0, 4)
+        , Nothing, Nothing
+        ]
+    equal (map (f 5 1) [0..6])
+        [ Just (1, 0), Just (1, 1), Just (1, 2), Just (1, 3), Just (1, 4)
+        , Nothing, Nothing
+        ]
+
+    -- Octave larger than the row.
+    equal (map (f 8 0) [0..6])
+        [ Just (0, 0), Just (0, 1), Just (0, 2), Just (0, 3), Just (0, 4)
+        , Just (0, 5), Just (0, 6)
+        ]
+    equal (map (f 8 1) [0..6])
+        [Just (0, 7), Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
+
+
+--    C D E F G A B|C D E F G A B
+-- C  1 2 3 4 5 / / 1 2 3 4 5 / /
+-- D  / 1 2 3 4 5 / / 1 2 3 4 5 /
+--    5 1 2 3 4 / /
+-- E / / 1 2 3 4 5 / / 1 2 3 4 5
+--
