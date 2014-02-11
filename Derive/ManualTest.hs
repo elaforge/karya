edb4f3683587c26e642310c7ae8e19efee48ebd8
qdunkan <qdunkan@gmail.com>
2014-02-11 09:16:40 +0000

    more example code in Derive.ManualTest

--- Derive/ManualTest.hs
+++ Derive/ManualTest.hs
@@ -5,10 +5,24 @@
 -- | Skeleton for one-off tests, presumably using the output of
 -- LDebug.dump_block.
 module Derive.ManualTest where
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+import Util.Control
+import qualified Util.Log as Log
+import qualified Util.Ranges as Ranges
 import Util.Test
+
 import qualified Ui.UiTest as UiTest
+import qualified Cmd.Cmd as Cmd
+import qualified Cmd.PlayUtil as PlayUtil
 import qualified Cmd.Simple as Simple
+
+import qualified Derive.Cache_test as Cache_test
+import qualified Derive.Derive as Derive
+import qualified Derive.DeriveSaved as DeriveSaved
 import qualified Derive.DeriveTest as DeriveTest
+
 import qualified Local.Instrument.Kontakt as Kontakt
 
 
@@ -25,3 +39,26 @@ synths = Kontakt.synth_descs
 
 dump :: Simple.State
 dump = undefined
+
+
+-- * cache
+
+run_cached = do
+    cmd_config <- DeriveSaved.load_cmd_config
+    let cstate = Cmd.initial_state cmd_config
+    state <- expect_right "load" <$> DeriveSaved.load_score "bug/cache"
+    let root = UiTest.bid "kendang-telek/telek1"
+        tempot = UiTest.tid "kendang-telek/telek1.t1"
+    let (uncached, logs) = expect_right "derive" $
+            DeriveSaved.derive_block state cstate root
+    mapM_ Log.write logs
+
+    let cache = Derive.r_cache uncached
+        damage = Derive.ScoreDamage
+            (Map.fromList [(tempot, Ranges.range 200 200)])
+            (Set.fromList [root]) mempty
+    let (cached, logs) = expect_right "cache derive" $
+            DeriveSaved.run_cmd state cstate $
+            PlayUtil.derive cache damage root
+    mapM_ Log.write logs
+    prettyp (Cache_test.r_block_logs cached)
