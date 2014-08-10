50d4ff53a92c50bb060e385fce0cc7b7ec85a5e7
Evan Laforge <qdunkan@gmail.com>
2014-08-10 06:22:55 +0000

    avoid excessive quoting in error msg

--- Cmd/MidiThru.hs
+++ Cmd/MidiThru.hs
@@ -140,11 +140,10 @@ map_scale patch_scale scale environ input = case input of
             Scale.scale_input_to_nn scale pos input
         -- I ignore _logs, any interesting errors should be in 'result'.
         case result of
-            Left err -> Cmd.throw $
-                "error deriving input key's nn: " ++ show err
-            Right (Left err) -> Cmd.throw $
-                "error deriving input key's nn: " ++ pretty err
+            Left err -> throw err
+            Right (Left err) -> throw $ pretty err
             Right (Right nn) -> return $ map_patch_scale patch_scale nn
+        where throw = Cmd.throw .  ("error deriving input key's nn: " <>)
 
 map_patch_scale :: Instrument.PatchScale -> Pitch.NoteNumber
     -> Maybe Pitch.NoteNumber
