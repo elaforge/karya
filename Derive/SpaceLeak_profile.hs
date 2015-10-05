f2572bf85255508bc525ca059ee7b79581bfafb9
qdunkan <qdunkan@gmail.com>
2015-10-05 02:37:31 +0000

    add Derive.SpaceLeak_profile
    I still don't know how to use it to find a space leak, but maybe I will
    if I mess with it long enough.

---
+++ Derive/SpaceLeak_profile.hs
@@ -0,0 +1,27 @@
+-- Copyright 2015 Evan Laforge
+-- This program is distributed under the terms of the GNU General Public
+-- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
+
+-- | Try to find space leaks in specific functions.
+module Derive.SpaceLeak_profile where
+import qualified Util.Seq as Seq
+import Util.Test
+import qualified Derive.Call.Post as Post
+import qualified Derive.Call.Post.Postproc as Postproc
+import qualified Derive.DeriveTest as DeriveTest
+import qualified Derive.Score as Score
+import qualified Derive.Stream as Stream
+
+import Global
+
+
+profile_cancel = do
+    let f = Postproc.group_and_cancel
+            (Postproc.cancel_strong_weak Postproc.merge_infer) Post.hand_key 2
+        make = Stream.from_sorted_events . map DeriveTest.mkevent
+        inst = Score.Instrument "i1"
+    let events = make [(s, 1, "4c", [], inst) | s <- Seq.range 0 (1024 * 50) 1]
+    -- With force, run with -K2905K
+    force events
+    print_timer "force" (\_ _ val -> "done: " <> val) $
+        return $! either show (show . length . Stream.to_list) $ f events
