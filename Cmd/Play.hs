0707b02a8986794dab3dcbcdf31c9f88ddda30ac
qdunkan <qdunkan@gmail.com>
2016-02-21 04:29:04 +0000

    simplify haddock in Cmd.Play a bit
    I can be a bit more concrete about the specific threads in use.

--- Cmd/Play.hs
+++ Cmd/Play.hs
@@ -8,34 +8,32 @@
 
     - Find the relevant block to play.
 
-    - Deriver generates a performable Score and an inverse tempo map.
+    - Deriver generates a performable score and an inverse tempo map.
 
-    - The Score is preprocessed by adding the current absolute time to it
+    - The score is preprocessed by adding the current absolute time to it
     and skipping notes based on the start offset.
 
-    - The Score is sent to the Performer, which splits it up by backend,
-    starts whatever processes are necessary to play the notes, and returns
-    a transport control mutable that can be used to stop the playback.
+    - The score is sent to the Performer, which starts a MIDI play thread
+    and returns a transport control mutable that can be used to stop the
+    play thread.
 
     - The transport and tempo map are passed to a play monitor thread, which
     uses the tempo map to display the play position in the various blocks, and
-    aborts along with the performer if the transport says to stop.  It's not
-    synchronized to the playback in any way (which may be a separate process
-    playing an audio file), but the fact that it's working from the same tempo
-    map that generated the audio should keep it in sync.
+    aborts along with the player if the transport says to stop.  It's not
+    synchronized to the playback in any way, but of course they are both
+    working from the same score.
 
-    - There are three threads involved: the performer manages whatever process
-    needs to perform the score, the play monitor sweeps the play position
-    along, and the app event handler is waiting for events in the responder.
+    - There are three threads involved: the player is scheduling MIDI msgs, the
+    play monitor sweeps the play position along, and the app event handler is
+    waiting for events in the responder.
 
-    - On cancel, kill the thread, and invoke a backend specific means to cancel
-    outstanding notes (flush midi port, kill external performer, ...).
+    - A stop means to set 'Transport.stop_player'.  The play thread will notice
+    this and quit.
 
     The player returns controls to communicate with the player and the play
-    monitor.  If the responder sets the player control, the player will quit.
-    The player stopping causes it to set the 'Transport.PlayMonitorControl',
-    which causes the monitor to quit (if there are multiple players, the
-    monitor should wait for them all to quit).
+    monitor.  If the responder sets the stop player control, the player will
+    quit.  The player stopping causes it to set the
+    'Transport.PlayMonitorControl', which causes the monitor to quit.
 
     There's a third control, which is a channel given to the player by the
     responder.  Both the player and the monitor use it to send transport msgs
@@ -44,36 +42,31 @@
     'cmd_play_msg', which can use them to set UI state like changing the
     play box color and logging.
 
-    The play monitor is kicked off simultaneously with the performer, and
+    The play monitor is kicked off simultaneously with the player, and
     advances the play selection in its own loop, using the tempo map from the
     deriver.  It will keep running until it gets a stop msg from the control or
     the tempo map tells it there is no more score to \"play\".  While the
     monitor doesn't actually play anything, it's the one that sends Playing and
-    Stopped transport msgs to indicate performer status.  This is because there
-    may be multiple simultaneous performers that may complete at different
-    times and the monitor will only emit Stopped if all of them have finished.
-    If all goes well, the monitor and the performer will run to completion, the
-    monitor will send Stopped, and the performer will exit on its own.
-
-    With multiple backends, there will be multiple monitor controls, and the
-    monitor will need to monitor them all.
+    Stopped transport msgs to indicate player status.  If all goes well, the
+    monitor and the player will run to completion, the monitor will send
+    Stopped, and the player will exit on its own.
 
     For example:
 
-    In a normal situation, the performer will do its thing and the monitor will
-    eventually run out of InverseTempoMap (which will return Nothing).  The
+    In a normal situation, the player will do its thing and the monitor will
+    eventually run out of InverseTempoMap, which will return Nothing.  The
     monitor will send Stopped, which will clear the player control from the
     responder Cmd.State, which is how the UI knows whether playing is in
-    progress.  The performer is assumed to have completed and exited on its
-    own, probably even before the playback audio is completed, since it likely
+    progress.  The player is assumed to have completed and exited on its own,
+    probably even before the playback audio is completed, since it likely
     schedules in advance.
 
-    If the performer dies on an error, it sends a Died to the responder chan.
-    As mentioned above, it will also tell the monitor to stop.  The monitor
-    will notice this, and may stop itself, emitting a Stopped msg.  The Stopped
-    msg will then cause the responder to clear the player control out of its
-    state, which lets it know that play has stopped and it's ok to start
-    another play thread.
+    If the player dies on an error, it sends a Died to the responder chan.  As
+    mentioned above, it will also tell the monitor to stop.  The monitor will
+    notice this, and may stop itself, emitting a Stopped msg.  The Stopped msg
+    will then cause the responder to clear the player control out of its state,
+    which lets it know that play has stopped and it's ok to start another play
+    thread.
 
     If the user requests a stop, the responder sets the player control to Stop.
     The player stops, telling the monitor to stop, which emits Stopped, which
