b17cbac92215e541afdd85d200e7d6b2fbd9b5a4
qdunkan <qdunkan@gmail.com>
2016-09-03 01:41:27 +0000

    fltk: make Event immutable

--- fltk/Event.h
+++ fltk/Event.h
@@ -20,12 +20,7 @@
 #include "util.h"
 
 
-// Events are immutable, but as usual I can't use const since I assign them by
-// value into STL containers.
 struct Event {
-    // This has a default contsructor so I can assign it by value into
-    // the EventTrackModel::Events map.
-    Event() : start(0), duration(0), style_id(0) {}
     Event(ScoreTime start, ScoreTime duration, const char *text,
             StyleId style_id, bool align_to_bottom = false) :
         start(start), duration(duration), text(text), style_id(style_id)
@@ -37,12 +32,12 @@ struct Event {
     ScoreTime min() const { return std::min(start, start + duration); }
     ScoreTime max() const { return std::max(start, start + duration); }
 
-    ScoreTime start;
-    ScoreTime duration;
+    const ScoreTime start;
+    const ScoreTime duration;
     // std::string would be nicer but I can't serialize to that from haskell.
     // This won't be modified, but will be freed, so const_cast for that.
     const char *text;
-    StyleId style_id;
+    const StyleId style_id;
 };
 
 inline std::ostream &
