9295aef0e9014831b3a076c89425eb7ec1b89007
qdunkan <qdunkan@gmail.com>
2017-02-19 05:59:29 +0000

    replace bundle-website.sh with upload-documentation.sh

--- tools/bundle-website.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-# Bundle up the documentation to upload to the website.
-# data is huge, no need to re-upload if it hasn't changed.
-tar -L --exclude build/doc/data -czvf website.tgz build/{doc,haddock,hscolour}
