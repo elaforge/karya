4834fc42114d4e3de9cf38f1d7a8b4c6ea8a7737
qdunkan <qdunkan@gmail.com>
2017-03-06 08:00:56 +0000

    have tools/upload.sh sync ../save as well

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 # Upload the documentation to upload to the website.
-rsync --verbose -r --copy-dirlinks --delete build/{doc,haddock,hscolour} \
+rsync --verbose -r --copy-dirlinks --delete build/{doc,haddock,hscolour} save \
     ofb.net:public_html/karya
