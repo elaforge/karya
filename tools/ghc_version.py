bb6797a3e7736461961d1728992448ba4a616bbd
qdunkan <qdunkan@gmail.com>
2017-03-06 07:31:29 +0000

    make mkmk set GHC_VERSION, since Util.CallStack needs it

---
+++ tools/ghc_version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# Get the GHC version.  This imitates 'Shake.Shakefile.parseGhcVersion'.
+
+import subprocess, os, re
+
+path = subprocess.check_output(['ghc', '--print-libdir']).decode('utf8').strip()
+path = os.path.split(path)[1]
+path = path.split('-', 1)[1]
+path = ''.join('0' * (2 - len(c)) + c for c in path.split('.'))
+path = path.lstrip('0')[:5]
+print(path)
