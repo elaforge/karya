2ad92e32644af05a716bb6c5d043b52804c1789e
qdunkan <qdunkan@gmail.com>
2017-04-01 02:55:52 +0000

    add a test to make sure back-to-back 0 dur control events work as expected

--- Derive/Control_test.hs
+++ Derive/Control_test.hs
@@ -8,9 +8,9 @@ import qualified Data.Map as Map
 import qualified Util.Seq as Seq
 import Util.Test
 import qualified Ui.Events as Events
-import qualified Ui.Ui as Ui
 import qualified Ui.Track as Track
 import qualified Ui.TrackTree as TrackTree
+import qualified Ui.Ui as Ui
 import qualified Ui.UiTest as UiTest
 
 import qualified Derive.Call as Call
@@ -29,58 +29,74 @@ import Global
 import Types
 
 
-do_derive :: (Score.Event -> a) -> UiTest.TrackSpec -> ([a], [String])
-do_derive extract track = DeriveTest.extract extract $
+derive :: (Score.Event -> a) -> UiTest.TrackSpec -> ([a], [String])
+derive extract track = DeriveTest.extract extract $
     DeriveTest.derive_tracks "" [(">", [(0, 8, "")]), track]
 
 test_control_track = do
-    let derive = do_derive (DeriveTest.e_control "cont")
+    let run = derive (DeriveTest.e_control "cont")
     let events = [(0, 0, "1"), (1, 0, "2")]
 
     -- various failures
-    let (val, logs) = derive ("*bork *bork *bork", events)
+    let (val, logs) = run ("*bork *bork *bork", events)
     equal val []
     strings_like logs ["track title: control track must be one of"]
 
-    strings_like (snd (derive ("cont", [(0, 0, "abc"), (1, 0, "def")])))
+    strings_like (snd (run ("cont", [(0, 0, "abc"), (1, 0, "def")])))
         ["not found: abc", "not found: def"]
-    equal (derive ("cont", events)) ([[(0, 1), (1, 2)]], [])
+    equal (run ("cont", events)) ([[(0, 1), (1, 2)]], [])
+
+test_back_to_back_controls = do
+    let run events = DeriveTest.extract (DeriveTest.e_control "c") $
+            DeriveTest.derive_tracks ""
+                [(">", [(0, 2, ""), (2, 2, "")]), ("c", events)]
+    equal (run [(0, 0, "0"), (2, 0, "i 2")]) ([[(0, 0), (1, 1)], [(2, 2)]], [])
+    equal (run [(0, 0, "0"), (2, -0, "i 2"), (2, 0, "4")])
+        ([[(0, 0), (1, 1)], [(2, 4)]], [])
+
+test_back_to_back_pitches = do
+    let run events = DeriveTest.extract DeriveTest.e_nns $
+            DeriveTest.derive_tracks ""
+                [(">", [(0, 2, ""), (2, 2, "")]), ("*", events)]
+    equal (run [(0, 0, "4c"), (2, 0, "i (4d)")])
+        ([[(0, NN.c4), (1, NN.cs4)], [(2, NN.d4)]], [])
+    equal (run [(0, 0, "4c"), (2, -0, "i (4d)"), (2, 0, "4f")])
+        ([[(0, NN.c4), (1, NN.cs4)], [(2, NN.f4)]], [])
 
 test_hex = do
-    let derive events =
-            do_derive (DeriveTest.e_control "cont") ("cont", events)
-    equal (derive [(0, 0, "`0x`ff"), (1, 0, "`0x`33"), (2, 0, "`0x`00")])
+    let run events = derive (DeriveTest.e_control "cont") ("cont", events)
+    equal (run [(0, 0, "`0x`ff"), (1, 0, "`0x`33"), (2, 0, "`0x`00")])
         ([[(0, 1), (1, 0.2), (2, 0)]], [])
 
 test_track_expression = do
-    let derive = do_derive (DeriveTest.e_control "cont")
-    equal (derive ("cont", [(0, 0, "0"), (4, 0, "i 1")]))
+    let run = derive (DeriveTest.e_control "cont")
+    equal (run ("cont", [(0, 0, "0"), (4, 0, "i 1")]))
         ([[(0, 0), (1, 0.25), (2, 0.5), (3, 0.75), (4, 1)]], [])
-    equal (derive ("cont | srate = 2", [(0, 0, "0"), (4, 0, "i 1")]))
+    equal (run ("cont | srate = 2", [(0, 0, "0"), (4, 0, "i 1")]))
         ([[(0, 0), (2, 0.5), (4, 1)]], [])
 
-    let derive_pitch = do_derive DeriveTest.e_nns
-    equal (derive_pitch ("*twelve | srate = 2",
+    let run_pitch = derive DeriveTest.e_nns
+    equal (run_pitch ("*twelve | srate = 2",
             [(0, 0, "4c"), (4, 0, "i (4d)")]))
         ([[(0, 60), (2, 61), (4, 62)]], [])
 
 test_derive_control = do
     let ex (sig, logs) = (Signal.unsignal sig, map DeriveTest.show_log logs)
-    let derive events = DeriveTest.extract_run ex $
+    let run events = DeriveTest.extract_run ex $
             DeriveTest.run Ui.empty $ Derive.with_default_imported $
             Control.derive_control False (mktrack 10 events) id
-    equal (derive [(0, 0, "1"), (1, 0, "2")])
+    equal (run [(0, 0, "1"), (1, 0, "2")])
         (Right ([(0, 1), (1, 2)], []))
-    equal (derive [(0, 0, "1"), (2, 0, "i 2")])
+    equal (run [(0, 0, "1"), (2, 0, "i 2")])
         (Right ([(0, 1), (1, 1.5), (2, 2)], []))
-    equal (derive [(0, 0, "1"), (2, 0, "i 2"), (4, 0, "i 1")])
+    equal (run [(0, 0, "1"), (2, 0, "i 2"), (4, 0, "i 1")])
         (Right ([(0, 1), (1, 1.5), (2, 2), (3, 1.5), (4, 1)], []))
 
     -- evaluation continues after an error
-    equal (derive [(0, 0, "1"), (1, 0, "def")])
+    equal (run [(0, 0, "1"), (1, 0, "def")])
         (Right ([(0, 1)],
             ["Error: control generator not found: def"]))
-    equal (derive [(0, 0, "1"), (1, 0, "def"), (2, 0, "i 2")])
+    equal (run [(0, 0, "1"), (1, 0, "def"), (2, 0, "i 2")])
         (Right ([(0, 1), (1, 1.5), (2, 2)],
             ["Error: control generator not found: def"]))
 
@@ -89,22 +105,22 @@ mktrack events_end events = TrackTree.make_track ">" evts events_end
     where evts = Events.from_list (map UiTest.make_event events)
 
 test_pitch_track = do
-    let derive = do_derive DeriveTest.e_nns
+    let run = derive DeriveTest.e_nns
 
-    let (val, logs) = derive ("*no-scale", [(0, 0, "1"), (1, 0, "2")])
+    let (val, logs) = run ("*no-scale", [(0, 0, "1"), (1, 0, "2")])
     equal val []
     strings_like logs ["get_scale: unknown scale: no-scale"]
 
-    let (val, logs) = derive ("*twelve", [(0, 0, "1"), (1, 0, "2")])
+    let (val, logs) = run ("*twelve", [(0, 0, "1"), (1, 0, "2")])
     equal val [[]]
     strings_like logs ["not found: 1", "not found: 2"]
-    let (val, logs) = derive
+    let (val, logs) = run
             ("*twelve", [(0, 0, "4c"), (1, 0, "4d"), (2, 0, "4hc")])
     equal val [[(0, 60), (1, 62)]]
     strings_like logs ["not found: 4hc"]
-    equal (derive ("*twelve", [(0, 0, "4c"), (1, 0, "4d")]))
+    equal (run ("*twelve", [(0, 0, "4c"), (1, 0, "4d")]))
         ([[(0, 60), (1, 62)]], [])
-    equal (derive ("*twelve", [(0, 0, "4c"), (2, 0, "i (4d)")]))
+    equal (run ("*twelve", [(0, 0, "4c"), (2, 0, "i (4d)")]))
         ([[(0, 60), (1, 61), (2, 62)]], [])
 
 test_merge_interleave = do
