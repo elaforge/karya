3580541828355eea47791dbaf14dd583ab3fe864
qdunkan <qdunkan@gmail.com>
2017-10-06 22:05:58 +0000

    ness: wait for the estimated runtime before trying to download the output

--- Ness/Submit.hs
+++ Ness/Submit.hs
@@ -16,7 +16,10 @@ import Global
 
 
 loginForm :: String
-loginForm = "https://www.ease.ed.ac.uk/"
+-- loginForm = "https://www.ease.ed.ac.uk/"
+loginForm = "https://www.ease.ed.ac.uk/\
+    \?cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk\
+    \&https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html"
 
 loginUrl :: String
 loginUrl = "https://www.ease.ed.ac.uk/cosign.cgi"
@@ -45,10 +48,11 @@ submitDownload isDemo instrument score out = do
     result <- submit isDemo instrument score
     case result of
         Left err -> errorIO err
-        Right (url, runTime) -> do
+        Right (url, estimatedTime) -> do
             putStrLn $ "=== response: " ++ url
-            putStrLn $ "=== run time: " ++ show runTime
-            ok <- download runTime out url
+            putStrLn $ "=== run time: " ++ show estimatedTime
+            Thread.delay $ realToFrac estimatedTime
+            ok <- download 5 out url
             unless ok $ putStrLn "=== gave up"
             return ()
 
@@ -94,24 +98,23 @@ submit isDemo instrument score = do
         , submitUrl
         ]
         ""
+    writeFile "ness-data/submit.result" json
     either errorIO return $ first txt $ parseJson $
         ByteString.Lazy.Char8.pack json
 
 download :: Double -> FilePath -> String -> IO Bool
-download runTime out url = go =<< Time.getCurrentTime
+download timeout out url = go =<< Time.getCurrentTime
     where
-    go start = do
-        ok <- poll out url
-        if ok then return True
+    go start = ifM (poll out url) (return True) $ do
+        now <- Time.getCurrentTime
+        let elapsed = realToFrac (now `Time.diffUTCTime` start)
+        if elapsed > timeout
+            then return False
             else do
-                now <- Time.getCurrentTime
-                if realToFrac (now `Time.diffUTCTime` start)
-                        > runTime + extraSeconds
-                    then return False
-                    else Thread.delay 2 >> go start
-
-extraSeconds :: Double
-extraSeconds = 5
+                putStrLn $ "elapsed " <> prettys elapsed <> " < "
+                    <> prettys timeout
+                Thread.delay 2
+                go start
 
 poll :: FilePath -> String -> IO Bool
 poll out url = do
@@ -120,7 +123,7 @@ poll out url = do
         , "--cookie", cookies
         , "--output", out
         -- , "--verbose"
-        , "--fail"
+        , "--fail" -- exit failure on 404
         , url
         ]
     return $ exit == Exit.ExitSuccess
@@ -143,88 +146,3 @@ parseJson json = do
                     ( dir <> "/output-mix.wav"
                     , fromMaybe 0 $ ParseText.float time
                     )
-
-{-
-    request:
-    POST /cosign.cgi HTTP/1.1
-    Host: www.ease.ed.ac.uk
-    Content-Type: application/x-www-form-urlencoded
-
-    response:
-    HTTP/1.1 302 Found
-    Set-Cookie: cosign=CEQaxKckosFgCXzJsOnvW-rP83vQO0Eplx8iN2uTBalJwzJR5Du1UxmfVF7rpvQ-1F3ME6AL9mUMZaq1nwLhlmcFBommvof-ig+KWVR0lCWNaQWQ5vNHUnno152+/1507052428/1; path=/; secure; httponly
-    Location: https://ness-frontend.epcc.ed.ac.uk/cosign/valid?cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk=Mxkh0M1yPdDA4reAaK3nWM1+7pGKjZUp1LKITC4wghBDZl3tvXCo6GIMen7UliPzFXKKbcOPR6jRaCHjcV9uaOz1grKpwLVndOIRo3MBcIfPFpG1aGKteC9J6cy9&https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html
-
-    request:
-    GET /cosign/valid?cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk=Mxkh0M1yPdDA4reAaK3nWM1+7pGKjZUp1LKITC4wghBDZl3tvXCo6GIMen7UliPzFXKKbcOPR6jRaCHjcV9uaOz1grKpwLVndOIRo3MBcIfPFpG1aGKteC9J6cy9&https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html HTTP/1.1
-    Host: ness-frontend.epcc.ed.ac.uk
-    Referer: https://www.ease.ed.ac.uk/?cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk&https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html
-    Cookie: __utma=1.1368592074.1500506252.1506442495.1506442495.1; __utmc=1; __utmz=1.1506442495.1.1.utmcsr=ease.ed.ac.uk|utmccn=(referral)|utmcmd=referral|utmcct=/services/; cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk=0mUzWEFxRJyOd0lieWX2ounligWObZR0yGpLCctJnqcIOgF-0pDS5tkvKpfaBL+TR7jdSt1X25PPu+eVU8rWoADuGXhna+rN4viffuk3xstZmTUkuR1DsnpyOznz/1506989239; _ga=GA1.3.1368592074.1500506252; _gid=GA1.3.103846257.1506977666
-
-    response:
-    HTTP/1.1 301 Moved Permanently
-    Date: Tue, 03 Oct 2017 17:40:30 GMT
-    Server: Apache/2.2.15 (Scientific Linux) mod_ssl/2.2.15 OpenSSL/1.0.1e-fips PHP/5.3.3
-    Set-Cookie: cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk=kOpZNV4Ost1ArmNxbg1woNujTzkldsiDN3EZmoV-Eer+7vZyn+2561p8DF6ef2FwYt0UjuKejIDRbf5h3y4mL3ohHYPwEz7+58yGA5-x67GzkkVGkEmT31wgsyQE/1507052430; path=/; secure
-    Location: https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html
-    Content-Length: 276
-    Keep-Alive: timeout=15, max=100
-    Connection: Keep-Alive
-    Content-Type: text/html; charset=iso-8859-1
--}
-
-{-
-    request: loginForm
-    POST /cosign.cgi HTTP/1.1
-    Host: www.ease.ed.ac.uk
-    Connection: keep-alive
-    Content-Length: 207
-    Cache-Control: max-age=0
-    Origin: https://www.ease.ed.ac.uk
-    Upgrade-Insecure-Requests: 1
-    Content-Type: application/x-www-form-urlencoded
-    User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36
-    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-    Referer: https://www.ease.ed.ac.uk/?cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk&https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html
-    Accept-Encoding: gzip, deflate, br
-    Accept-Language: en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2
-    Cookie: cosign-eucsCosign-www.ease.ed.ac.uk=TWJNSFfb3wqOvrjf5h+r28kEzld4jR6xAJlmcUvlmIxV4mWWc7CGPgWkeqdLTp1MGkgqz0paD0jG62Yvsik8ZswofmJU9ssELftYDcfI081gJBI5i1PY4eNpS+Za/1506442362; __utma=1.1368592074.1500506252.1506442495.1506442495.1; __utmc=1; __utmz=1.1506442495.1.1.utmcsr=ease.ed.ac.uk|utmccn=(referral)|utmcmd=referral|utmcct=/services/; _ga=GA1.3.1368592074.1500506252; _gid=GA1.3.103846257.1506977666; cosign=CEQaxKckosFgCXzJsOnvW-rP83vQO0Eplx8iN2uTBalJwzJR5Du1UxmfVF7rpvQ-1F3ME6AL9mUMZaq1nwLhlmcFBommvof-ig+KWVR0lCWNaQWQ5vNHUnno152+/1507051835
-
-    response:
-    HTTP/1.1 302 Found
-    Date: Tue, 03 Oct 2017 17:40:28 GMT
-    Server: Apache
-    Set-Cookie: cosign=CEQaxKckosFgCXzJsOnvW-rP83vQO0Eplx8iN2uTBalJwzJR5Du1UxmfVF7rpvQ-1F3ME6AL9mUMZaq1nwLhlmcFBommvof-ig+KWVR0lCWNaQWQ5vNHUnno152+/1507052428/1; path=/; secure; httponly
-    Location: https://ness-frontend.epcc.ed.ac.uk/cosign/valid?cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk=Mxkh0M1yPdDA4reAaK3nWM1+7pGKjZUp1LKITC4wghBDZl3tvXCo6GIMen7UliPzFXKKbcOPR6jRaCHjcV9uaOz1grKpwLVndOIRo3MBcIfPFpG1aGKteC9J6cy9&https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html
-    Content-Length: 476
-    Keep-Alive: timeout=5, max=100
-    Connection: Keep-Alive
-    Content-Type: text/html; charset=iso-8859-1
-    Request Headers
-    view parsed
-
-    request:
-    GET /cosign/valid?cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk=Mxkh0M1yPdDA4reAaK3nWM1+7pGKjZUp1LKITC4wghBDZl3tvXCo6GIMen7UliPzFXKKbcOPR6jRaCHjcV9uaOz1grKpwLVndOIRo3MBcIfPFpG1aGKteC9J6cy9&https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html HTTP/1.1
-    Host: ness-frontend.epcc.ed.ac.uk
-    Connection: keep-alive
-    Cache-Control: max-age=0
-    Upgrade-Insecure-Requests: 1
-    User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36
-    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-    Referer: https://www.ease.ed.ac.uk/?cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk&https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html
-    Accept-Encoding: gzip, deflate, br
-    Accept-Language: en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2
-    Cookie: __utma=1.1368592074.1500506252.1506442495.1506442495.1; __utmc=1; __utmz=1.1506442495.1.1.utmcsr=ease.ed.ac.uk|utmccn=(referral)|utmcmd=referral|utmcct=/services/; cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk=0mUzWEFxRJyOd0lieWX2ounligWObZR0yGpLCctJnqcIOgF-0pDS5tkvKpfaBL+TR7jdSt1X25PPu+eVU8rWoADuGXhna+rN4viffuk3xstZmTUkuR1DsnpyOznz/1506989239; _ga=GA1.3.1368592074.1500506252; _gid=GA1.3.103846257.1506977666
-
-    response:
-    HTTP/1.1 301 Moved Permanently
-    Date: Tue, 03 Oct 2017 17:40:30 GMT
-    Server: Apache/2.2.15 (Scientific Linux) mod_ssl/2.2.15 OpenSSL/1.0.1e-fips PHP/5.3.3
-    Set-Cookie: cosign-eucsCosign-ness-frontend.epcc.ed.ac.uk=kOpZNV4Ost1ArmNxbg1woNujTzkldsiDN3EZmoV-Eer+7vZyn+2561p8DF6ef2FwYt0UjuKejIDRbf5h3y4mL3ohHYPwEz7+58yGA5-x67GzkkVGkEmT31wgsyQE/1507052430; path=/; secure
-    Location: https://ness-frontend.epcc.ed.ac.uk/~pgraham/NUI/Web/nuiWebMain.html
-    Content-Length: 276
-    Keep-Alive: timeout=15, max=100
-    Connection: Keep-Alive
-    Content-Type: text/html; charset=iso-8859-1
-
--}
