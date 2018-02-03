1ffb17fe1c7bc86cf6dca73ac8de5d1eeb89cb05
qdunkan <qdunkan@gmail.com>
2018-02-03 19:36:45 +0000

    replace tools/backup-save with tools/backup_save.py
    I now include the patch date in the filename, so I can know at which patch
    the scores should be valid.

---
+++ tools/backup_save.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import datetime, os, sys, subprocess, json
+
+
+dir = '../data/save-backup'
+verify_binary = 'build/opt/verify_performance'
+
+def main():
+    run(['bin/mk', verify_binary])
+    patch = patch_json()
+    tgz = '%s_run:%s.tgz' % (patch['date'], datetime.datetime.now().isoformat())
+    tgz = os.path.join(dir, tgz)
+    run(['tar', '-cLzf', tgz, 'save'])
+    run(['ls', '-lh', tgz])
+
+def patch_json():
+    return json.loads(
+        subprocess.run(
+            [verify_binary, '--mode=PatchInfo'],
+            stdout=subprocess.PIPE,
+            check=True).stdout)
+
+def run(cmd):
+    print('##', ' '.join(cmd))
+    subprocess.run(cmd, check=True)
+
+if __name__ == '__main__':
+    main()
