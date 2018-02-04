6eba0628c96c778e876f31bde21050538d4d3a40
qdunkan <qdunkan@gmail.com>
2018-02-04 18:07:57 +0000

    cleanup Derive.C.Prelude.Pitch_test

--- Derive/C/Prelude/Pitch_test.hs
+++ Derive/C/Prelude/Pitch_test.hs
@@ -3,7 +3,6 @@
 -- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
 
 module Derive.C.Prelude.Pitch_test where
-import qualified Util.Seq as Seq
 import Util.Test
 import qualified Ui.UiTest as UiTest
 import qualified Derive.Call.CallTest as CallTest
@@ -12,7 +11,6 @@ import qualified Derive.DeriveTest as DeriveTest
 
 import qualified Perform.NN as NN
 import qualified Perform.Pitch as Pitch
-
 import Global
 import Types
 
@@ -34,8 +32,8 @@ test_set_and_val_to_pitch = do
             ([[(0, NN.c4), (1, NN.c4), (1, NN.d4)]], [])
 
 test_set = do
-    equal (run2 [(0, "set (4c)")]) [(0, NN.c4)]
-    equal (run2 [(0, "4c"), (2, "set (4c) | i (4d)")])
+    equal (run [(0, "set (4c)")]) [(0, NN.c4)]
+    equal (run [(0, "4c"), (2, "set (4c) | i (4d)")])
         [(0, NN.c4), (2, NN.d4), (2, NN.c4)]
 
 test_multiply = do
@@ -89,7 +87,7 @@ test_neighbor = do
     equal (run_tempo 2 [(0, "n (4c) 1d 1t")]) [(0, 62), (0.5, 60)]
 
 test_approach = do
-    equal (run2 [(0, "4c"), (10, "a 2s"), (20, "4d")])
+    equal (run [(0, "4c"), (10, "a 2s"), (20, "4d")])
         [(0, NN.c4), (10, NN.c4), (12, NN.d4), (20, NN.d4)]
 
     let run = DeriveTest.extract_events DeriveTest.e_nns_literal
@@ -101,25 +99,24 @@ test_approach = do
         ]
 
 test_linear = do
-    equal (run [(0, "4c"), (2, "i (4d)")])
-        [(0, NN.c4), (2, 62)]
+    equal (run [(0, "4c"), (2, "i (4d)")]) [(0, NN.c4), (2, NN.d4)]
 
 test_porta = do
-    equal (run2 [(0, "4c"), (2, "porta-place=1 | p (4d) 2s")])
+    equal (run [(0, "4c"), (2, "porta-place=1 | p (4d) 2s")])
         [(0, NN.c4), (2, NN.c4), (4, NN.d4)]
-    equal (run2 [(0, "4c"), (2, "porta-place=0 | p (4d) 1s")])
+    equal (run [(0, "4c"), (2, "porta-place=0 | p (4d) 1s")])
         [(0, NN.c4), (1, NN.c4), (2, NN.d4)]
-    equal (run2 [(0, "4c"),
+    equal (run [(0, "4c"),
             (2, "porta-place=1 | curve=(cf-expon 2) | p (4d) 2s")])
         [(0, NN.c4), (2, NN.c4), (3, 60.5), (4, 62)]
 
 test_linear_next = do
-    equal (run2 [(0, "4c"), (4, "i> (4d)"), (6, "4c")])
+    equal (run [(0, "4c"), (4, "i> (4d)"), (6, "4c")])
         [(0, NN.c4), (4, NN.c4), (6, NN.d4), (6, NN.c4)]
     -- A number is interpreted as a transposition of the previous pitch.
-    equal (run2 [(0, "4c"), (4, "i> 4"), (6, "4c")])
+    equal (run [(0, "4c"), (4, "i> 4"), (6, "4c")])
         [(0, NN.c4), (4, NN.c4), (6, NN.c4 + 4), (6, NN.c4)]
-    equal (run2 [(0, "4c"), (2, "i> (>)"), (4, "4d")])
+    equal (run [(0, "4c"), (2, "i> (>)"), (4, "4d")])
         [(0, NN.c4), (2, NN.c4), (4, NN.d4)]
     -- Test with slicing.
     let run3 = DeriveTest.extract_events DeriveTest.e_nns_literal
@@ -128,18 +125,11 @@ test_linear_next = do
         [[(0, NN.c4), (1, NN.c4)], [(1, NN.c4), (3, NN.d4)], [(3, NN.e4)]]
 
 run :: [(ScoreTime, Text)] -> [(RealTime, Pitch.NoteNumber)]
-run = run_tempo 1
-
-run2 :: [(ScoreTime, Text)] -> [(RealTime, Pitch.NoteNumber)]
-run2 = CallTest.run_pitch2 ""
+run = CallTest.run_pitch2 ""
 
 run_tempo :: Int -> [(ScoreTime, Text)] -> [(RealTime, Pitch.NoteNumber)]
 run_tempo tempo pitches = extract $ run_ tempo pitches []
-    where
-    -- Slicing implementation details can make dups, but they don't matter for
-    -- performance.
-    extract = Seq.drop_dups snd . head
-        . DeriveTest.extract_events DeriveTest.e_nns
+    where extract = head . DeriveTest.extract_events DeriveTest.e_nns_literal
 
 run_ :: Int -> [(ScoreTime, Text)] -> [UiTest.TrackSpec] -> Derive.Result
 run_ tempo pitches tracks = DeriveTest.derive_tracks "" $
