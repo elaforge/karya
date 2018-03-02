07f4625a8bdc00d31afc8de6978474bb5d271738
Evan Laforge <qdunkan@gmail.com>
2018-03-01 18:09:22 -0800

    audio: add Audio.mergeChannels

--- Util/Audio/Audio.hs
+++ Util/Audio/Audio.hs
@@ -2,7 +2,8 @@
 -- This program is distributed under the terms of the GNU General Public
 -- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
 
-{-# LANGUAGE DataKinds, KindSignatures #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE DataKinds, KindSignatures, TypeOperators #-}
 module Util.Audio.Audio (
     -- * types
     AudioM(..), AudioIO, AudioId
@@ -14,12 +15,18 @@ module Util.Audio.Audio (
     , gain
     -- * mix
     , mix
+    -- * mergeChannels
+    , mergeChannels
+#ifdef TESTING
+    , module Util.Audio.Audio
+#endif
 ) where
 import qualified Control.Monad.Identity as Identity
 import qualified Control.Monad.Trans.Resource as Resource
 import qualified Data.Maybe as Maybe
 import qualified Data.Vector.Storable as V
 import qualified GHC.TypeLits as TypeLits
+import GHC.TypeLits (KnownNat)
 import qualified Streaming as S
 import qualified Streaming.Prelude as S
 
@@ -53,13 +60,14 @@ type Channels = Int
 chunkSize :: Count
 chunkSize = 5000
 
-framesCount :: TypeLits.KnownNat channels => Proxy channels -> Frames -> Count
-framesCount channels (Frames frames) =
-    frames * fromIntegral (TypeLits.natVal channels)
+framesCount :: KnownNat channels => Proxy channels -> Frames -> Count
+framesCount channels (Frames frames) = frames * natVal channels
 
-countFrames :: TypeLits.KnownNat channels => Proxy channels -> Count -> Frames
-countFrames channels_ count = Frames $
-    count `div` fromIntegral (TypeLits.natVal channels_)
+countFrames :: KnownNat channels => Proxy channels -> Count -> Frames
+countFrames channels count = Frames $ count `div` natVal channels
+
+chunkFrames :: KnownNat channels => Proxy channels -> V.Vector Sample -> Frames
+chunkFrames channels = countFrames channels . V.length
 
 -- * construct
 
@@ -79,9 +87,9 @@ gain n (Audio audio) = Audio $ S.map (V.map (*n)) audio
 -- | Mix together the audio streams at the given start times.
 --
 -- TODO the input could also be a stream, in case it somehow comes from IO.
-mix :: forall m rate channels. (Monad m, TypeLits.KnownNat channels)
+mix :: forall m rate channels. (Monad m, KnownNat channels)
     => [(Frames, AudioM m rate channels)] -> AudioM m rate channels
-mix = Audio . S.map merge . synchronize . map pad
+mix = Audio . S.map merge . synchronizeChunks . map pad
     where
     pad (frames, Audio a)
         | frames > 0 =
@@ -89,9 +97,9 @@ mix = Audio . S.map merge . synchronize . map pad
         | otherwise = S.map Chunk a
     merge chunks
         | null vs = case [c | Silence c <- chunks] of
-            -- All Silences should be the same, thanks to 'synchronize'.
+            -- All Silences should be the same, thanks to 'synchronizeChunks'.
             count : _ -> V.replicate count 0
-            -- 'synchronize' shouldn't emit empty chunks.
+            -- 'synchronizeChunks' shouldn't emit empty chunks.
             [] -> V.empty
         | otherwise = zipWithN (+) vs
         where vs = [v | Chunk v <- chunks]
@@ -122,9 +130,9 @@ zipWithN f vs = case vs of
 
 -- | Synchronize chunk size for all streams: pull from each one, then split
 -- each to the shortest one.
-synchronize :: Monad m => [S.Stream (S.Of Chunk) m ()]
+synchronizeChunks :: Monad m => [S.Stream (S.Of Chunk) m ()]
     -> S.Stream (S.Of [Chunk]) m ()
-synchronize = S.unfoldr unfold
+synchronizeChunks = S.unfoldr unfold
     where
     unfold audios = do
         pairs <- Maybe.catMaybes <$> mapM S.uncons audios
@@ -136,5 +144,76 @@ synchronize = S.unfoldr unfold
         | otherwise = case chunk of
             Chunk v -> (Chunk pre, S.cons (Chunk post) tail)
                 where (pre, post) = V.splitAt size v
-            Silence frames ->
-                (Silence size, S.cons (Silence (frames - size)) tail)
+            Silence count ->
+                (Silence size, S.cons (Silence (count - size)) tail)
+
+
+-- * mergeChannels
+
+mergeChannels :: forall m rate chan1 chan2.
+    (Monad m, KnownNat chan1, KnownNat chan2)
+    => AudioM m rate chan1 -> AudioM m rate chan2
+    -> AudioM m rate (chan1 TypeLits.+ chan2)
+mergeChannels audio1 audio2 =
+    Audio $ S.map (merge . to0) $ synchronize audio1 audio2
+    where
+    to0 (a1, a2) =
+        ( fromMaybe (V.replicate count1 0) a1
+        , fromMaybe (V.replicate count2 0) a2
+        )
+        where
+        count1 = framesCount chan1 $ maybe 0 (chunkFrames chan2) a2
+        count2 = framesCount chan2 $ maybe 0 (chunkFrames chan1) a1
+    -- These should now have the same number of frames.
+    merge (a1, a2) = interleave $
+        deinterleave (natVal chan1) a1 ++ deinterleave (natVal chan2) a2
+    chan1 = Proxy :: Proxy chan1
+    chan2 = Proxy :: Proxy chan2
+
+deinterleave :: V.Storable a => Channels -> V.Vector a -> [V.Vector a]
+deinterleave channels v
+    | channels == 1 = [v]
+    | otherwise = map gen [0 .. channels - 1]
+    where
+    gen chan = V.generate frames (\f -> v V.! (channels * f + chan))
+    frames = V.length v `div` channels
+
+interleave :: V.Storable a => [V.Vector a] -> V.Vector a
+interleave vs = V.generate (sum (map V.length vs)) gen
+    where
+    gen i = (vs !! vsi) V.! vi
+        where (vi, vsi) = i `divMod` len
+    len = length vs
+
+-- | Synchronize chunk size for two streams.  If one stream runs out ahead of
+-- the other, it will emit Nothings.
+synchronize :: forall m rate chan1 chan2.
+    (Monad m, KnownNat chan1, KnownNat chan2)
+    => AudioM m rate chan1 -> AudioM m rate chan2
+    -> S.Stream (S.Of (Maybe (V.Vector Sample), Maybe (V.Vector Sample))) m ()
+synchronize audio1 audio2 = S.unfoldr unfold (_stream audio1, _stream audio2)
+    where
+    unfold (a1, a2) = recons a1 a2 <$> ((,) <$> S.uncons a1 <*> S.uncons a2)
+    recons a1 a2 = \case
+        (Nothing, Nothing) -> Left ()
+        (Just (c1, as1), Nothing) -> Right ((Just c1, Nothing), (as1, a2))
+        (Nothing, Just (c2, as2)) -> Right ((Nothing, Just c2), (a1, as2))
+        (Just (c1, as1), Just (c2, as2)) -> Right $
+            case compare frames1 frames2 of
+                LT -> ((Just c1, Just pre2), (as1, S.cons post2 as2))
+                GT -> ((Just pre1, Just c2), (S.cons post1 as1, as2))
+                EQ -> ((Just c1, Just c2), (as1, as2))
+            where
+            frames1 = chunkFrames chan1 c1
+            frames2 = chunkFrames chan2 c2
+            (pre1, post1) = V.splitAt (framesCount chan1 shortest) c1
+            (pre2, post2) = V.splitAt (framesCount chan2 shortest) c2
+            shortest = min frames1 frames2
+    chan1 = Proxy :: Proxy chan1
+    chan2 = Proxy :: Proxy chan2
+
+
+-- * util
+
+natVal :: KnownNat n => Proxy n -> Int
+natVal = fromIntegral . TypeLits.natVal
--- Util/Audio/Audio_test.hs
+++ Util/Audio/Audio_test.hs
@@ -3,9 +3,11 @@
 -- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
 
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
 module Util.Audio.Audio_test where
 import qualified Control.Monad.Identity as Identity
 import qualified Data.Vector.Storable as V
+import qualified Streaming.Prelude as S
 
 import qualified Util.Audio.Audio as Audio
 import Util.Test
@@ -23,6 +25,9 @@ test_mix = do
     equal (f [(0, [[1, 2]]), (4, [[3]])]) [1, 2, 0, 0, 3]
     equal (f [(2, [[1, 2]]), (2, [[3]])]) [0, 0, 4, 2]
 
+    -- Deal with empty chunks.
+    equal (f [(0, [[1], [], [3]]), (0, [[], [2], []])]) [3, 3]
+
 test_mix2 = do
     let f = concat . toSamples . Audio.mix . map (second fromSamples2)
     equal (f [(0, [[0, 1], [2, 3]]), (1, [[4, 5]])])
@@ -33,6 +38,38 @@ test_mix2 = do
 test_gain = do
     let f n = concat . toSamples . Audio.gain n . fromSamples
     equal (f 0.5 [[1, 2], [3]]) [0.5, 1, 1.5]
+
+test_mergeChannels = do
+    let f a1 a2 = toSamples $ Audio.mergeChannels a1 a2
+    equal (f (fromSamples []) (fromSamples [])) []
+    equal (f (fromSamples [[1, 3]]) (fromSamples [[2], [4]])) [[1, 2], [3, 4]]
+
+test_synchronize = do
+    let f a1 a2 = map (fmap V.toList *** fmap V.toList) $ unstream $
+            Audio.synchronize a1 a2
+    equal (f (fromSamples []) (fromSamples [])) []
+    equal (f (fromSamples [[1]]) (fromSamples [])) [(Just [1], Nothing)]
+    equal (f (fromSamples [[1]]) (fromSamples [[2, 3]]))
+        [(Just [1], Just [2]), (Nothing, Just [3])]
+    equal (f (fromSamples [[1]]) (fromSamples [[2], [], [3]]))
+        [(Just [1], Just [2]), (Nothing, Just []), (Nothing, Just [3])]
+    equal (f (fromSamples [[1]]) (fromSamples2 [[2, 3]]))
+        [(Just [1], Just [2, 3])]
+
+unstream :: S.Stream (S.Of a) Identity.Identity () -> [a]
+unstream = Identity.runIdentity . S.toList_
+
+test_deinterleave = do
+    let f chans = map V.toList . Audio.deinterleave chans . V.fromList @Float
+    equal (f 1 [1, 2, 3, 4]) [[1, 2, 3, 4]]
+    equal (f 2 [1, 2, 3, 4]) [[1, 3], [2, 4]]
+    equal (f 3 [1, 2, 3, 4]) [[1], [2], [3]]
+
+test_interleave = do
+    let f = V.toList . Audio.interleave . map (V.fromList @Float)
+    equal (f [[1, 2, 3, 4]]) [1, 2, 3, 4]
+    equal (f [[1, 3], [2, 4]]) [1, 2, 3, 4]
+    equal (f [[1, 3], [2, 4], [5]]) [1, 2, 5, 3, 4]
 
 fromSamples :: [[Audio.Sample]] -> Audio.AudioId 10 1
 fromSamples = Audio.fromSamples . map V.fromList
