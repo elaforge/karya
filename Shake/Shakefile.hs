elaforge/karya

commit 2efe2f8525dc3fc20a055e0cfb3b8878eae87824

switch to using cpphs instead of whatever cpp is installed
This means I can finally use backslash string gaps in CPP-using modules.
The combination of the #ifdef TESTING convention and Derive.C.* including
inline documentation means I run into this problem.  Not super frequently,
but enough to be annoying.

Actually there was just one Derive.C.* module with that problem.  But I
know I've not bothered to put on export lists because that would imply
just get rid of the problem.

Shake/Shakefile.hs
@@ -628,6 +628,9 @@ configure midi = do
             -- Except for profiling, where it wants "p_dyn" libraries, which
             -- don't seem to exist.
             [ ["-dynamic" | mode /= Profile]
+            -- There's no particular reason for --nomacro, except I don't use
+            -- them, and I don't want to start unless for good reason.
+            , ["-pgmP", "cpphs --nomacro --cpp"]
             , case mode of
                 Debug -> []
                 Opt -> ["-O"]

.travis.yml
@@ -76,8 +76,8 @@ install:
   - ghc-pkg list
   - cp data/cabal.config .
   - travis_retry cabal update
-  - if which hsc2hs && which c2hs; then echo ok;
-    else travis_retry cabal install -j$cpus hsc2hs c2hs;
+  - if which hsc2hs && which c2hs && which cpphs; then echo ok;
+    else travis_retry cabal install -j$cpus hsc2hs c2hs cpphs;
     fi
   # mk typecheck needs this even though it's not in the .cabal
   # TODO Ugh.  I should switch to gauge and get rid of this hack.

Derive/C/India/Gamakam5.hs
@@ -228,16 +228,15 @@ step_difference p1 p2 = fmap realToFrac $
     (-) <$> Pitches.pitch_nn p1 <*> Pitches.pitch_nn p2
 
 pitch_sequence_doc :: Doc.Doc
-pitch_sequence_doc = Doc.Doc $ mconcat
-    [ "This is a mini-language, where each one or two characters is a call."
-    , " An upper-case call will take a single character argument. A special"
-    , " parsing rule means that `-` and its following character is considered"
-    , " a single character, so `-1` is a valid call or argument."
-    , " Most of these calls represent a pitch movement:\n"
-    , Text.unlines (map pitch_call_doc (Map.toAscList pitch_call_map))
-    , "\nCurrently the transition curve is hardcoded to a sigmoid curve, but"
-    , " I could add a curve env var if necessary."
-    ]
+pitch_sequence_doc = Doc.Doc $
+    "This is a mini-language, where each one or two characters is a call.\
+    \ An upper-case call will take a single character argument. A special\
+    \ parsing rule means that `-` and its following character is considered\
+    \ a single character, so `-1` is a valid call or argument.\
+    \ Most of these calls represent a pitch movement:"
+    <> Text.unlines (map pitch_call_doc (Map.toAscList pitch_call_map))
+    <> "Currently the transition curve is hardcoded to a sigmoid curve, but\
+    \ I could add a curve env var if necessary."
 
 pitch_call_doc :: (Char, [PitchCall]) -> Text
 pitch_call_doc (name, pcalls) =
@@ -299,13 +298,12 @@ c_dyn_sequence = Derive.generator1 (module_ <> "dyn") "dyn-sequence" mempty doc
                 { state_from_dyn = maybe 0 snd (Args.prev_control args) }
         Derive.at start $ dyn_sequence (end - start) state text
     where
-    doc = Doc.Doc $ mconcat
-        [ "This is a mini-language, where each one or two characters is a call."
-        , " Each character can take an argument, which can only be a single"
-        , " digit. Typically this represents a dyn level / 9, so 0 is 0 and"
-        , " 9 is 1.  Calls:\n"
-        , Text.unlines (map dyn_call_doc (Map.toAscList dyn_call_map))
-        ]
+    doc = Doc.Doc $
+        "This is a mini-language, where each one or two characters is a call.\
+        \ Each character can take an argument, which can only be a single\
+        \ digit. Typically this represents a dyn level / 9, so 0 is 0 and\
+        \ 9 is 1.  Calls:"
+        <> Text.unlines (map dyn_call_doc (Map.toAscList dyn_call_map))
 
 dyn_call_doc :: (Char, DynCall) -> Text
 dyn_call_doc (name, dcall) = "`" <> Text.singleton name <> "` - "
@@ -769,6 +767,6 @@ pitch_has_argument c = Char.isUpper c || c == '-'
 
 c_sahitya :: Derive.Taggable a => Derive.Transformer a
 c_sahitya = Derive.transformer module_ "sahitya" mempty
-    ("Ignore the transformed deriver. Put this on a track to ignore its"
-    <> " contents, and put in sahitya.")
+    "Ignore the transformed deriver. Put this on a track to ignore its\
+    \ contents, and put in sahitya."
     $ Sig.call0t $ \_args _deriver -> return Stream.empty

Derive/Scale/Twelve.hs
@@ -58,12 +58,11 @@ relative_scale = ChromaticScales.make_scale "twelve-r" relative_scale_map
     "This is 12TET, but spelled relative to the current key and mode."
 
 keyed_scale :: Scale.Scale
-keyed_scale = ChromaticScales.make_scale "twelve-k" keyed_scale_map $ mconcat
-    [ "This variant treats accidentals like staff notation. If a pitch doesn't"
-    , " have an accidental, it will inherit from the key signature. It thus"
-    , " needs an explicit natural to cancel that out. Unlike staff notation,"
-    , " accidentals don't persist until the next barline."
-    ]
+keyed_scale = ChromaticScales.make_scale "twelve-k" keyed_scale_map
+    "This variant treats accidentals like staff notation. If a pitch doesn't\
+    \ have an accidental, it will inherit from the key signature. It thus\
+    \ needs an explicit natural to cancel that out. Unlike staff notation,\
+    \ accidentals don't persist until the next barline."
 
 scale_id :: Pitch.ScaleId
 scale_id = "twelve"

Midi/TestMidi.hs
@@ -144,18 +144,17 @@ blocking_get :: Interface.ReadChan -> ReadMsg
 blocking_get read_chan = STM.atomically $ fmap Just (STM.readTChan read_chan)
 
 usage :: String
-usage = unlines
-    [ "(no arg)             monitor all inputs"
-    , "record-sysex         save incoming sysex msgs to files"
-    , "send-sysex <out> fn  read a raw sysex from fn and send it to the port"
-    , "monitor <a> <b> ...  monitor input ports 'a' and 'b'"
-    , "help                 print this usage"
-    , "thru <out>           msgs from any input are relayed to <out>"
-    , "melody <out>         play a melody on <out>, also relaying msgs thru"
-    , "spam <out> n         spam <out> with 'n' msgs in rapid succession"
-    , "test                 run some semi-automatic tests"
-    , "pb-range <out> n     send pitch bend range"
-    ]
+usage =
+    "(no arg)             monitor all inputs\n\
+    \record-sysex         save incoming sysex msgs to files\n\
+    \send-sysex <out> fn  read a raw sysex from fn and send it to the port\n\
+    \monitor <a> <b> ...  monitor input ports 'a' and 'b'\n\
+    \help                 print this usage\n\
+    \thru <out>           msgs from any input are relayed to <out>\n\
+    \melody <out>         play a melody on <out>, also relaying msgs thru\n\
+    \spam <out> n         spam <out> with 'n' msgs in rapid succession\n\
+    \test                 run some semi-automatic tests\n\
+    \pb-range <out> n     send pitch bend range\n"
 
 
 -- * program change

Shake/HsDeps.hs
@@ -150,13 +150,11 @@ parseImports = Maybe.mapMaybe (parse . B.words) . B.lines
     parse (w1:w2:_) | w1 == "import" = Just w2
     parse _ = Nothing
 
--- | Read the file, and preprocess with CPP if cppFlags are given.
+-- | Read the file, and preprocess with CPPHS if cppFlags are given.
 preprocess :: Maybe [String] -> FilePath -> IO B.ByteString
 preprocess Nothing fn = B.readFile fn
-preprocess (Just flags) fn = processStdout "cpp" (["-w", "-P"] ++ flags ++ [fn])
-    -- Use -w to suppress warnings.  CPP doesn't understand haskell comments
-    -- and will warn about unterminated 's in comments.  -P suppresses the
-    -- '# line' stuff I don't care about.
+preprocess (Just flags) fn =
+    processStdout "cpphs" ("--cpp" : flags ++ [fn])
 
 
 -- * util

TODO
@@ -261,6 +261,7 @@ UNSORTED
   - support cabal new-build
     . The advantage is that people can install dependencies without messing up
       their global package db.
+    . Also I might be able to skip the install build tools step.
     . cabal sandbox already does this, but with new-build they can reuse
       installed packages.
     . I think to support this I install deps via new-build --only-dependencies
@@ -280,6 +281,7 @@ UNSORTED
         regressions due to dependency changes.
       . But locally maybe I want to freeze, to avoid churn whenever I install
         anything new.
+    . All in all, this is not really a big deal.
   track down 8.4.1 performance regression:
     . performance regression notes: http://ghc.haskell.org/trac/ghc/ticket/14964
     - try with 8.0.2
@@ -316,7 +318,7 @@ UNSORTED
         . opt/seq     1339.73s user 39.87s system 341% cpu 6:43.50 total
   upgrade to ghc 8.4.1
     first make sure deps build
-      - sandi, filed bug.  It's only used in Ness.* though.
+      * sandi, filed bug.  It's only used in Ness.* though.
   - extend gliss so I can gliss on harmonics
     . Why does eval_quoted give opposite pitches?
     note = Eval.eval_one_call True $ Expr.call0 Symbols.null_note
@@ -387,7 +389,7 @@ UNSORTED
     Pitch.c_set could make curves by default?
 
   - pnovla @pno1 32.25t mordent broken, probably too much note overlap
-  - If I use cpphs I might be able to use \ string continuation for CPP
+  * If I use cpphs I might be able to use \ string continuation for CPP
     modules.  It would add another dependency though... worth it?
     . On the other hand, I only have 54 modules with CPP and I don't exactly
       want to have more.  And most of them don't need string gaps... but

karya.cabal
@@ -24,6 +24,9 @@ source-repository head
 
 -- build-depends are appended by the shake rule.
 library
+    -- Supported by Cabal >=2.0, should make --only-dependencies install the
+    -- binaries:
+    -- build-tool-depends: cpphs, c2hs
     build-depends:
         Diff >=0.2,
         QuickCheck,

commit ed4909b8b9dee0930b761471227078333e8e27b5

shakefile: fix haddock, broken by 2efe2f8525dc3fc20a055e0cfb3b8878eae87824

Shake/Shakefile.hs
@@ -631,9 +631,6 @@ configure midi = do
             -- Except for profiling, where it wants "p_dyn" libraries, which
             -- don't seem to exist.
             [ ["-dynamic" | mode /= Profile]
-            -- There's no particular reason for --nomacro, except I don't use
-            -- them, and I don't want to start unless for good reason.
-            , ["-pgmP", "cpphs --nomacro --cpp"]
             , case mode of
                 Debug -> []
                 Opt -> ["-O"]
@@ -686,6 +683,42 @@ configure midi = do
             , define = ["-D__linux__"]
             }
     run cmd args = Process.readProcess cmd args ""
+
+-- | Flags used by both ghc and haddock.  This is unlike 'hcFlags', which is
+-- used by ghc only, and vary based on Mode.
+ghcGlobalFlags :: [Flag]
+ghcGlobalFlags =
+    -- There's no particular reason for --nomacro, except I don't use
+    -- them, and I don't want to start unless for good reason.
+    ["-pgmP", "cpphs --nomacro --cpp"]
+    ++ ghcLanguageFlags
+
+-- | Language extensions which are globally enabled.
+ghcLanguageFlags :: [Flag]
+ghcLanguageFlags = map ("-X"++)
+    -- Pretty conservative, and useful.
+    [ "BangPatterns"
+    -- This enables slightly more concise record initialization and doesn't
+    -- seem to hurt anything.
+    , "DisambiguateRecordFields"
+    -- ghc-7.10 adds a new rule where you can't infer a signature you can't
+    -- type.  OverloadedStrings combined with local definitions results in
+    -- a lot of types like "IsString [a] => [a] -> ...", which results in
+    -- "Non type-variable argument in the constraint: IsString [a]".
+    , "FlexibleContexts"
+    -- Just too useful.
+    , "GeneralizedNewtypeDeriving"
+    , "LambdaCase"
+    , "MultiWayIf"
+    -- Without this, it becomes really annoying to use Text everywhere.
+    , "OverloadedStrings"
+    , "ScopedTypeVariables"
+    -- It's nicer than flip (,), but not worth using if you have to put in
+    -- a LANGUAGE.
+    , "TupleSections"
+    -- Allow instances on nested types, and fully applied type synonyms.
+    , "FlexibleInstances", "TypeSynonymInstances"
+    ]
 
 -- | When using gcc I get these defines automatically, but I need to add them
 -- myself for ghci.  But then c2hs complains about duplicate definitions, so
@@ -1020,7 +1053,7 @@ makeHaddock modeConfig = do
             ]
     let ghcFlags = concat
             [ define flags, cInclude flags
-            , ghcLanguageFlags
+            , ghcGlobalFlags
             , packageFlags flags packages
             ]
     Util.system "haddock" $
@@ -1413,7 +1446,7 @@ ghcFlags config = concat $
     [ "-outputdir", oDir config, "-osuf", ".hs.o"
     , "-i" ++ List.intercalate ":" [oDir config, hscDir config, chsDir config]
     ] :
-    [ ghcLanguageFlags
+    [ ghcGlobalFlags
     , define (configFlags config)
     , cInclude (configFlags config)
     , ghcWarnings config
@@ -1443,33 +1476,6 @@ ghciFlags config = concat
         -- Otherwise ghci warns "Hpc can't be used with byte-code interpreter."
         , flag == "-fhpc"
         ]
-
--- | Language extensions which are globally enabled.
-ghcLanguageFlags :: [String]
-ghcLanguageFlags = map ("-X"++)
-    -- Pretty conservative, and useful.
-    [ "BangPatterns"
-    -- This enables slightly more concise record initialization and doesn't
-    -- seem to hurt anything.
-    , "DisambiguateRecordFields"
-    -- ghc-7.10 adds a new rule where you can't infer a signature you can't
-    -- type.  OverloadedStrings combined with local definitions results in
-    -- a lot of types like "IsString [a] => [a] -> ...", which results in
-    -- "Non type-variable argument in the constraint: IsString [a]".
-    , "FlexibleContexts"
-    -- Just too useful.
-    , "GeneralizedNewtypeDeriving"
-    , "LambdaCase"
-    , "MultiWayIf"
-    -- Without this, it becomes really annoying to use Text everywhere.
-    , "OverloadedStrings"
-    , "ScopedTypeVariables"
-    -- It's nicer than flip (,), but not worth using if you have to put in
-    -- a LANGUAGE.
-    , "TupleSections"
-    -- Allow instances on nested types, and fully applied type synonyms.
-    , "FlexibleInstances", "TypeSynonymInstances"
-    ]
 
 -- * cc
 

Synth/Faust/Render_test.hs
@@ -1,3 +1,7 @@
+-- Copyright 2018 Evan Laforge
+-- This program is distributed under the terms of the GNU General Public
+-- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
+
 module Synth.Faust.Render_test where
 import qualified Data.Map as Map
 import Util.Test