5ce41ba53d679b407ffd3875efd051a10c25b54d
Evan Laforge <qdunkan@gmail.com>
2018-07-07 12:38:03 -0700

    test: make CPUS env var override Cpu.physicalCores, just like tools/ncpus

--- Util/Cpu.hs
+++ Util/Cpu.hs
@@ -9,20 +9,32 @@ import qualified Data.Text as Text
 import Data.Text (Text)
 import qualified Data.Text.IO as Text.IO
 
+import qualified System.Environment as Environment
 import qualified System.Info
 import qualified System.Process as Process
 
+import qualified Text.Read as Read
 
+
+-- | Get number of physical cores.  This can be overidden with a CPUS
+-- environment variable.  This is useful if you are running on a VM in CI
+-- and don't agree with how many cores it claims to have.
 physicalCores :: IO Int
-physicalCores = case System.Info.os of
+physicalCores = maybe getPhysicalCores return =<< envCores
+
+getPhysicalCores :: IO Int
+getPhysicalCores = case System.Info.os of
     "darwin" -> read <$>
         Process.readProcess "/usr/sbin/sysctl" ["-n", "hw.physicalcpu"] ""
-    "linux" -> cpuinfoPhysical <$> Text.IO.readFile "/proc/cpuinfo"
+    "linux" -> linuxPhysicalCores <$> Text.IO.readFile "/proc/cpuinfo"
     _ -> error $ "unknown platform: " ++ System.Info.os
 
--- | Parse /proce/cpuinfo for physical cpu count.
-cpuinfoPhysical :: Text -> Int
-cpuinfoPhysical = length . unique . map cpu . Text.splitOn "\n\n"
+envCores :: IO (Maybe Int)
+envCores = (Read.readMaybe =<<) <$> Environment.lookupEnv "CPUS"
+
+-- | Parse /proc/cpuinfo for physical cpu count.
+linuxPhysicalCores :: Text -> Int
+linuxPhysicalCores = length . unique . map cpu . Text.splitOn "\n\n"
     where
     -- unique pairs of (physical id, core id)
     cpu = filter (\s -> any (`Text.isPrefixOf` s) ["physical id", "core id"])
