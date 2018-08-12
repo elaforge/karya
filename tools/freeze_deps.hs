5968e71bee630480652b3e8101d7bb8d5ec785a9
Evan Laforge <qdunkan@gmail.com>
2018-08-11 20:51:25 -0700

    update cabal.configs, and make tools/freeze_deps.hs do all the freezing work

--- tools/freeze_deps.hs
+++ tools/freeze_deps.hs
@@ -4,29 +4,49 @@
 -- Run cabal freeze, then use this to take out the base deps, so it has a
 -- chance of working with another ghc version.
 import qualified Data.Text as Text
-import qualified System.Process as Process
 import Data.Text (Text)
 import qualified Data.Text.IO as Text.IO
+
+import qualified System.Directory as Directory
+import qualified System.Posix.Temp as Temp
+import qualified System.Process as Process
 
 
 main :: IO ()
 main = do
-    constraints <- parseConstraints . Text.lines <$>
-        Text.IO.readFile "cabal.config"
-    global <- map (Text.dropWhileEnd (=='-') . Text.dropWhileEnd (/='-')
-        . Text.strip) . drop 1 . Text.lines
-        . Text.pack <$> Process.readProcess "ghc-pkg" ["list", "--global"] ""
-    mapM_ Text.IO.putStrLn $ ("constraints:":) $
+    global <- getGlobalPackags
+    freeze global "karya.cabal" "cabal.config"
+    freeze global "data/all-deps.cabal" "data/all-deps.cabal.config"
+
+-- Cabal is really awkward to script.
+freeze :: [Text] -> FilePath -> FilePath -> IO ()
+freeze global cabalFile freezeFile = do
+    cabal <- Text.IO.readFile cabalFile
+    tmp <- Temp.mkdtemp "/tmp/freeze_deps"
+    constraints <- Directory.withCurrentDirectory tmp $ do
+        Text.IO.writeFile "fake-project.cabal" cabal
+        Process.callProcess "cabal" ["freeze"]
+        parseConstraints . Text.lines <$> Text.IO.readFile "cabal.config"
+    Text.IO.writeFile freezeFile $ Text.unlines $
+        ("constraints:":) $
         mapInit (<>",") $ map (\(a, b) -> "    " <> Text.unwords [a, b]) $
         filter ((`notElem` global) . fst) constraints
 
+getGlobalPackags :: IO [Text]
+getGlobalPackags =
+    map (Text.dropWhileEnd (=='-') . Text.dropWhileEnd (/='-') . Text.strip)
+        . drop 1 . Text.lines . Text.pack
+        <$> Process.readProcess "ghc-pkg" ["list", "--global"] ""
+
 parseConstraints :: [Text] -> [(Text, Text)]
+parseConstraints [] = []
 parseConstraints (line:lines) =
     map (split . strip) (Text.dropWhile (/=' ') line : lines)
     where
     strip = Text.dropWhileEnd (==',') . Text.strip
     split s = case Text.words s of
         [a, b] -> (a, b)
+        ws -> error $ "can't parse line: " ++ show ws
 
 mapInit :: (a -> a) -> [a] -> [a]
 mapInit _ [] = []
