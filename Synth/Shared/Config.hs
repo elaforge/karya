elaforge/karya

commit 35af7a5c09eb689753dd3357befb7d07b8d24bb6

group all data directories into the `data` dir, and make AppDir its own type
This started as a simple clean up since there are more and more of these
data dirs, but grew into something unreasonably complicated.

Synth/Shared/Config.hs
@@ -5,16 +5,11 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 -- | Shared config to coordinate between the sequencer and im subsystems.
---
--- TODO Currently paths rely on you being in the right directory, but should
--- probably have some more robust configuration at some point.  Of course
--- 'App.Config.app_dir' is just return '.' too.
 module Synth.Shared.Config where
 import qualified Data.Map as Map
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
 
-import qualified System.Directory as Directory
 import qualified System.FilePath as FilePath
 import System.FilePath ((</>))
 import qualified System.IO as IO
@@ -25,6 +20,8 @@ import qualified Util.Log as Log
 import qualified Util.Parse as Parse
 import qualified Util.Seq as Seq
 
+import qualified App.Config as Config
+import qualified App.Path as Path
 import qualified Perform.RealTime as RealTime
 import qualified Ui.Id as Id
 
@@ -33,13 +30,6 @@ import Synth.Types
 
 #include "config.h"
 
-
--- TODO synchronize this with all the others uses of the data dir
--- I need an absolute path, because this goes to PlayCache, which runs with
--- who knows what CWD.
-getDataDir :: FilePath
-getDataDir = Unsafe.unsafePerformIO $ Directory.canonicalizePath "../data"
-{-# NOINLINE getDataDir #-}
 
 data Config = Config {
     -- | All of the data files used by the Im backend are based in this
@@ -50,9 +40,12 @@ data Config = Config {
     }
     deriving (Eq, Show)
 
-config :: Config
-config = Config
-    { imDir = "im"
+getConfig :: IO Config
+getConfig = config <$> Path.get_app_dir
+
+config :: Path.AppDir -> Config
+config appDir = Config
+    { imDir = Path.absolute appDir Config.im_dir
     , synths = Map.fromList
         [ (samplerName, sampler)
         , (faustName, faust)
@@ -83,8 +76,21 @@ sampler = Synth
     }
 
 -- | Base directory for sampler patches.
-samplerRoot :: FilePath
-samplerRoot = getDataDir </> "sampler"
+samplerRoot :: Path.Relative
+samplerRoot = Config.data_dir Path.</> "sampler"
+
+-- | This is samplerRoot, but as an absolute path.
+--
+-- Technically, Path.get_app_dir is in IO, so I can't get an absolute path
+-- without IO.  However, I need to put the absolute path in the OSC play msgs
+-- (ultimately since play_cache runs with an unknown CWD), and it gets really
+-- annoying to try to get a Path.AppDir into the thru function's closure.  So
+-- unsafePerformIO it is.  I could probably just put that on app_dir, but this
+-- is the only thing that actually needs it.
+unsafeSamplerRoot :: FilePath
+unsafeSamplerRoot =
+    Path.absolute (Unsafe.unsafePerformIO Path.get_app_dir) samplerRoot
+{-# NOINLINE unsafeSamplerRoot #-}
 
 faustName :: SynthName
 faustName = "faust"

User/Elaforge/Config.hs
@@ -14,6 +14,7 @@ import qualified Util.Seq as Seq
 import qualified App.Config as Config
 import qualified App.LoadInstruments as LoadInstruments
 import qualified App.ParseArgs as ParseArgs
+import qualified App.Path as Path
 import qualified App.StaticConfig as StaticConfig
 
 import qualified Cmd.Cmd as Cmd
@@ -34,7 +35,7 @@ import Global
 
 load_static_config :: IO StaticConfig.StaticConfig
 load_static_config = do
-    app_dir <- Config.get_app_dir
+    app_dir <- Path.get_app_dir
     instrument_db <- LoadInstruments.load app_dir
     midi <- get_midi_config instrument_db
     return $ StaticConfig.StaticConfig

User/Empty/Config.hs
@@ -5,20 +5,22 @@
 -- | Setup 'StaticConfig.StaticConfig'.  This is an empty example config, which
 -- you'll want to customize locally.
 module User.Empty.Config (load_static_config) where
-import qualified Cmd.Cmd as Cmd
-import qualified Cmd.Msg as Msg
-import qualified Derive.C.All as C.All
 import qualified App.Config as Config
 import qualified App.LoadInstruments as LoadInstruments
 import qualified App.ParseArgs as ParseArgs
+import qualified App.Path as Path
 import qualified App.StaticConfig as StaticConfig
+
+import qualified Cmd.Cmd as Cmd
+import qualified Cmd.Msg as Msg
+import qualified Derive.C.All as C.All
 
 import Global
 
 
 load_static_config :: IO StaticConfig.StaticConfig
 load_static_config = do
-    app_dir <- Config.get_app_dir
+    app_dir <- Path.get_app_dir
     instrument_db <- LoadInstruments.load app_dir
     midi <- get_midi_config instrument_db
     return $ StaticConfig.StaticConfig

.gitignore
@@ -2,7 +2,9 @@
 /log/
 /Local/
 
-# Symlinks, generated by mklinks:
+# Symlink to the data dir, generated by tools/do-setup
+/data
+# Symlinks to data dir stuff I keep around because I'm used to them:
 /save
 /inst_db
 /im

App/Config.hsc
@@ -4,12 +4,12 @@
 
 -- | Global static app defaults.
 module App.Config where
+import qualified App.Path as Path
+import App.Path ((</>))
 import qualified Data.Array.IArray as IArray
 import qualified Data.Bits as Bits
 import qualified Data.Map.Strict as Map
-import qualified Data.String as String
 import qualified System.Info
-import qualified System.FilePath as FilePath
 
 import qualified Util.Array as Array
 import qualified Util.Thread as Thread
@@ -40,48 +40,43 @@ platform = case System.Info.os of
 
 -- * paths
 
--- | Paths which are intended to be relative to the app dir get this type,
--- so it's harder to accidentally use them directly.
-newtype RelativePath = RelativePath FilePath deriving (Show, String.IsString)
+-- | All local data is relative to this dir.
+data_dir :: Path.Relative
+data_dir = "data"
 
-(</>) :: RelativePath -> RelativePath -> RelativePath
-RelativePath a </> RelativePath b = RelativePath (a FilePath.</> b)
-
-make_path :: FilePath -> RelativePath -> FilePath
-make_path app_dir (RelativePath path) = app_dir FilePath.</> path
-
--- | All paths should be relative to this one.
--- I may later change this to an env var, a flag, or just leave it hardcoded.
-get_app_dir :: IO FilePath
-get_app_dir = return "."
-
--- | All code and data local to an installation (i.e. specific to a particular
--- configuration) should go here.
-local_dir :: RelativePath
-local_dir = "Local"
-
--- | These directories are searched for ky files containing local definitions.
--- The directory of the saved score is prepended to the list.
-ky_paths :: [RelativePath]
-ky_paths = [local_dir </> "ky"]
-
--- | Store instrument db code and data.
-instrument_dir :: RelativePath
-instrument_dir = "inst_db"
-
--- | Directory for instruments with slow patch loading to save their caches.
--- This should be below 'instrument_dir'.
-instrument_cache_dir :: RelativePath
-instrument_cache_dir = "db"
-
-log_dir :: RelativePath
-log_dir = "log"
+-- ** data, shared between repos
 
 -- | Saved scores are expected to be relative to this directory.  I use
 -- a symlink to have a path relative to the app dir, but point to a global
 -- directory.
-save_dir :: RelativePath
-save_dir = "save"
+save_dir :: Path.Relative
+save_dir = data_dir </> "save"
+
+-- | These directories are searched for ky files containing local definitions.
+-- The directory of the saved score is prepended to the list.
+ky_paths :: [Path.Relative]
+ky_paths = [local_dir </> "ky"]
+
+-- | Store instrument db code and data.
+instrument_dir :: Path.Relative
+instrument_dir = data_dir </> "inst_db"
+
+im_dir :: Path.Relative
+im_dir = data_dir </> "im"
+
+-- | Directory for instruments with slow patch loading to save their caches.
+instrument_cache_dir :: Path.Relative
+instrument_cache_dir = instrument_dir </> "db"
+
+-- ** data, local to a repo
+
+-- | All code and data local to an installation (i.e. specific to a particular
+-- configuration) should go here.
+local_dir :: Path.Relative
+local_dir = "Local"
+
+log_dir :: Path.Relative
+log_dir = "log"
 
 
 -- * status view

App/ExtractDoc.hs
@@ -14,7 +14,7 @@ import qualified Text.Printf as Printf
 
 import qualified Util.Doc as Doc
 import qualified Util.Seq as Seq
-import qualified Ui.Key as Key
+import qualified App.Path as Path
 import qualified Cmd.CallDoc as CallDoc
 import qualified Cmd.Cmd as Cmd
 import qualified Cmd.GlobalKeymap as GlobalKeymap
@@ -24,20 +24,23 @@ import qualified Cmd.NoteTrackKeymap as NoteTrackKeymap
 
 import qualified Derive.C.All as C.All
 import qualified Derive.Scale.All as Scale.All
+import qualified Ui.Key as Key
+
 import Global
 
 
 main :: IO ()
 main = do
     args <- Environment.getArgs
+    app_dir <- Path.get_app_dir
     case args of
         ["keymap"] -> Text.IO.putStr keymap_doc
         ["calls"] -> do
-            hstate <- Doc.get_html_state "../haddock" "."
+            hstate <- Doc.get_html_state "../haddock" app_dir
             Text.IO.putStr $ Doc.un_html $ CallDoc.doc_html hstate $
                 CallDoc.builtins C.All.builtins
         ["scales"] -> do
-            hstate <- Doc.get_html_state "../haddock" "."
+            hstate <- Doc.get_html_state "../haddock" app_dir
             Text.IO.putStr $ Doc.un_html $ CallDoc.scales_html hstate $
                 CallDoc.scale_docs Scale.All.docs
         _ -> error "usage: extract_doc [ keymap | calls | scales ]"

App/LoadInstruments.hs
@@ -18,14 +18,15 @@
 module App.LoadInstruments where
 import System.FilePath ((</>))
 
-import qualified Util.Log as Log
+import qualified App.Path as Path
 import qualified Cmd.Cmd as Cmd
 import qualified Cmd.Instrument.MidiInst as MidiInst
-import qualified Perform.Im.Play
-import qualified Perform.Lilypond.Constants as Lilypond.Constants
 import qualified Instrument.Inst as Inst
 import qualified Instrument.InstTypes as InstTypes
 import qualified Instrument.Parse as Parse
+import qualified Perform.Im.Play
+import qualified Perform.Lilypond.Constants as Lilypond.Constants
+import qualified Util.Log as Log
 
 import qualified Local.Instrument
 import qualified App.Config as Config
@@ -70,18 +71,16 @@ synth_warnings = concat
 internal_synths :: [MidiInst.Synth]
 internal_synths = [Lilypond.Constants.ly_synth Cmd.empty_code]
 
-load :: FilePath -> IO (Inst.Db Cmd.InstrumentCode)
+load :: Path.AppDir -> IO (Inst.Db Cmd.InstrumentCode)
 load app_dir = do
-    loaded <- mapMaybeM
-        (($ Config.make_path app_dir Config.instrument_dir) . snd . snd)
-        all_loads
+    loaded <- mapMaybeM (($ app_dir) . snd . snd) all_loads
     let synths = concat
             [ im_synths
             , loaded
             , midi_synths
             , internal_synths
             ]
-    let annot_fn = Config.make_path app_dir Config.local_dir
+    let annot_fn = Path.absolute app_dir Config.local_dir
             </> "instrument_annotations"
     annots <- Parse.parse_annotations annot_fn >>= \case
         -- The parsec error already includes the filename.

App/Path.hs
@@ -1,0 +1,42 @@
+-- Copyright 2018 Evan Laforge
+-- This program is distributed under the terms of the GNU General Public
+-- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
+
+module App.Path (
+    AppDir(..), get_app_dir
+    , Relative, relative, unrelative, (</>)
+    , absolute, get_absolute
+) where
+import qualified Data.String as String
+import qualified System.Directory as Directory
+import qualified System.FilePath as FilePath
+
+
+-- | All paths should be relative to this one.
+-- I may later change this to an env var, a flag, or just leave it hardcoded.
+get_app_dir :: IO AppDir
+get_app_dir = AppDir <$> Directory.getCurrentDirectory
+
+newtype AppDir = AppDir FilePath
+    deriving (Eq, Show)
+
+-- | Paths which are intended to be relative to the app dir get this type,
+-- so it's harder to accidentally use them directly.
+newtype Relative = Relative FilePath deriving (Eq, Show, String.IsString)
+
+relative :: FilePath -> Relative
+relative = Relative
+
+-- | Normally you should use 'absolute', but sometimes I need the Realtive path
+-- as a string.
+unrelative :: Relative -> FilePath
+unrelative (Relative path) = path
+
+(</>) :: Relative -> Relative -> Relative
+Relative a </> Relative b = Relative (a FilePath.</> b)
+
+absolute :: AppDir -> Relative -> FilePath
+absolute (AppDir app_dir) (Relative path) = app_dir FilePath.</> path
+
+get_absolute :: Relative -> IO FilePath
+get_absolute dir = absolute <$> get_app_dir <*> pure dir

commit 50990333f3dd3fb45eb12d88d86e3bc890dcf805

im: fix a bug where I got output cache directory wrong
Introduced by 35af7a5c09eb689753dd3357befb7d07b8d24bb6

Synth/Shared/Config.hs
@@ -181,8 +181,8 @@ outputFilename imDir notesFilename inst =
         FilePath.splitPath notesFilename
 
 -- |
--- > im/notes/$scorePath/$scoreFname/$namespace/$block/sampler ->
--- > im/cache/$scorePath/$scoreFname/$namespace/$block/$instrument
+-- > .../im/notes/$scorePath/$scoreFname/$namespace/$block/sampler ->
+-- > .../im/cache/$scorePath/$scoreFname/$namespace/$block/$instrument
 outputDirectory :: FilePath
     -> FilePath -- ^ Names as produced by 'notesFilename'.
     -> Text -- ^ ScoreTypes.Instrument, but I don't want to import ScoreTypes.
@@ -192,15 +192,16 @@ outputDirectory imDir notesFilename inst =
 
 -- | Get the directory which contains each instrument subdir.
 --
--- > im/notes/$scorePath/$scoreFname/$namespace/$block/sampler ->
--- > im/cache/$scorePath/$scoreFname/$namespace/$block
+-- > .../im/notes/$scorePath/$scoreFname/$namespace/$block/sampler ->
+-- > .../im/cache/$scorePath/$scoreFname/$namespace/$block
 instrumentDirectory :: FilePath -> FilePath -> FilePath
 instrumentDirectory imDir notesFilename = imDir </> cacheDir </> scorePathBlock
     where
-    -- Recover scorePath/ns/block from the path so I don't have to put it in
+    -- Derive scorePath/ns/block from the path so I don't have to put it in
     -- a file or something.  TODO It's a bit sketchy though.
-    scorePathBlock = FilePath.joinPath $ Seq.rdrop 1 $ drop 2 $
-        FilePath.splitPath notesFilename
+    scorePathBlock = FilePath.joinPath $
+        Seq.rdrop 1 $ drop 1 $ FilePath.splitPath $
+        dropWhile (=='/') $ drop (length imDir) notesFilename
 
 -- | This is text sent over MIDI to tell play_cache which directory to play
 -- from.  Relative to imDir/cacheDir.