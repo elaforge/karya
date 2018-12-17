elaforge/karya

commit 1b37fd434e1c9b2cfce8943e5feb28c07813cec7

move the Triggered flag to Instrument.Common

Instrument/Common.hs
@@ -23,6 +23,7 @@ import qualified Derive.ScoreTypes as ScoreTypes
 import qualified Derive.ShowVal as ShowVal
 
 import qualified Instrument.Tag as Tag
+
 import Global
 
 
@@ -42,6 +43,7 @@ data Common code = Common {
     , common_tags :: ![Tag.Tag]
     -- | So, instrument, tell me about yourself.
     , common_doc :: !Doc.Doc
+    , common_flags :: !(Set Flag)
     } deriving (Show)
 
 code = Lens.lens common_code (\f r -> r { common_code = f (common_code r) })
@@ -49,6 +51,7 @@ environ = Lens.lens common_environ
     (\f r -> r { common_environ = f (common_environ r) })
 tags = Lens.lens common_tags (\f r -> r { common_tags = f (common_tags r) })
 doc = Lens.lens common_doc (\f r -> r { common_doc = f (common_doc r) })
+flags = Lens.lens common_flags (\f r -> r { common_flags = f (common_flags r) })
 
 common :: code -> Common code
 common code = Common
@@ -56,15 +59,29 @@ common code = Common
     , common_environ = mempty
     , common_tags = []
     , common_doc = ""
+    , common_flags = mempty
     }
 
 instance Pretty code => Pretty (Common code) where
-    format (Common code env tags doc) = Pretty.record "Instrument"
+    format (Common code env tags doc flags) = Pretty.record "Instrument"
         [ ("code", Pretty.format code)
         , ("restricted_environ", Pretty.format env)
         , ("tags", Pretty.format tags)
         , ("doc", Pretty.format doc)
+        , ("flags", Pretty.format flags)
         ]
+
+data Flag =
+    -- | Patch doesn't pay attention to duration, e.g. percussion.  The UI can
+    -- use this to create zero duration events for this instrument.
+    Triggered
+    deriving (Eq, Ord, Show, Enum, Bounded)
+
+instance Pretty Flag where pretty = showt
+
+instance Serialize.Serialize Flag where
+    put = Serialize.put_enum
+    get = Serialize.get_enum
 
 -- * AttributeMap
 

Perform/Midi/Patch.hs
@@ -377,17 +377,12 @@ nn_at scale key
 -- | Various instrument flags.  Add new ones at the bottom to avoid messing up
 -- serialization.
 data Flag =
-    -- | Patch doesn't pay attention to duration.  E.g., drum samples may not
-    -- pay attention to note off.  The UI can use this to create zero duration
-    -- events for this patch.  TODO this is actually not MIDI-specific, so it
-    -- should go in Instrument.Common.
-    Triggered
     -- | Patch uses continuous pressure control, assigned to CC 2 (breath),
     -- instead of trigger velocity.  This is used to support the @dyn@ control.
     -- Percussive instruments like pianos map it to MIDI velocity, and
     -- continuous instruments like winds always have maximum velocity and map
     -- @dyn@ to breath.
-    | Pressure
+    Pressure
     -- | If set, a keysitch has to be held while its note is playing.
     -- Otherwise, it will just be tapped before the note starts.
     | HoldKeyswitch

Instrument/Serialize.hs
@@ -45,8 +45,8 @@ instance Serialize InstrumentDb where
     get = InstrumentDb <$> get <*> get
 
 instance Serialize (Common.Common ()) where
-    put (Common.Common a b c d) = put a >> put b >> put c >> put d
-    get = Common.Common <$> get <*> get <*> get <*> get
+    put (Common.Common a b c d e) = put a >> put b >> put c >> put d >> put e
+    get = Common.Common <$> get <*> get <*> get <*> get <*> get
 
 instance Serialize Search.Index where
     put (Search.Index a b) = put a >> put b

Perform/Im/Patch.hs
@@ -4,8 +4,6 @@
 
 -- | Describe an Im 'Patch', from the sequencer's point of view.
 module Perform.Im.Patch where
-import qualified Data.Set as Set
-
 import qualified Util.Pretty as Pretty
 import qualified Derive.Attrs as Attrs
 import qualified Instrument.Common as Common
@@ -19,28 +17,18 @@ data Patch = Patch {
     -- TODO maybe I need a separate one for pitch controls.
     patch_controls :: !(Map Control.Control Text)
     , patch_attribute_map :: !AttributeMap
-    , patch_flags :: !(Set Flag)
     } deriving (Show)
 
 patch :: Patch
 patch = Patch
     { patch_controls = mempty
     , patch_attribute_map = Common.AttributeMap []
-    , patch_flags = mempty
     }
 
-add_flag :: Flag -> Patch -> Patch
-add_flag flag patch =
-    patch { patch_flags = Set.insert flag (patch_flags patch) }
-
-has_flag :: Patch -> Flag -> Bool
-has_flag patch flag = Set.member flag (patch_flags patch)
-
 instance Pretty Patch where
-    format (Patch controls attr_map flags) = Pretty.record "Patch"
+    format (Patch controls attr_map) = Pretty.record "Patch"
         [ ("controls", Pretty.format controls)
         , ("attribute_map", Pretty.format attr_map)
-        , ("flags", Pretty.format flags)
         ]
 
 -- | Since the synth understands Attributes directly, this is just a list of

Cmd/Instrument/CUtil.hs
@@ -252,9 +252,8 @@ drum_cmd thru = insert_call thru . notes_to_calls
 -- ** patch
 
 drum_patch :: [(Drums.Note, Midi.Key)] -> MidiInst.Patch -> MidiInst.Patch
-drum_patch note_keys = MidiInst.patch
-    %= MidiInst.add_flag Patch.Triggered
-        . (Patch.call_map #= make_call_map (map fst note_keys))
+drum_patch note_keys = (MidiInst.triggered .) $
+    MidiInst.patch %= (Patch.call_map #= make_call_map (map fst note_keys))
         . (Patch.attribute_map #= Patch.unpitched_keymap
             [(Drums._attributes note, key) | (note, key) <- note_keys])
 
@@ -314,9 +313,8 @@ make_cc_keymap base_key range root_pitch =
 -- | Annotate a Patch with an 'Patch.AttributeMap' from the given
 -- PitchedNotes.
 pitched_drum_patch :: PitchedNotes -> MidiInst.Patch -> MidiInst.Patch
-pitched_drum_patch notes =
-    MidiInst.patch %= MidiInst.add_flag Patch.Triggered
-        . (Patch.call_map #= make_call_map (map fst notes))
+pitched_drum_patch notes = (MidiInst.triggered .) $
+    MidiInst.patch %= (Patch.call_map #= make_call_map (map fst notes))
         . (Patch.attribute_map #= make_attribute_map notes)
 
 make_call_map :: [Drums.Note] -> Patch.CallMap

Cmd/Instrument/ImInst.hs
@@ -7,6 +7,8 @@
 module Cmd.Instrument.ImInst (
     module Cmd.Instrument.ImInst, module MidiInst
 ) where
+import qualified Data.Set as Set
+
 import qualified Util.Doc as Doc
 import qualified Util.Lens as Lens
 import qualified Cmd.Cmd as Cmd
@@ -14,8 +16,8 @@ import qualified Cmd.EditUtil as EditUtil
 import qualified Cmd.InputNote as InputNote
 import qualified Cmd.Instrument.MidiInst as MidiInst
 import Cmd.Instrument.MidiInst
-       (Code, generator, transformer, both, null_call, note_calls,
-        note_generators, note_transformers, val_calls, postproc, cmd)
+       (both, cmd, generator, note_calls, note_generators, note_transformers,
+        null_call, postproc, transformer, val_calls, Code)
 import qualified Cmd.MidiThru as MidiThru
 
 import qualified Derive.EnvKey as EnvKey
@@ -98,5 +100,8 @@ thru thru_f = MidiInst.thru convert
                     Right plays -> return $ map (Cmd.ImThru . Osc.play) plays
             _ -> return []
 
-add_flag :: Patch.Flag -> Patch.Patch -> Patch.Patch
-add_flag flag = Patch.add_flag flag
+add_flag :: Common.Flag -> Patch -> Patch
+add_flag flag = common#Common.flags %= Set.insert flag
+
+triggered :: Patch -> Patch
+triggered = add_flag Common.Triggered

Cmd/Instrument/MidiInst.hs
@@ -18,7 +18,7 @@ module Cmd.Instrument.MidiInst (
     , make_patch, patch_from_pair, named_patch, default_patch
     -- ** modify
     , code, doc, attribute_map, decay, synth_controls
-    , add_flag, add_flags, pressure
+    , add_flag, add_flags, pressure, add_common_flag, triggered
     -- ** environ
     , environ, default_scale, range, nn_range
     -- * allocations
@@ -278,6 +278,12 @@ add_flags flags = Patch.defaults#Patch.flags %= Set.union (Set.fromList flags)
 pressure :: Patch -> Patch
 pressure = patch %= (Patch.defaults#Patch.decay #= Just 0)
     . add_flag Patch.Pressure
+
+add_common_flag :: Common.Flag -> Patch -> Patch
+add_common_flag flag = common#Common.flags %= Set.insert flag
+
+triggered :: Patch -> Patch
+triggered = add_common_flag Common.Triggered
 
 -- ** environ
 

Cmd/NoteTrack.hs
@@ -16,6 +16,7 @@
 module Cmd.NoteTrack where
 import qualified Data.List as List
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 import qualified Util.Seq as Seq
 import qualified App.Config as Config
@@ -35,8 +36,8 @@ import qualified Derive.Parse as Parse
 import qualified Derive.ParseTitle as ParseTitle
 import qualified Derive.Score as Score
 
-import qualified Perform.Im.Patch as Im.Patch
-import qualified Perform.Midi.Patch as Patch
+import qualified Instrument.Common as Common
+import qualified Instrument.Inst as Inst
 import qualified Ui.Event as Event
 import qualified Ui.Events as Events
 import qualified Ui.Id as Id
@@ -328,12 +329,11 @@ ensure_note_event pos = do
 -- so I can make the duration 0.
 triggered_inst :: Cmd.M m => Maybe Score.Instrument -> m Bool
 triggered_inst Nothing = return False -- don't know, but guess it's not
-triggered_inst (Just inst) =
-    Cmd.lookup_backend inst >>= return . \case
-        Nothing -> False
-        Just (Cmd.Midi _ config) -> Patch.has_flag config Patch.Triggered
-        Just (Cmd.Im patch) -> Im.Patch.has_flag patch Im.Patch.Triggered
-        -- TODO Triggered should be in Instrument.Common
+triggered_inst (Just inst) = Set.member Common.Triggered <$> common_flags inst
+
+common_flags :: Cmd.M m => Score.Instrument -> m (Set Common.Flag)
+common_flags inst = maybe mempty flags <$> Cmd.lookup_instrument inst
+    where flags = Common.common_flags . Inst.inst_common . Cmd.inst_instrument
 
 modify_event_at :: Cmd.M m => EditUtil.Pos -> Bool -> Bool
     -> EditUtil.Modify -> m ()

commit 90312d090fba83e5acb57ca90be55c6a0b7a65d0

fix a bug where flags got deserialized incorrectly
This was caused by 1b37fd434e1c9b2cfce8943e5feb28c07813cec7.  The problem
was that using Serialize.get_enum and put_enum is not safe when the enum
is modified.  Ultimately the problem is that I stopped observing the
convention of putting Ui.State Serialize instances in Cmd.Serialize and
being careful about compatibility.  It would be nice to have a more
automatic way to enforce that.

Cmd/Repl/LInst.hs
@@ -306,7 +306,7 @@ clear_environ = modify_common_config_ $ Common.cenviron #= mempty
 -- ** Midi.Patch.Config
 
 set_addr :: Ui.M m => Text -> [Midi.Channel] -> Instrument -> m ()
-set_addr wdev chans = modify_midi_config $
+set_addr wdev chans = modify_midi_config_ $
     Patch.allocation #= [((dev, chan), Nothing) | chan <- chans]
     where dev = Midi.write_device wdev
 
@@ -325,7 +325,7 @@ set_tuning_scale tuning scale inst = do
 
 set_control_defaults :: Ui.M m => [(Score.Control, Signal.Y)] -> Instrument
     -> m ()
-set_control_defaults controls = modify_midi_config $
+set_control_defaults controls = modify_midi_config_ $
     Patch.control_defaults #= Map.fromList controls
 
 -- ** Midi.Patch.Config settings
@@ -335,7 +335,7 @@ get_scale inst =
     (Patch.settings#Patch.scale #$) . snd <$> Cmd.get_midi_instrument inst
 
 set_scale :: Ui.M m => Patch.Scale -> Instrument -> m ()
-set_scale scale = modify_midi_config $ Patch.settings#Patch.scale #= Just scale
+set_scale scale = modify_midi_config_ $ Patch.settings#Patch.scale #= Just scale
 
 copy_scale :: Cmd.M m => Instrument -> Instrument -> m ()
 copy_scale from to = do
@@ -343,15 +343,15 @@ copy_scale from to = do
     set_scale scale to
 
 add_flag :: Ui.M m => Patch.Flag -> Instrument -> m ()
-add_flag flag = modify_midi_config $
+add_flag flag = modify_midi_config_ $
     Patch.settings#Patch.flags %= Patch.add_flag flag
 
 remove_flag :: Ui.M m => Patch.Flag -> Instrument -> m ()
-remove_flag flag = modify_midi_config $
+remove_flag flag = modify_midi_config_ $
     Patch.settings#Patch.flags %= Patch.remove_flag flag
 
 set_decay :: Ui.M m => Maybe RealTime -> Instrument -> m ()
-set_decay decay = modify_midi_config $ Patch.settings#Patch.decay #= decay
+set_decay decay = modify_midi_config_ $ Patch.settings#Patch.decay #= decay
 
 -- * util
 
@@ -381,9 +381,9 @@ modify_config modify inst_ = do
     Ui.modify_config $ Ui.allocations_map %= Map.insert inst new
     return result
 
-modify_midi_config :: Ui.M m => (Patch.Config -> Patch.Config) -> Instrument
+modify_midi_config_ :: Ui.M m => (Patch.Config -> Patch.Config) -> Instrument
     -> m ()
-modify_midi_config modify =
+modify_midi_config_ modify =
     modify_config $ \common midi -> ((common, modify midi), ())
 
 modify_common_config :: Ui.M m => (Common.Config -> (Common.Config, a))

Cmd/Repl/LTuning.hs
@@ -150,7 +150,7 @@ get_tuning inst scale = do
 realtime :: Cmd.M m => Util.Instrument -> Patch.Scale -> m ()
 realtime inst scale = do
     LInst.set_scale scale inst
-    LInst.modify_midi_config
+    LInst.modify_midi_config_
         (Patch.initialization %= Set.insert Patch.Tuning) inst
     LInst.initialize_realtime_tuning (Util.instrument inst)
 
@@ -158,7 +158,7 @@ realtime inst scale = do
 nrpn :: Cmd.M m => Util.Instrument -> Patch.Scale -> m ()
 nrpn inst scale = do
     LInst.set_scale scale inst
-    LInst.modify_midi_config
+    LInst.modify_midi_config_
         (Patch.initialization %= Set.insert Patch.NrpnTuning) inst
     LInst.initialize_nrpn_tuning (Util.instrument inst)
 

Cmd/Serialize.hs
@@ -161,18 +161,6 @@ instance Serialize UiConfig.Backend where
         2 -> return UiConfig.Dummy
         _ -> bad_tag "UiConfig.Backend" tag
 
-instance Serialize Common.Config where
-    put (Common.Config a b c d) = Serialize.put_version 0
-        >> put a >> put b >> put c >> put d
-    get = Serialize.get_version >>= \v -> case v of
-        0 -> do
-            environ :: RestrictedEnviron.Environ <- get
-            controls :: ScoreTypes.ControlValMap <- get
-            mute :: Bool <- get
-            solo :: Bool <- get
-            return $ Common.Config environ controls mute solo
-        _ -> Serialize.bad_version "Common.Config" v
-
 -- | For backward compatibility.
 newtype MidiConfigs = MidiConfigs (Map Score.Instrument Patch.Config)
     deriving (Show)
@@ -449,14 +437,12 @@ instance Serialize Ruler.Ruler where
 
 instance Serialize Ruler.MeterConfig where
     put (Ruler.MeterConfig a b) = Serialize.put_version 0 >> put a >> put b
-    get = do
-        v <- Serialize.get_version
-        case v of
-            0 -> do
-                name :: Text <- get
-                start_measure :: Int <- get
-                return $ Ruler.MeterConfig name start_measure
-            _ -> Serialize.bad_version "Ruler.MeterConfig" v
+    get = Serialize.get_version >>= \case
+        0 -> do
+            name :: Text <- get
+            start_measure :: Int <- get
+            return $ Ruler.MeterConfig name start_measure
+        v -> Serialize.bad_version "Ruler.MeterConfig" v
 
 instance Serialize Ruler.Marklist where
     put mlist = put (Ruler.marklist_vec mlist)
@@ -479,33 +465,31 @@ instance Serialize Ruler.Mark where
 -- ** Track
 
 instance Serialize Track.Track where
-    put (Track.Track a b c d) = Serialize.put_version 4 >>
-        put a >> put b >> put c >> put d
-    get = do
-        v <- Serialize.get_version
-        case v of
-            4 -> do
-                title :: Text <- get
-                events :: Events.Events <- get
-                color :: Color.Color <- get
-                render :: Track.RenderConfig <- get
-                return $ Track.Track title events color render
-            _ -> Serialize.bad_version "Track.Track" v
+    put (Track.Track a b c d) = Serialize.put_version 4
+        >> put a >> put b >> put c >> put d
+    get = Serialize.get_version >>= \case
+        4 -> do
+            title :: Text <- get
+            events :: Events.Events <- get
+            color :: Color.Color <- get
+            render :: Track.RenderConfig <- get
+            return $ Track.Track title events color render
+        v -> Serialize.bad_version "Track.Track" v
 
 instance Serialize Track.RenderConfig where
     put (Track.RenderConfig a b) = Serialize.put_version 1 >> put a >> put b
-    get = Serialize.get_version >>= \v -> case v of
+    get = Serialize.get_version >>= \case
         1 -> do
             style :: Track.RenderStyle <- get
             color :: Color.Color <- get
             return $ Track.RenderConfig style color
-        _ -> Serialize.bad_version "Track.RenderConfig" v
+        v -> Serialize.bad_version "Track.RenderConfig" v
 
 instance Serialize Track.RenderStyle where
     put Track.NoRender = put_tag 0
     put (Track.Line a) = put_tag 1 >> put a
     put (Track.Filled a) = put_tag 2 >> put a
-    get = get_tag >>= \tag -> case tag of
+    get = get_tag >>= \case
         0 -> return Track.NoRender
         1 -> do
             source :: Maybe Track.RenderSource <- get
@@ -513,7 +497,7 @@ instance Serialize Track.RenderStyle where
         2 -> do
             source :: Maybe Track.RenderSource <- get
             return $ Track.Filled source
-        _ -> bad_tag "Track.RenderStyle" tag
+        tag -> bad_tag "Track.RenderStyle" tag
 
 instance Serialize Track.RenderSource where
     put (Track.Control a) = put_tag 0 >> put a
@@ -523,45 +507,43 @@ instance Serialize Track.RenderSource where
         -- RenderSource isn't versioned so adjust here.
         let c = if a == Score.default_pitch then Nothing else Just a
         put c
-    get = get_tag >>= \tag -> case tag of
+    get = get_tag >>= \case
         0 -> do
             control :: Score.Control <- get
             return $ Track.Control control
         1 -> do
             control :: Maybe Score.PControl <- get
             return $ Track.Pitch (fromMaybe Score.default_pitch control)
-        _ -> bad_tag "Track.RenderSource" tag
+        tag -> bad_tag "Track.RenderSource" tag
 
 -- ** Perform.Midi.Patch
 
 instance Serialize Patch.Config where
     put (Patch.Config a b c d) = Serialize.put_version 9
         >> put a >> put b >> put c >> put d
-    get = do
-        v <- Serialize.get_version
-        case v of
-            7 -> do
-                alloc :: [(Patch.Addr, Maybe Patch.Voices)] <- get
-                scale :: Maybe Patch.Scale <- get
-                control_defaults :: Score.ControlValMap <- get
-                let settings = old_settings { Patch.config_scale = scale }
-                return $ Patch.Config alloc control_defaults mempty settings
-            8 -> do
-                alloc :: [(Patch.Addr, Maybe Patch.Voices)] <- get
-                scale :: Maybe Patch.Scale <- get
-                control_defaults :: Score.ControlValMap <- get
-                initialization :: Set Patch.Initialization <- get
-                let settings = old_settings { Patch.config_scale = scale }
-                return $ Patch.Config alloc control_defaults initialization
-                    settings
-            9 -> do
-                alloc :: [(Patch.Addr, Maybe Patch.Voices)] <- get
-                control_defaults :: Score.ControlValMap <- get
-                initialization :: Set Patch.Initialization <- get
-                settings :: Patch.Settings <- get
-                return $ Patch.Config alloc control_defaults initialization
-                    settings
-            _ -> Serialize.bad_version "Patch.Config" v
+    get = Serialize.get_version >>= \case
+        7 -> do
+            alloc :: [(Patch.Addr, Maybe Patch.Voices)] <- get
+            scale :: Maybe Patch.Scale <- get
+            control_defaults :: Score.ControlValMap <- get
+            let settings = old_settings { Patch.config_scale = scale }
+            return $ Patch.Config alloc control_defaults mempty settings
+        8 -> do
+            alloc :: [(Patch.Addr, Maybe Patch.Voices)] <- get
+            scale :: Maybe Patch.Scale <- get
+            control_defaults :: Score.ControlValMap <- get
+            initialization :: Set Patch.Initialization <- get
+            let settings = old_settings { Patch.config_scale = scale }
+            return $ Patch.Config alloc control_defaults initialization
+                settings
+        9 -> do
+            alloc :: [(Patch.Addr, Maybe Patch.Voices)] <- get
+            control_defaults :: Score.ControlValMap <- get
+            initialization :: Set Patch.Initialization <- get
+            settings :: Patch.Settings <- get
+            return $ Patch.Config alloc control_defaults initialization
+                settings
+        v -> Serialize.bad_version "Patch.Config" v
 
 -- | This tags Settings which will have to be upgraded by merging with patch
 -- defaults.
@@ -578,25 +560,41 @@ instance Serialize Patch.Scale where
 
 instance Serialize Patch.Initialization where
     put a = Serialize.put_version 0 >> Serialize.put_enum a
-    get = do
-        v <- Serialize.get_version
-        case v of
-            0 -> Serialize.get_enum
-            _ -> Serialize.bad_version "Patch.Initialization" v
+    get = Serialize.get_version >>= \case
+        0 -> Serialize.get_enum
+        v -> Serialize.bad_version "Patch.Initialization" v
 
 instance Serialize Patch.Settings where
     put (Patch.Settings a b c d) = Serialize.put_version 0
         >> put a >> put b >> put c >> put d
-    get = do
-        v <- Serialize.get_version
-        case v of
-            0 -> do
-                flags :: Set Patch.Flag <- get
-                scale :: Maybe Patch.Scale <- get
-                decay :: Maybe RealTime <- get
-                pitch_bend_range :: Midi.Control.PbRange <- get
-                return $ Patch.Settings flags scale decay pitch_bend_range
-            _ -> Serialize.bad_version "Patch.Settings" v
+    get = Serialize.get_version >>= \case
+        0 -> do
+            flags :: Set Patch.Flag <- get
+            scale :: Maybe Patch.Scale <- get
+            decay :: Maybe RealTime <- get
+            pitch_bend_range :: Midi.Control.PbRange <- get
+            return $ Patch.Settings flags scale decay pitch_bend_range
+        v -> Serialize.bad_version "Patch.Settings" v
+
+-- ** Instrument.Common
+
+instance Serialize Common.Config where
+    put (Common.Config a b c d) = Serialize.put_version 0
+        >> put a >> put b >> put c >> put d
+    get = Serialize.get_version >>= \case
+        0 -> do
+            environ :: RestrictedEnviron.Environ <- get
+            controls :: ScoreTypes.ControlValMap <- get
+            mute :: Bool <- get
+            solo :: Bool <- get
+            return $ Common.Config environ controls mute solo
+        v -> Serialize.bad_version "Common.Config" v
+
+instance Serialize Common.Flag where
+    put a = Serialize.put_version 0 >> Serialize.put_enum a
+    get = Serialize.get_version >>= \case
+        0 -> Serialize.get_enum
+        v -> Serialize.bad_version "Common.Flag" v
 
 -- ** lilypond
 

Derive/Derive_profile.hs
@@ -12,18 +12,20 @@ import qualified Util.Seq as Seq
 import qualified Util.Test.Testing as Testing
 import qualified Util.Thread as Thread
 
-import qualified Ui.ScoreTime as ScoreTime
-import qualified Ui.TrackTree as TrackTree
-import qualified Ui.Ui as Ui
-import qualified Ui.UiTest as UiTest
-
 import qualified Cmd.Cmd as Cmd
 import qualified Cmd.Create as Create
+import qualified Cmd.Simple as Simple
+
 import qualified Derive.Derive as Derive
 import qualified Derive.DeriveSaved as DeriveSaved
 import qualified Derive.DeriveTest as DeriveTest
 import qualified Derive.ParseSkeleton as ParseSkeleton
 import qualified Derive.Stream as Stream
+
+import qualified Ui.ScoreTime as ScoreTime
+import qualified Ui.TrackTree as TrackTree
+import qualified Ui.Ui as Ui
+import qualified Ui.UiTest as UiTest
 
 import Global
 import Types
@@ -253,7 +255,7 @@ set_allocations = Ui.config#Ui.allocations #= allocs
         [ ("i1", ("s/1", dev [0, 1]))
         , ("i2", ("s/2", dev [2]))
         ]
-        where dev = map ("wdev",)
+        where dev = Simple.Midi . map ("wdev",)
 
 note_track inst = ctrack 1 inst [""]
 make_tempo n = track_take (ceiling (fromIntegral n / 10)) $

Instrument/Common.hs
@@ -28,6 +28,8 @@ import qualified Instrument.Tag as Tag
 import Global
 
 
+-- | Attributes common to all instruments.  Unlike 'Config', these are
+-- part of the instrument itself and not configurable.
 data Common code = Common {
     -- | Cmds and Derive calls.  This is abstract so this can be defined
     -- without incurring a dependency on "Cmd.Cmd", which would wind up being
@@ -44,6 +46,12 @@ data Common code = Common {
     , common_tags :: ![Tag.Tag]
     -- | So, instrument, tell me about yourself.
     , common_doc :: !Doc.Doc
+    -- | Flags shared with all instruments.
+    --
+    -- TODO unlike midi flags, these are hardcoded and can't be changed
+    -- per-instrument.  I should probably do the same thing as Midi.Patch and
+    -- have a Settings which is copied as the default.  But it's a hassle and
+    -- I don't need it right now.
     , common_flags :: !(Set Flag)
     , common_call_map :: !CallMap
     } deriving (Show)
@@ -89,10 +97,6 @@ data Flag =
     deriving (Eq, Ord, Show, Enum, Bounded)
 
 instance Pretty Flag where pretty = showt
-
-instance Serialize.Serialize Flag where
-    put = Serialize.put_enum
-    get = Serialize.get_enum
 
 -- * AttributeMap
 

Perform/Midi/Patch.hs
@@ -381,12 +381,32 @@ data Flag =
     -- NoteOffs until the final one.  This is for synthesizers which turn the
     -- note off on the first one, such as Kontakt.
     | UseFinalNoteOff
+    -- | Obsolete, don't use.
+    | Old_Triggered
     deriving (Eq, Ord, Read, Show, Bounded, Enum)
 
 instance Pretty Flag where pretty = showt
+
+-- TODO this should have had a version.  Add one when I next do an incompatible
+-- update, and remove Old_Triggered.
 instance Serialize.Serialize Flag where
-    put = Serialize.put_enum
-    get = Serialize.get_enum
+    -- The tag is Int rather than Word8, because this originally used
+    -- Serialize.put_enum and get_enum.  Those are dangerous for compatibility
+    -- though, because when I deleted a Flag it silently broke saves.
+    put = \case
+        Pressure -> tag 1
+        HoldKeyswitch -> tag 2
+        ResumePlay -> tag 3
+        UseFinalNoteOff -> tag 4
+        where
+        tag n = Serialize.put (n :: Int)
+    get = Serialize.get >>= \(tag :: Int) -> case tag of
+        0 -> return Old_Triggered
+        1 -> return Pressure
+        2 -> return HoldKeyswitch
+        3 -> return ResumePlay
+        4 -> return UseFinalNoteOff
+        _ -> Serialize.bad_tag "Flag" (fromIntegral tag)
 
 add_flag :: Flag -> Set Flag -> Set Flag
 add_flag = Set.insert

karya.cabal
@@ -43,7 +43,6 @@ library
         cereal,
         colour,
         containers >=0.5,
-        cryptohash-md5,
         data-ordlist,
         deepseq,
         digest,