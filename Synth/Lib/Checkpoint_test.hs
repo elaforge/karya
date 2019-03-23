elaforge/karya

commit c5d3e3eff6d7f330e6bf7e587872f6c5e303d792

sampler: fix a bug where I lost checkpoints if they were past the last note
This happens when the last note has a decay.

Synth/Lib/Checkpoint.hs
@@ -65,7 +65,7 @@ skipCheckpoints outputDir hashes = do
     Directory.createDirectoryIfMissing False (outputDir </> checkpointDir)
     files <- Directory.listDirectory (outputDir </> checkpointDir)
     let (skipped, (remainingHashes, stateFname)) =
-            findLastState (Set.fromList files) hashes
+            findLastState (Set.fromList files) (extendHashes hashes)
     mbState <- if null stateFname
         then return Nothing
         else Just . State
@@ -115,8 +115,7 @@ write outputDir trackIds skippedCount chunkSize hashes getState audio
         result <- AUtil.catchSndfile $ Resource.runResourceT $
             Audio.File.writeCheckpoints
                 chunkSize (getFilename outputDir getState) chunkComplete
-                AUtil.outputFormat (extendHashes hashes)
-                audio
+                AUtil.outputFormat (extendHashes hashes) audio
         return $ case result of
             Left err -> Left err
             Right written -> Right (written, written + skippedCount)

Synth/Sampler/RenderSample.hs
@@ -14,7 +14,6 @@ module Synth.Sampler.RenderSample (
 #endif
 ) where
 import           GHC.TypeLits (KnownNat)
-import qualified Sound.File.Sndfile as Sndfile
 
 import qualified Util.Audio.Audio as Audio
 import qualified Util.Audio.File as File
@@ -109,8 +108,7 @@ envelopeDur start sig = case Signal.last sig of
 -- | Predict how long a sample will be if resampled with the given ratio
 -- signal.
 predictFileDuration :: Signal.Signal -> FilePath -> IO Audio.Frame
-predictFileDuration ratios =
-    fmap (predictDuration ratios . Audio.Frame . Sndfile.frames) . File.getInfo
+predictFileDuration ratios = fmap (predictDuration ratios) . File.duration
 
 type FrameF = Double
 

Util/Audio/File.hs
@@ -8,7 +8,8 @@
 -- formats supported by libsndfile.
 module Util.Audio.File (
     -- * read
-    check, checkA, getInfo, read, readFrom, read44k, readUnknown
+    check, checkA, getInfo, duration
+    , read, readFrom, read44k, readUnknown
     , concat
     -- * write
     , write, writeCheckpoints
@@ -49,6 +50,9 @@ checkA _ = check (Proxy :: Proxy rate) (Proxy :: Proxy channels)
 getInfo :: FilePath -> IO Sndfile.Info
 getInfo fname = Exception.bracket (openRead fname) Sndfile.hClose
     (return . Sndfile.hInfo)
+
+duration :: FilePath -> IO Audio.Frame
+duration = fmap (Audio.Frame . Sndfile.frames) . getInfo
 
 -- | Since the file is opened only when samples are demanded, a sample rate or
 -- channels mismatch will turn into an exception then, not when this is called.

commit 1932f08d74b02b8ce26182f8234c426fef04d86f

sampler: fix infinite loop introduced by c5d3e3eff6d7f330e6bf7e587872f6c5e303d792
Extending hashes to infinity in skipCheckpoints made a test diverge, but
that in turn turned up that not extending hashes in skipCheckpoints was
intentional, to avoid rerendering a final decay.  Figuring out how to
properly skip a decay turns out to be annoyingly subtle, but I think it
works now.

Synth/Lib/Checkpoint.hs
@@ -65,7 +65,7 @@ skipCheckpoints outputDir hashes = do
     Directory.createDirectoryIfMissing False (outputDir </> checkpointDir)
     files <- Directory.listDirectory (outputDir </> checkpointDir)
     let (skipped, (remainingHashes, stateFname)) =
-            findLastState (Set.fromList files) (extendHashes hashes)
+            findLastState (Set.fromList files) hashes
     mbState <- if null stateFname
         then return Nothing
         else Just . State
@@ -86,7 +86,13 @@ findLastState files = go "" initialState
     go prevStateFname state ((chunknum, hash) : hashes)
         | fname `Set.member` files = case Set.lookupGT prefix files of
             Just stateFname | prefix `List.isPrefixOf` stateFname ->
-                first (fname:) $ go stateFname nextState hashes
+                first (fname:) $ go stateFname nextState $
+                    -- I ran out of notes, but there are still chunks.  This
+                    -- indicates that there is a decay after the last note,
+                    -- so keep following chunks with empty note hash.  They
+                    -- were rendered in the first place beceause 'extendHash'
+                    -- does the same thing for 'write'.
+                    if null hashes then [(chunknum+1, mempty)] else hashes
                 where nextState = drop (length prefix) stateFname
             -- I didn't find a corresponding .state file for the .wav.  This
             -- can happen if a previous render was killed while writing them.
@@ -96,7 +102,12 @@ findLastState files = go "" initialState
             _ -> done
         | otherwise = done
         where
-        done = ([], ((chunknum, hash) : hashes, prevStateFname))
+        done
+            -- This means I'm "in the decay", as above, so don't return one
+            -- of my made-up empty note hashes.  This way 'write' will notice
+            -- null hashes, and skip all work.
+            | hash == mempty && null hashes = ([], ([], ""))
+            | otherwise = ([], ((chunknum, hash) : hashes, prevStateFname))
         prefix = FilePath.replaceExtension fname ".state."
         fname = filenameOf2 chunknum hash state
     go _ _ [] = ([], ([], ""))
@@ -216,7 +227,7 @@ zeroPad digits n =
 
 -- * hash
 
--- | Extend the [(index, hash)] list with 0 hashes.
+-- | Extend the [(index, hash)] list with mempty hashes.
 --
 -- 'Audio.File.writeCheckpoints' needs this because it still wants states
 -- while rendering the decay of the last note.  Previously, I just had

TODO
@@ -2271,6 +2271,9 @@ synth: im:
       . Does this mean all my uses of multiplying dyn are inaccurate?  And in
         what way?
     bug:
+      * now there's a loop in
+        'build/test/RunTests-Synth.Faust.Render test_write_increm'
+        . Probably extendHashes.
       * add 5c at 49.5t of im-zheng @b3:
         notes still playing at 2447550f(55.5s):
         [Playing:r67NHd42kGAPZSp7Zhr6JA(1874250f(42.5s)--2443052f(55.39s))]

Synth/Lib/Checkpoint_test.hs
@@ -57,17 +57,27 @@ test_groupOverlapping = do
 test_findLastState = do
     let f files = Checkpoint.findLastState (Set.fromList files)
             . map (second (Note.Hash))
-    let initialState = Checkpoint.encodeState $ Checkpoint.State mempty
+    let mkstate = Checkpoint.encodeState . Checkpoint.State
     let fn chunk hash state =
             Checkpoint.filenameOf2 chunk (Note.Hash hash) state
-    let wav1 = fn 0 "a" initialState
+    let wav0 = fn 0 "a" (mkstate "")
+    let state0 = FilePath.replaceExtension wav0 (".state." <> mkstate "1")
+
     equal (f [] [(0, "a")]) ([], ([(0, Note.Hash "a")], ""))
     -- .wav is present, but not .state, don't skip the .wav.
-    equal (f [wav1] [(0, "a"), (1, "b")])
+    equal (f [wav0] [(0, "a"), (1, "b")])
         ([], ([(0, Note.Hash "a"), (1, Note.Hash "b")], ""))
-    let state1 = FilePath.replaceExtension wav1 ".state.b"
-    equal (f [wav1, state1] [(0, "a"), (1, "b")])
-        ([wav1], ([(1, Note.Hash "b")], state1))
+    equal (f [wav0, state0] [(0, "a"), (1, "b")])
+        ([wav0], ([(1, Note.Hash "b")], state0))
+
+    -- There's no note hash for wav1, which means it's in the decay.  I still
+    -- find all the corresponding wavs, but don't return any continuation
+    -- hashes or the final successor-less state.
+    let wav1 = fn 1 "" (mkstate "1")
+    let state1 = FilePath.replaceExtension wav1 (".state." <> mkstate "2")
+    pprint [wav0, state0, wav1, state1]
+    equal (f [wav0, state0, wav1, state1] [(0, "a")])
+        ([wav0, wav1], ([], ""))
 
 -- filenameOf2 :: Config.ChunkNum -> Note.Hash -> String -> FilePath
 