614aa19f05ffa41219b40e120b6aed9cadd00d81
Evan Laforge <qdunkan@gmail.com>
2019-07-17 00:00:40 -0700

    update default.nix

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 # Examples:
 # nix-shell --attr build-env --arg withIm true
 # nix-store -r $(nix-instantiate --attr libsamplerate-elaforge)
-# nix build -f default.nix libsamplerate
+# nix build -f default.nix --arg withIm true --arg withDocs true build-env
 
 { nixpkgs ? import <nixpkgs> {}
 , withLilypond ? false # This drags in all of texlive!
@@ -13,7 +13,17 @@
 }:
 
 let
-    ghc = nixpkgs.haskell.packages.ghc844;
+    ghc = let p = nixpkgs.haskell.packages;
+        in if p?ghc844 then p.ghc844 else p.ghc842;
+
+    # config = {
+    #     packageOverrides = pkgs: rec {
+    #         haskellPackages = pkgs.haskellPackages.override {
+    #             overrides = newPkgs: oldPkgs: rec {
+    #             };
+    #         };
+    #     };
+    # };
 
     # util
     guard = bool: list: if bool then list else [];
@@ -23,9 +33,9 @@ let
     readLines = fn: lines (builtins.readFile fn);
 in rec {
     basicDeps = with nixpkgs; [
-        hackage2
-        git
         fltk
+        git
+        hackage
         pcre
     ];
 
@@ -39,14 +49,14 @@ in rec {
         nixpkgs.pandoc
     ];
 
-    # libsamplerate with my patches to save and resume. The official one is
-    # nixpkgs.libsamplerate.
-    # I compile without libsndfile and none of the utils, so no deps on e.g.
-    # CoreServices are needed.
     libsamplerate-elaforge = nixpkgs.stdenv.mkDerivation {
+        # libsamplerate with my patches to save and resume. The official one is
+        # nixpkgs.libsamplerate.
+        # I compile without libsndfile and none of the utils, so no deps on
+        # e.g.  CoreServices are needed.
         name = "libsamplerate-elaforge";
         src = builtins.fetchGit {
-            url = "git@github.com:elaforge/libsamplerate.git";
+            url = "https://github.com/elaforge/libsamplerate.git";
             rev = "fb2e4db83202943219e520f3e85013dccf907e15";
             ref = "local";
         };
@@ -72,86 +82,25 @@ in rec {
     # Turn off all tests.
     # The test for Diff is broken and since I'm going to have to compile
     # everything anyway, I don't need tests.
+    # TODO: I should probably override all of the haskellPackages with this,
+    # or else dependencies will still compile with tests and profiling.
+    # How do I do that for the instance of haskellPackages that comes out of
+    # ghc###?  Or are they all from the same place?
     disableTest = drv:
+        # ghc bootlibs show up in pkgs as nulls.
         if drv == null then drv
         else nixpkgs.haskell.lib.overrideCabal drv (drv: {
             doCheck = false;
             doBenchmark = false;
-            enableLibraryProfiling = false;
             enableExecutableProfiling = false;
+            # I might want library profiling.
+            enableLibraryProfiling = false;
+            # I do want haddock, to link docs.
             # doHaddock = false;
+
+            # med-module is marked broken, but it's not.
             broken = false;
         });
-
-    notBroken = pkg: nixpkgs.haskell.lib.overrideCabal pkg
-        (_: { broken = false; });
-    hackage = ghc.ghcWithPackages (pkgs: with pkgs; map disableTest [
-        Diff
-        QuickCheck
-        aeson
-        ansi-terminal
-        array
-        async
-        attoparsec
-        base
-        base64-bytestring
-        binary
-        bytestring
-        c-storable
-        cereal
-        colour
-        concurrent-output
-        containers
-        data-ordlist
-        deepseq
-        digest
-        directory
-        dlist
-        extra
-        fclabels
-        filepath
-        # ghc
-        ghc-events
-        ghc-paths
-        ghc-prim
-        hashable
-        haskeline
-        haskell-src
-        hedgehog
-        hlibgit2
-        # med-module is marked broken, but it's not.
-        (notBroken med-module)
-        megaparsec
-        mersenne-random-pure64
-        mtl
-        network
-        old-locale
-        parser-combinators
-        pcre-heavy
-        pcre-light
-        pretty
-        process
-        random
-        random-shuffle
-        semigroups
-        shake
-        stm
-        terminfo
-        text
-        time
-        transformers
-        unix
-        utf8-string
-        vector
-        wcwidth
-        # writer-cps-mtl 0.1.1.6 gets
-        # writer-cps-transformers, which wants transformers >=0.5.6.0
-        # but ghc 8.4.4 has transformers-0.5.5.0
-        # So I have to use writer-cps-transformers <=0.5.5.0
-        # writer-cps-mtl
-        zlib
-        zmidi-core
-    ]);
 
     wantPkg = pkg:
         # nix gets the "ghc" package confused with the compiler.
@@ -162,7 +111,7 @@ in rec {
         # So I have to use writer-cps-transformers <=0.5.5.0
         && pkg != "writer-cps-mtl";
 
-    hackage2 = ghc.ghcWithPackages (pkgs: map (pkg: disableTest pkgs."${pkg}") (
+    hackage = ghc.ghcWithPackages (pkgs: map (pkg: disableTest pkgs."${pkg}") (
         builtins.filter wantPkg (builtins.concatLists [
             (readLines doc/cabal/basic)
             (guard withIm (readLines doc/cabal/im))
@@ -181,6 +130,22 @@ in rec {
     # Make a nix-shell that can run `mkmk` and `mk`.
     build-env = nixpkgs.stdenv.mkDerivation {
         name = "build-env";
-        buildInputs = deps;
+        builder = "${nixpkgs.bash}/bin/bash";
+        args = [(builtins.toFile "build-env-builder.sh" ''
+            # TODO surely I can use a standard builder script instead.
+            PATH=""
+            for p in $buildInputs; do
+                export PATH=$p/bin''${PATH:+:}$PATH
+            done
+
+            set -eux
+            mkdir $out
+            cd $out
+            for src in $srcs; do
+                ln -s $src $(basename $src)
+            done
+        '')];
+        srcs = deps;
+        buildInputs = [nixpkgs.coreutils];
     };
 }
