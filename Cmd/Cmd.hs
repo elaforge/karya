elaforge/karya

commit 6bb501137bdbd353597924cd3f883d41ee34f939

get rid of InstTypes.dummy and Inst.lookup's special behaviour for it
It didn't seem to be used.  I think this was a leftover from when I didn't
have an explicit Dummy backend.

Cmd/Cmd.hs
@@ -1322,7 +1322,7 @@ resolve_instrument :: InstrumentDb -> UiConfig.Allocation
 resolve_instrument db alloc = do
     let qualified = UiConfig.alloc_qualified alloc
     inst <- justErr ("patch not in db: " <> pretty qualified) $
-        inst_lookup qualified db
+        Inst.lookup qualified db
     backend <- case (Inst.inst_backend inst, UiConfig.alloc_backend alloc) of
         (Inst.Midi patch, UiConfig.Midi config) ->
             return $ Midi patch (Patch.merge_defaults patch config)
@@ -1401,10 +1401,7 @@ lookup_qualified qualified = do
 
 state_lookup_qualified :: State -> InstTypes.Qualified -> Maybe Inst
 state_lookup_qualified state qualified =
-    inst_lookup qualified $ config_instrument_db (state_config state)
-
-inst_lookup :: InstTypes.Qualified -> InstrumentDb -> Maybe Inst
-inst_lookup = Inst.lookup empty_code
+    Inst.lookup qualified $ config_instrument_db (state_config state)
 
 -- ** misc
 

Derive/DeriveTest.hs
@@ -465,7 +465,7 @@ im_allocs allocs =
 
 lookup_qualified :: Cmd.InstrumentDb -> InstTypes.Qualified
     -> Maybe Cmd.Inst
-lookup_qualified db qual = Cmd.inst_lookup qual db
+lookup_qualified db qual = Inst.lookup qual db
 
 with_allocations :: UiConfig.Allocations -> Setup
 with_allocations allocs = with_ui $ Ui.config#Ui.allocations %= (allocs <>)
@@ -550,7 +550,7 @@ default_convert_lookup =
 
 synths_lookup_qualified :: [MidiInst.Synth] -> InstTypes.Qualified
     -> Maybe Cmd.Inst
-synths_lookup_qualified synth = \qualified -> Cmd.inst_lookup qualified db
+synths_lookup_qualified synth = \qualified -> Inst.lookup qualified db
     where db = synths_to_db synth
 
 synths_to_convert_lookup :: UiConfig.Allocations -> [MidiInst.Synth] -> Lookup

Instrument/Inst.hs
@@ -126,10 +126,9 @@ synths (Db db) = Map.toList db
 lookup_synth :: InstTypes.SynthName -> Db code -> Maybe (Synth code)
 lookup_synth synth (Db db) = Map.lookup synth db
 
-lookup :: code -> InstTypes.Qualified -> Db code -> Maybe (Inst code)
-lookup empty_code q@(InstTypes.Qualified synth name) (Db db)
-    | q == InstTypes.dummy = Just $ Inst Dummy (Common.common empty_code)
-    | otherwise = Map.lookup name . synth_insts =<< Map.lookup synth db
+lookup :: InstTypes.Qualified -> Db code -> Maybe (Inst code)
+lookup (InstTypes.Qualified synth name) (Db db) =
+    Map.lookup name . synth_insts =<< Map.lookup synth db
 
 -- | Unchecked synth declaration.  'db' will check it for duplicates and other
 -- problems.  (name, doc, patches)

Instrument/InstTypes.hs
@@ -16,11 +16,6 @@ import Global
 -- of an instrument in a particular score.
 data Qualified = Qualified SynthName Name deriving (Show, Eq, Ord)
 
--- | All Inst.Dummy instruments are the same, so a Qualified name isn't
--- necessary.
-dummy :: Qualified
-dummy = Qualified "" ""
-
 synth :: Qualified -> SynthName
 synth (Qualified synth _) = synth
 

Instrument/Inst_test.hs
@@ -15,7 +15,7 @@ import Util.Test
 test_lookup = do
     let db = fst $ Inst.db [Kontakt.synth]
     let f synth name = Inst.inst_midi
-            =<< Inst.lookup Cmd.empty_code (InstTypes.Qualified synth name) db
+            =<< Inst.lookup (InstTypes.Qualified synth name) db
 
     let kontakt_inst name = Patch.patch Kontakt.pb_range name
     equal (Patch.patch_name <$> f "kontakt" "hang") $ Just "hang"

Perform/Midi/PerformTest.hs
@@ -37,7 +37,7 @@ read_perf_events events = do
 
 lookup_patch :: Cmd.InstrumentDb -> InstTypes.Qualified -> Maybe Types.Patch
 lookup_patch db qualified = do
-    patch <- Inst.inst_midi =<< Cmd.inst_lookup qualified db
+    patch <- Inst.inst_midi =<< Inst.lookup qualified db
     let score_inst = ScoreT.Instrument (InstTypes.show_qualified qualified)
     return $ Types.patch_from_settings score_inst mempty patch
 

commit b0968f829f1e5768e616fbd00f44358c6bd1aea4

change so dummy instruments don't need to be in the db
This was broken by 6bb501137bdbd353597924cd3f883d41ee34f939.  I could
revert that, but I think it's clearer to put the logic in
Cmd.resolve_instrument.

Cmd/Cmd.hs
@@ -1321,8 +1321,12 @@ resolve_instrument :: InstrumentDb -> UiConfig.Allocation
     -> Either Text ResolvedInstrument
 resolve_instrument db alloc = do
     let qualified = UiConfig.alloc_qualified alloc
-    inst <- justErr ("patch not in db: " <> pretty qualified) $
-        Inst.lookup qualified db
+    inst <- case (Inst.lookup qualified db, UiConfig.alloc_backend alloc) of
+        (Just inst, _) -> Right inst
+        -- Dummy instruments don't need a db entry.
+        (Nothing, UiConfig.Dummy) ->
+            Right $ Inst.Inst Inst.Dummy (Common.common empty_code)
+        (Nothing, _) -> Left $ "patch not in db: " <> pretty qualified
     backend <- case (Inst.inst_backend inst, UiConfig.alloc_backend alloc) of
         (Inst.Midi patch, UiConfig.Midi config) ->
             return $ Midi patch (Patch.merge_defaults patch config)