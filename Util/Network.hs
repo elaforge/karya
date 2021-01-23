b21a16828782b2ebcc53cd66403736770307dcd3
Evan Laforge <qdunkan@gmail.com>
2021-01-23 13:12:11 -0800

    im: ignore a connection error when sending thru

--- Util/Network.hs
+++ Util/Network.hs
@@ -4,13 +4,19 @@
 
 -- | A higher level wrapper around "Network.Socket", since "Network" is
 -- deprecated.
-module Util.Network where
+module Util.Network (
+    Addr(..)
+    , listenUnix
+    , withConnection, withConnection_
+    , getHostName
+) where
 import qualified Control.Exception as Exception
 import qualified Foreign
 import qualified Foreign.C as C
 import qualified Network.Socket as Socket
 import qualified Network.Socket.Internal as Socket.Internal
 import qualified System.IO as IO
+import qualified System.IO.Error as IO.Error
 
 
 data Addr = Unix FilePath | IP Socket.PortNumber
@@ -27,6 +33,7 @@ listenUnix fname = do
     Socket.listen socket 1
     return socket
 
+-- | Connect to the Addr and run the action with the handle.
 withConnection :: Addr -> (IO.Handle -> IO a) -> IO a
 withConnection addr action = do
     socket <- case addr of
@@ -42,6 +49,23 @@ withConnection addr action = do
         IP port -> Socket.SockAddrInet port
             (Socket.tupleToHostAddress (127, 0, 0, 1))
         Unix fname -> Socket.SockAddrUnix fname
+
+-- | Like 'withConnection' except ignore a connection failure.  Other
+-- exceptions pass through.
+withConnection_ :: Addr -> (IO.Handle -> IO ()) -> IO ()
+withConnection_ addr action =
+    Exception.handleJust isConnectError (const $ return ()) $
+        withConnection addr action
+
+-- | The network lib turns ECONNREFUSED and ENOENT into isDoesNotExistError.
+-- That's ok, because 'IP' gives ECONNREFUSED while 'Unix' gives ENOENT:
+--
+-- > connect: <socket: 28>: does not exist (Connection refused)
+-- > connect: <socket: 28>: does not exist (No such file or directory)
+isConnectError :: IO.Error.IOError -> Maybe ()
+isConnectError exc
+    | IO.Error.isDoesNotExistError exc = Just ()
+    | otherwise = Nothing
 
 unixSocket :: IO Socket.Socket
 unixSocket = Socket.socket Socket.AF_UNIX Socket.Stream Socket.defaultProtocol
