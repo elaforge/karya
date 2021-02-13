fd14891458b99b3fff6b92d2739aa821246784d3
Evan Laforge <qdunkan@gmail.com>
2021-02-13 10:47:23 -0800

    nix: port hackage.nix back to callHackage and all-cabal-hashes
    Trying to cut out the cabal revisions nonsense by downloading it all
    myself didn't work because the revisions nonsense is actually hardcoded
    into cabal2nix and ghc.mkDerivation.  I could have stripped out the
    editedCabalFile and revision args and it might have worked, but I give
    up.  We do it their way.

--- nix/hackage.nix
+++ nix/hackage.nix
@@ -1,41 +1,55 @@
-# Produce a { name = src; } for each package described in hackage.json,
-# which is produced by make_hackage.py.
-{ nixpkgs }:
+# Read a json file produced by make_hackage.py with package versions.
+# TODO either port it to nix, or run the script as a derivation
+{ nixpkgs, profiling }:
 let
-  # Combine the hackage source and the updated cabal file from the
-  # all-cabal-files repo, which has had metadata revisions applied.
-  mkSrc = { pkg, ver, cabal, cabalHash, src, srcHash }:
-    nixpkgs.stdenv.mkDerivation {
-      name = "${pkg}-${ver}-source";
-      PATH = "${nixpkgs.coreutils}/bin";
-      src = builtins.fetchTarball {
-        url = src;
-        sha256 = srcHash;
-      };
-      cabal = builtins.fetchurl {
-        url = cabal;
-        sha256 = cabalHash;
-      };
-      inherit pkg;
-      builder = "${nixpkgs.bash}/bin/bash";
-      args = ["-euo" "pipefail" (builtins.toFile "builder.sh" ''
-        mkdir $out
-        cd $out
-        for f in $src/*; do
-          ln -s "$f"
-        done
-        rm $pkg.cabal
-        cp $cabal $pkg.cabal
-        # Some packages (language-c at least) checked 'dist' into source
-        # control, and then I get an unwritable 'dist' in the build dir.
-        rm -f dist
-      '')];
+  # Fix broken stuff in hackage that they just never fix.
+  callHackageArgs = ghc: {
+    # Otherwise zlib is circular because nixpkgs doesn't differentiate
+    # haskell and C deps.
+    zlib = { inherit (nixpkgs) zlib; };
+    # Apparently missing this dep.
+    digest = { inherit (nixpkgs) zlib; };
+    hlibgit2 = {
+      mkDerivation = args: ghc.mkDerivation (args // {
+        # New gcc doesn't like -Wno-format without -Wno-format-security.
+        patches = [./hlibgit2.patch];
+      });
     };
+  };
+
+  callHackage = ghc: name: ver:
+    let args = (callHackageArgs ghc)."${name}" or {};
+    in overrideCabal (ghc.callHackage name ver args);
+
+  overrideCabal = drv:
+    with nixpkgs.haskell.lib;
+      (if profiling then (x: x) else disableLibraryProfiling)
+      (dontCheck drv);
+
+  mkDrv = ghc: { pkg, ver }: callHackage ghc pkg ver;
+
   compose = nixpkgs.lib.foldr (f: g: x: f (g x)) (x: x);
-in compose [
-  (builtins.mapAttrs (_: mkSrc))
-  builtins.listToAttrs
-  (map (a: { name = a.pkg; value = a; }))
-  builtins.fromJSON
-  builtins.readFile
-] ./hackage.json
+
+  # This should work better than jailbreak-cabal, but apparently needs
+  # brand-new Cabal (3.0.1.0 lacks it, 3.2.0.0 has it).
+  # jailbreak = drv: nixpkgs.haskell.lib.appendConfigureFlags drv
+  #   ["--allow-older" "--allow-newer"];
+
+in {
+  all-cabal-hashes =
+    let commit = "21fa0f534f906ead4abe15b071564e21e916c9f7";
+    in nixpkgs.fetchurl {
+      url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/"
+        + "${commit}.tar.gz";
+      sha256 = "0niyw8l5qbnpwdqbaynyzsp85p7bv42rha9b8gjgjarg865r8iyy";
+    };
+
+  overrides = ghc:
+    compose [
+      (builtins.mapAttrs (_name: mkDrv ghc))
+      builtins.listToAttrs
+      (map (a: { name = a.pkg; value = a; }))
+      builtins.fromJSON
+      builtins.readFile
+    ] ./hackage.json;
+}
--- nix/make_hackage.py
+++ nix/make_hackage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Take a cabal.config freeze file on stdin, and produce a JSON file with the
-    appropriate hashes on stdout.
+"""Read a cabal.config freeze file and produce a JSON file, for hackage.nix.
 """
 
 import sys
@@ -9,10 +8,15 @@ import subprocess
 import json
 
 
+INPUT = "doc/cabal/all-deps.cabal.config"
+OUTPUT = "nix/hackage.json"
+
+
 def main():
+    # Omit the bootlibs, since their versions are hardcoded by the compiler.
     bootlibs = get_bootlibs()
     pkgs = []
-    for line in sys.stdin:
+    for line in open(INPUT):
         if line.startswith('constraints: '):
             line = line[len('constraints: '):]
         m = re.match(r'\s*(\S+) ==([0-9.]+),?$', line)
@@ -21,31 +25,15 @@ def main():
         pkg, version = m.groups()
         if pkg in bootlibs:
             continue
-        src = (
-            'http://hackage.haskell.org/package'
-            + f'/{pkg}-{version}/{pkg}-{version}.tar.gz'
-        )
-        cabal = (
-            'https://raw.githubusercontent.com/commercialhaskell'
-            + f'/all-cabal-files/hackage/{pkg}/{version}/{pkg}.cabal'
-        )
-        src_hash = get_hash(src, unpack=True)
-        cabal_hash = get_hash(cabal, unpack=False)
-        pkgs.append({
-            'pkg': pkg, 'ver': version,
-            'src': src, 'srcHash': src_hash,
-            'cabal': cabal, 'cabalHash': cabal_hash,
-        })
+        pkgs.append({'pkg': pkg, 'ver': version})
     pkgs.sort(key=lambda p: p['pkg'])
-    print(json.dumps(pkgs, indent=2, sort_keys=True))
-
-
-def get_hash(url, unpack):
-    cmd = ['nix-prefetch-url', url] + (['--unpack'] if unpack else [])
-    return subprocess.check_output(cmd).decode('utf8').strip()
+    with open(OUTPUT, 'w') as fp:
+        fp.write(json.dumps(pkgs, indent=2, sort_keys=True))
 
 
 def get_bootlibs():
+    # This works for me because I don't install into the global namespace,
+    # but isn't reliable in general.
     return set(subprocess.check_output(
         ['ghc-pkg', 'list', '--global', '--simple-output', '--names-only']
     ).decode('utf8').split())
