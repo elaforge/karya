d00911aacd0e2c5fcdf5ebbba6cd27d15633aeed
Evan Laforge <qdunkan@gmail.com>
2021-02-20 14:17:31 -0800

    fix Util.Processes_test to not depend on scheduler timing

--- Util/Processes_test.hs
+++ Util/Processes_test.hs
@@ -5,20 +5,21 @@
 module Util.Processes_test where
 import qualified Control.Concurrent as Concurrent
 import qualified Control.Concurrent.Chan as Chan
-import Control.Monad (replicateM)
+import qualified Control.Concurrent.MVar as MVar
+import           Control.Monad (replicateM)
 
-import qualified Data.IORef as IORef
 import qualified Data.List as List
 import qualified System.Process as Process
 
 import qualified Util.Processes as Processes
 import qualified Util.Thread as Thread
 
-import Util.Test
+import           Util.Test
 
 
 -- It's kind of annoying to test this automatically, so just make sure it looks
 -- right when run by hand.
+manual_test_supervised :: IO ()
 manual_test_supervised = do
     tid <- Concurrent.forkIO $
         Processes.supervised (Process.shell "sleep 1; echo sub done")
@@ -26,9 +27,11 @@ manual_test_supervised = do
     Thread.delay 0.1
     Concurrent.killThread tid
 
+manual_test_supervised_no_binary :: IO ()
 manual_test_supervised_no_binary =
     Processes.supervised (Process.proc "aoeu" [])
 
+test_conversation_stdout :: Test
 test_conversation_stdout = do
     input <- Chan.newChan
     Processes.conversation "cat" ["-u"] Nothing input $ \output -> do
@@ -37,6 +40,7 @@ test_conversation_stdout = do
         Chan.writeChan input Processes.EOF
         io_equal (Chan.readChan output) (Processes.Exit (Processes.ExitCode 0))
 
+test_conversation_stderr :: Test
 test_conversation_stderr = do
     input <- noInput
     Processes.conversation "cat" ["no-such-file"] Nothing input $ \output -> do
@@ -44,6 +48,7 @@ test_conversation_stderr = do
             (Processes.Stderr "cat: no-such-file: No such file or directory")
         io_equal (Chan.readChan output) (Processes.Exit (Processes.ExitCode 1))
 
+test_conversation_not_found :: Test
 test_conversation_not_found = do
     input <- noInput
     Processes.conversation "no-such-bin" [] Nothing input $ \output ->
@@ -54,16 +59,16 @@ noisySleep :: Int -> (FilePath, [String])
 noisySleep seconds =
     ("sh", ["-c", "sleep " <> show seconds <> "; echo done " <> show seconds])
 
+test_multiple_supervised :: Test
 test_multiple_supervised = do
     -- If the thread that started the processes is killed, it kills them in
     -- turn.
-    outputRef <- IORef.newIORef Nothing
+    outputVar <- MVar.newEmptyMVar
     tid <- Concurrent.forkIO $
         Processes.multipleOutput [noisySleep 1, noisySleep 2] $ \output -> do
-            IORef.writeIORef outputRef (Just output)
+            MVar.putMVar outputVar output
             Thread.delay 4
-    Thread.delay 0.1
-    Just output <- IORef.readIORef outputRef
+    output <- MVar.takeMVar outputVar
     Concurrent.killThread tid
     let sigterm = Processes.Exit (Processes.ExitCode (-15))
     io_equal (snd <$> Chan.readChan output) sigterm
@@ -75,6 +80,7 @@ noInput = do
     Chan.writeChan input Processes.EOF
     return input
 
+test_multiple :: Test
 test_multiple = do
     Processes.multipleOutput [("cat", ["nofile1"]), ("cat", ["nofile2"])] $
         \output -> do
