0d682093a8cf00ad7cd8a674b21efe27eb03e401
Evan Laforge <qdunkan@gmail.com>
2021-05-15 17:12:17 -0700

    update prof_diff.py to compare multiple summaries

--- tools/prof_diff.py
+++ tools/prof_diff.py
@@ -16,26 +16,42 @@ def main():
     parser.add_argument('old')
     parser.add_argument('new')
     args = parser.parse_args()
-    old_gc, old_index = parse(args.old)
-    new_gc, new_index = parse(args.new)
+    old_fname = infer_filename(args.old)
+    new_fname = infer_filename(args.new)
+    if os.path.isdir(old_fname) and os.path.isdir(new_fname):
+        for old, new in pairs(os.listdir(old_fname), os.listdir(new_fname)):
+            if old:
+                old = f'{old_fname}/{old}/hc.summary'
+            if new:
+                new = f'{new_fname}/{new}/hc.summary'
+            print(f'=== prof_diff.py {old} {new} ===')
+            diff(old, new, vimdiff=args.vimdiff)
+            print()
+    else:
+        diff(old_fname, new_fname, vimdiff=args.vimdiff)
+
+
+def diff(old_fname, new_fname, vimdiff):
+    old_gc, old_index = parse(old_fname)
+    new_gc, new_index = parse(new_fname)
 
     old_out = temp('old')
     new_out = temp('new')
     for (old_name, old), (new_name, new) in zip(old_gc, new_gc):
-        assert old_name == new_name, f'{old_name} != {new_name}'
-        if float(new) > float(old):
-            change = '+'
-        elif new == old:
-            change = '='
-        else:
-            change = '-'
+        assert old_name == new_name, f'{old_name=} != {new_name=}'
+        delta = float(new) - float(old)
+        delta = f'{delta:+.2f}'.ljust(7)
         width = len('prof-total-alloc:')
-        prefix = f'{change} {old_name:{width}} '
+        prefix = f'{delta} {old_name:{width}} '
         old_out.write(prefix + old)
         new_out.write(prefix + new)
 
     old_out.write('\n')
     new_out.write('\n')
+    max_width = max(
+        max(map(len, old_index.values())),
+        max(map(len, new_index.values())),
+    ) * 2 + 6
     for key, old in sorted(old_index.items()):
         new = new_index.pop(key, '\n')
         old_out.write(old)
@@ -44,13 +60,8 @@ def main():
         new_out.write(new)
     old_out.close()
     new_out.close()
-    max_width = max(
-        max(map(len, old_index.values())),
-        max(map(len, new_index.values())),
-    ) * 2 + 6
-    print(max_width, old_out.name, new_out.name)
     try:
-        if args.vimdiff:
+        if vimdiff:
             subprocess.call(['vimdiff', old_out.name, new_out.name])
         else:
             subprocess.call([
@@ -60,6 +71,18 @@ def main():
     finally:
         os.remove(old_out.name)
         os.remove(new_out.name)
+
+def infer_filename(arg):
+    if os.path.isfile(arg) or os.path.isdir(arg):
+        return arg
+    elif arg.isdigit():
+        matches = [
+            fn for fn in os.listdir('data/prof/run_profile')
+            if int(fn.split('.', 1)[0]) == int(arg)
+        ]
+        return f'data/prof/run_profile/{matches[0]}'
+    else:
+        raise Exception(f'neither file nor number: {arg}')
 
 def parse(fname):
     d = {}
@@ -80,6 +103,23 @@ def parse(fname):
 def temp(name):
     _, name = tempfile.mkstemp(suffix='.'+name)
     return open(name, 'w')
+
+def pairs(xs, ys, key=lambda x: x):
+    xs = list(reversed(sorted(xs)))
+    ys = list(reversed(sorted(ys)))
+    out = []
+    while xs or ys:
+        if not ys:
+            out.append((xs.pop(), None))
+        elif not xs:
+            out.append((None, ys.pop()))
+        elif key(xs[-1]) == key(ys[-1]):
+            out.append((xs.pop(), ys.pop()))
+        elif key(xs[-1]) < key(ys[-1]):
+            out.append((xs.pop(), None))
+        else:
+            out.append((None, ys.pop()))
+    return out
 
 
 if __name__ == '__main__':
