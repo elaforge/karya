ff6a998b0de70345b80dea406de7376b34cd58ac
Evan Laforge <qdunkan@gmail.com>
2021-07-03 18:22:30 -0700

    fix Midi.StubMidi

--- Midi/StubMidi.hs
+++ Midi/StubMidi.hs
@@ -5,18 +5,13 @@
 -- | A midi implementation module that does nothing.
 module Midi.StubMidi (initialize, interface) where
 import qualified Control.Concurrent.STM as STM
-import qualified Data.Text as Text
 import qualified Data.Time as Time
 
 import qualified Midi.Interface as Interface
-import qualified Midi.Midi as Midi
 import qualified Perform.RealTime as RealTime
 
 
-initialize :: String
-    -> (Midi.Message -> Bool) -- ^ read msgs that return false are filtered
-    -> (Either Text.Text (Interface.RawInterface Midi.WriteMessage) -> IO a)
-    -> IO a
+initialize :: Interface.Initialize a
 initialize _app_name _want_message app = app . Right =<< interface
 
 interface :: IO (Interface.RawInterface a)
