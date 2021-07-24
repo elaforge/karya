abc5b9ec348bcdcf8fa90c99bbdde39cc6e425b9
Evan Laforge <qdunkan@gmail.com>
2021-07-24 14:27:21 -0700

    update my ShakeConfig
    I shouldn't be hardcoding things out of nix, but as long as I am, they
    should be up to date at least.

--- User/Elaforge/ShakeConfig.hs
+++ User/Elaforge/ShakeConfig.hs
@@ -16,7 +16,7 @@ localConfig = defaultConfig
     -- TODO: normally this would come out of NIX_CFLAGS and NIX_LDFLAGS, but I
     -- haven't migrated to consistently using tools/nix-enter yet.
     , rubberband = C.ExternalLibrary
-        { C.libLink = ["/nix/store/jn87aiywvlwnm28jy0kin1sdqzmagafv-rubberband-1.8.2/lib/librubberband.a"]
-        , C.libCompile = ["-I/nix/store/jn87aiywvlwnm28jy0kin1sdqzmagafv-rubberband-1.8.2/include"]
+        { C.libLink = ["/nix/store/hkbzvc64rinnwhi2wa0f26485m5s384f-rubberband-1.8.2/lib/librubberband.a"]
+        , C.libCompile = ["-I/nix/store/hkbzvc64rinnwhi2wa0f26485m5s384f-rubberband-1.8.2/include"]
         }
     }
