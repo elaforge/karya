968bc796fa30fcf5776452b0f2da47bf85cda9a6
Evan Laforge <qdunkan@gmail.com>
2022-08-12 08:58:14 -0700

    update Deriver benchmarks

--- Derive/Deriver/DeriveMBench.hs
+++ Derive/Deriver/DeriveMBench.hs
@@ -1,10 +1,14 @@
+-- Copyright 2022 Evan Laforge
+-- This program is distributed under the terms of the GNU General Public
+-- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
+
 -- | Put benchmark functions in a separate module, just in case cross-module
 -- makes a difference.
 module Derive.Deriver.DeriveMBench where
+import           Control.Monad (when)
+
 import qualified Util.Log as Log
 import qualified Derive.Deriver.DeriveM as DeriveM
-
-import           Global
 
 
 runRefCountdown :: Int -> (Int, Int)
@@ -20,17 +24,25 @@ data Error = Error deriving (Show)
 
 type Deriver = DeriveM.Deriver State Error
 
-runCountdown :: Int -> (Int, Int)
-runCountdown n = case DeriveM.run (State n) countdown of
+run :: Deriver Int -> Int -> (Int, Int)
+run deriver n = case DeriveM.run (State n) deriver of
     (Right val, State n, []) -> (val, n)
+    (Left Error, State 0, [_msg]) -> (0, 0)
     r -> error $ "unexpected result: " <> show r
 
 countdown :: Deriver Int
 countdown = do
+    State n <- DeriveM.get
+    if n <= 0 then pure n else do
+        DeriveM.put $ State (n - 1)
+        countdown
+
+countdownThrow :: Deriver Int
+countdownThrow = do
     State n <- DeriveM.get
     when (n <= 0) $ do
         DeriveM.write $ Log.msg Log.Debug Nothing "hi"
         DeriveM.throw Error
     if n <= 0 then pure n else do
         DeriveM.put $ State (n - 1)
-        countdown
+        countdownThrow
--- Derive/Deriver/DeriveM_criterion.hs
+++ Derive/Deriver/DeriveM_criterion.hs
@@ -1,3 +1,7 @@
+-- Copyright 2022 Evan Laforge
+-- This program is distributed under the terms of the GNU General Public
+-- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
+
 module Derive.Deriver.DeriveM_criterion where
 import qualified Criterion
 import qualified Criterion.Main
@@ -11,17 +15,21 @@ import qualified Derive.Deriver.DeriveMBench as DeriveMBench
 -- Either I'm doing it wrong, or there is none.
 main :: IO ()
 main = Environment.getArgs >>= \case
-    [n] | Just n <- Read.readMaybe n ->
-        print (DeriveMBench.runRefCountdown (10^n))
+    [benchs, ns] -> do
+        let Just n = Read.readMaybe ns
+        let Just bench = lookup benchs benchmarks
+        print (bench (10^n))
     _ -> Criterion.Main.defaultMain
-        [ Criterion.bgroup "countdown" $ do
-            n <- ns
-            return $ Criterion.bench (show n)
-                (Criterion.nf DeriveMBench.runCountdown n)
-        , Criterion.bgroup "reference" $ do
-            n <- ns
-            return $ Criterion.bench (show n)
-                (Criterion.nf DeriveMBench.runRefCountdown n)
+        [ Criterion.bgroup name $ map (mkBench bench) ns
+        | (name, bench) <- benchmarks
         ]
     where
+    mkBench bench n = Criterion.bench (show n) (Criterion.nf bench n)
     ns = [10^2, 10^3, 10^4, 10^5, 10^6]
+
+benchmarks :: [(String, Int -> (Int, Int))]
+benchmarks =
+    [ ("reference", DeriveMBench.runRefCountdown)
+    , ("countdown", DeriveMBench.run DeriveMBench.countdown)
+    , ("throw", DeriveMBench.run DeriveMBench.countdownThrow)
+    ]
