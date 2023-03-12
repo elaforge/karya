elaforge/karya

commit b1187a946f6655d5c65e0ad90470f716159526d8

meter: switch to typed instead of integral Rank

Cmd/Serialize.hs
@@ -503,7 +503,8 @@ instance Serialize Ruler.Ruler where
                 align_to_bottom
             where
             upgrade :: OldMarklists -> Ruler.Marklists
-            upgrade = fmap $ first $ const Nothing
+            upgrade = fmap $ \(_config, OldMarklist mlist) ->
+                (Nothing, Mark.marklist_from_vector mlist)
         -- Upgrade marklists by throwing out the OldMeterConfig.
         -- Alternately, I could try to automatically figure out what the Meter
         -- should be...
@@ -516,7 +517,7 @@ instance Serialize Ruler.Ruler where
             return $ Ruler.Ruler marklists bg show_names align_to_bottom
         v -> Serialize.bad_version "Ruler.Ruler" v
 
-type OldMarklists = Map Text (Maybe OldMeterConfig, Mark.Marklist)
+type OldMarklists = Map Text (Maybe OldMeterConfig, OldMarklist)
 
 instance Serialize OldMeterConfig where
     put (OldMeterConfig a b) = Serialize.put_version 0 >> put a >> put b
@@ -556,11 +557,11 @@ instance Serialize Meter.Config where
         0 -> Meter.Config <$> get <*> get <*> get <*> get <*> get
         v -> Serialize.bad_version "Meter.Config" v
 
-instance Serialize Meter.RankName where
+instance Serialize Meter.Rank where
     put a = Serialize.put_version 0 >> Serialize.put_enum a
     get = Serialize.get_version >>= \case
         0 -> Serialize.get_enum
-        v -> Serialize.bad_version "Meter.RankName" v
+        v -> Serialize.bad_version "Meter.Rank" v
 
 instance Serialize Meter.LabelConfig where
     put a = Serialize.put_version 0 >> case a of
@@ -582,15 +583,23 @@ instance Serialize Meter.AbstractMeter where
         1 -> Meter.D <$> get
         tag -> Serialize.bad_tag "Meter.AbstractMeter" tag
 
-instance Serialize Mark.Marklist where
-    put mlist = put (Mark.marklist_vec mlist)
-    get = do
-        vec :: Mark.MarklistVector <- get
-        return $ Mark.marklist_from_vector vec
+-- The old version is unversioned
+newtype OldMarklist = OldMarklist Mark.MarklistVector
+    deriving (Serialize)
 
+instance Serialize Mark.Marklist where
+    put mlist = Serialize.put_version 0 >> put (Mark.marklist_vec mlist)
+    get = Serialize.get_version >>= \case
+        0 -> do
+            vec :: Mark.MarklistVector <- get
+            return $ Mark.marklist_from_vector vec
+        v -> Serialize.bad_version "Mark.Marklist" v
+
+-- TODO I thought to make a new Mark with typed Mark.Rank, but it's
+-- easier to just fromEnum it.
 instance Serialize Mark.Mark where
-    put (Mark.Mark a b c d e f) = put a >> put b >> put c >> put d >> put e
-        >> put f
+    put (Mark.Mark a b c d e f) =
+        put (fromEnum a) >> put b >> put c >> put d >> put e >> put f
     get = do
         rank :: Int <- get
         width :: Int <- get
@@ -598,7 +607,7 @@ instance Serialize Mark.Mark where
         name :: Text <- get
         name_zoom :: Double <- get
         zoom :: Double <- get
-        return $ Mark.Mark rank width color name name_zoom zoom
+        return $ Mark.Mark (toEnum rank) width color name name_zoom zoom
 
 -- ** Track
 

Cmd/Cmd.hs
@@ -109,6 +109,7 @@ import qualified Ui.Color as Color
 import qualified Ui.Event as Event
 import qualified Ui.Key as Key
 import qualified Ui.KeycapsT as KeycapsT
+import qualified Ui.Meter.Meter as Meter
 import qualified Ui.Sel as Sel
 import qualified Ui.Types as Types
 import qualified Ui.Ui as Ui
@@ -876,8 +877,8 @@ initial_play_state = PlayState
     , state_performance = Map.empty
     , state_current_performance = Map.empty
     , state_performance_threads = Map.empty
-    , state_play_step =
-        TimeStep.time_step $ TimeStep.RelativeMark TimeStep.AllMarklists 0
+    , state_play_step = TimeStep.time_step $
+        TimeStep.RelativeMark TimeStep.AllMarklists Meter.Section
     , state_step = Nothing
     , state_play_multiplier = RealTime.seconds 1
     , state_sync = Nothing
@@ -1005,8 +1006,7 @@ initial_edit_state = EditState {
     , state_advance = True
     , state_chord = False
     , state_record_velocity = False
-    , state_time_step =
-        TimeStep.time_step $ TimeStep.AbsoluteMark TimeStep.AllMarklists 0
+    , state_time_step = state_play_step initial_play_state
     , state_note_duration = TimeStep.event_edge
     , state_note_orientation = Types.Positive
     , state_note_text = ""

Cmd/Edit.hs
@@ -74,7 +74,7 @@ import qualified Perform.Pitch as Pitch
 import qualified Ui.Block as Block
 import qualified Ui.Event as Event
 import qualified Ui.Events as Events
-import qualified Ui.Meter.Mark as Mark
+import qualified Ui.Meter.Meter as Meter
 import qualified Ui.ScoreTime as ScoreTime
 import qualified Ui.Sel as Sel
 import qualified Ui.Types as Types
@@ -123,7 +123,7 @@ cmd_toggle_val_edit_kbd_entry = Cmd.modify_edit_state $ \st ->
 -- | If the TimeStep is AbsoluteMark or RelativeMark, set its rank.  Otherwise,
 -- set it to the deflt.  This means the marklist names are sticky, so if you
 -- set it manually the default bindings won't mess it up.
-set_step_rank :: Cmd.M m => TimeStep.TimeStep -> Mark.Rank -> m ()
+set_step_rank :: Cmd.M m => TimeStep.TimeStep -> Meter.Rank -> m ()
 set_step_rank deflt rank = Cmd.modify_edit_state $ \st ->
     st { Cmd.state_time_step =
         set (TimeStep.to_list (Cmd.state_time_step st)) }

Cmd/GlobalKeymap.hs
@@ -385,14 +385,14 @@ edit_state_bindings = concat
         Edit.cmd_toggle_val_edit_kbd_entry
 
     , command_char '0' "set event step" $ Edit.set_step TimeStep.event_step
-    , uncurry (command_char '1') (step_rank Meter.r_section)
-    , uncurry (command_char '2') (step_rank Meter.r_1)
-    , uncurry (command_char '3') (step_rank Meter.r_2)
-    , uncurry (command_char '4') (step_rank Meter.r_4)
-    , uncurry (command_char '5') (step_rank Meter.r_8)
-    , uncurry (command_char '6') (step_rank Meter.r_16)
-    , uncurry (command_char '7') (step_rank Meter.r_32)
-    , uncurry (command_char '8') (step_rank Meter.r_64)
+    , uncurry (command_char '1') (step_rank Meter.Section)
+    , uncurry (command_char '2') (step_rank Meter.W)
+    , uncurry (command_char '3') (step_rank Meter.H)
+    , uncurry (command_char '4') (step_rank Meter.Q)
+    , uncurry (command_char '5') (step_rank Meter.E)
+    , uncurry (command_char '6') (step_rank Meter.S)
+    , uncurry (command_char '7') (step_rank Meter.T32)
+    , uncurry (command_char '8') (step_rank Meter.T64)
     , shift_char '=' "toggle duration" Edit.toggle_note_duration
 
     , bind_key [PrimaryCommand] (Key.Char '`') "toggle absolute/relative step"

Cmd/Repl/LEvent_test.hs
@@ -5,7 +5,6 @@
 module Cmd.Repl.LEvent_test where
 import qualified Cmd.Repl.LEvent as LEvent
 import qualified Ui.Event as Event
-import qualified Ui.Meter.Meter as Meter
 import qualified Ui.Ui as Ui
 import qualified Ui.UiTest as UiTest
 
@@ -54,8 +53,8 @@ test_quantize_timestep = do
     equal (run LEvent.End marks2 "w" [(0.5, 0.1)]) (Just [(0.5, 0.5)])
     equal (run LEvent.End marks2 "w" [(0.5, 0.5)]) (Just [(0.5, 0.5)])
 
-run_ruler :: [(TrackTime, Meter.Rank)] -> [UiTest.TrackSpec] -> Ui.StateId a
-    -> a
+run_ruler :: [(TrackTime, UiTest.RankNum)] -> [UiTest.TrackSpec]
+    -> Ui.StateId a -> a
 run_ruler marks tracks = UiTest.eval $ UiTest.exec Ui.empty $
     UiTest.mkblock_ruler (UiTest.mkruler_ranks marks) UiTest.default_block_id
         "" tracks

Cmd/Repl/LRuler.hs
@@ -230,7 +230,7 @@ get_marks block_id =
         (Ui.get_ruler =<< Ui.ruler_of block_id)
 
 -- | Ruler under the selection having at least the given rank.
-selected_marks :: Cmd.M m => Mark.Rank -> m [(TrackTime, Mark.Mark)]
+selected_marks :: Cmd.M m => Meter.Rank -> m [(TrackTime, Mark.Mark)]
 selected_marks rank = do
     ruler <- Ui.get_ruler =<< selected
     (start, end) <- selection_range
@@ -258,7 +258,7 @@ infer_measure_dur mlist =
     maybe 0 fst $ Seq.maximum_on snd $ map (second length) $
         Seq.keyed_group_sort id $ zipWith subtract starts (drop 1 starts)
     where
-    starts = map fst . filter ((<= Meter.r_1) . Mark.mark_rank . snd)
+    starts = map fst . filter ((<= Meter.W) . Mark.mark_rank . snd)
         . Mark.to_list $ mlist
 
 upgrade_gong :: Upgrade
@@ -337,7 +337,7 @@ replace_range start end insert = Meter.modify_sections $ \ss ->
 -- TODO This is no longer possible since that's now hardcoded at the Meter.Make
 -- level.  Having to do it at all is no good though, I should not have to pay
 -- for marks which are not visible.
--- strip_ranks :: Cmd.M m => Meter.RankName -> m Modify
+-- strip_ranks :: Cmd.M m => Meter.Rank -> m Modify
 -- strip_ranks max_rank =
 --     modify_selected $ Meter.strip_ranks (Meter.name_to_rank max_rank)
 
@@ -490,7 +490,7 @@ add_cue_at block_id tracknum pos label =
 
 cue_mark :: Text -> Mark.Mark
 cue_mark label = Mark.Mark
-    { mark_rank = 0
+    { mark_rank = minBound
     , mark_width = 2
     , mark_color = Color.black
     , mark_name = label

Cmd/Responder.hs
@@ -146,7 +146,7 @@ setup_state :: [Rect.Rect] -> Cmd.State -> Cmd.State
 setup_state screens state = state
     { Cmd.state_edit = (Cmd.state_edit state)
         { Cmd.state_time_step = TimeStep.time_step $
-            TimeStep.AbsoluteMark TimeStep.AllMarklists Meter.r_4
+            TimeStep.AbsoluteMark TimeStep.AllMarklists Meter.Q
         }
     , Cmd.state_hooks = (Cmd.state_hooks state)
         { Cmd.hooks_selection = Internal.default_selection_hooks }

Cmd/Ruler/Gong.hs
@@ -63,7 +63,7 @@ config = Meter.Config
     , config_strip_depth = 2
     }
 
-labeled_ranks :: Set Meter.RankName
+labeled_ranks :: Set Meter.Rank
 labeled_ranks = Set.fromList [Meter.Section, Meter.H, Meter.S, Meter.T128]
     -- Section: gong, W: gong stroke, H: jegog, Q: calung, E: kotekan*2,
     -- S: kotekan*4, ...

commit 907a8e2d355cb070766f6446114506578b87921c

fix a bug where unserialize for old marklists was broken
Broken in b1187a946f6655d5c65e0ad90470f716159526d8

Cmd/Serialize.hs
@@ -491,14 +491,15 @@ instance Serialize Ruler.Ruler where
         strip (Nothing, mlist) = (Nothing, mlist)
     get = get_version >>= \case
         6 -> do
-            marklists :: Map Ruler.Name (Maybe Text, Mark.Marklist) <- get
+            marklists :: Map Ruler.Name (Maybe Text, OldMarklist) <- get
             bg :: Color.Color <- get
             show_names :: Bool <- get
             align_to_bottom :: Bool <- get
             return $ Ruler.Ruler (upgrade <$> marklists) bg show_names
                 align_to_bottom
             where
-            upgrade (_name, mlist) = (Nothing, mlist)
+            upgrade (_name, OldMarklist mlist) =
+                (Nothing, Mark.marklist_from_vector mlist)
         7 -> do
             marklists :: OldMarklists <- get
             bg :: Color.Color <- get