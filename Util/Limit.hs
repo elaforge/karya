a984bc4d65a09c5c7a4b0a59b3c8fec395911cfb
Evan Laforge <qdunkan@gmail.com>
2025-12-05 11:30:01 -0800

    ghc-9.6: remove Show ResourceLimits

--- Util/Limit.hs
+++ Util/Limit.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE StandaloneDeriving #-}
 -- Copyright 2021 Evan Laforge
 -- This program is distributed under the terms of the GNU General Public
 -- License 3.0, see COPYING or http://www.gnu.org/licenses/gpl-3.0.txt
@@ -7,9 +6,6 @@
 module Util.Limit (set, Resource(..)) where
 import           System.Posix.Resource
 
-
-deriving instance Show ResourceLimit
-deriving instance Show ResourceLimits
 
 set :: Resource -> Integer -> IO ()
 set resource soft = do
