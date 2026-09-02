7fbef0cc9e0758a9769b35c48df054ce58b0f96a
Evan Laforge <qdunkan@gmail.com>
2026-09-01 19:16:25 -0700

    remove ancient allocs from Repl.hs

--- User/Elaforge/Repl.hs
+++ User/Elaforge/Repl.hs
@@ -8,55 +8,7 @@
 module User.Elaforge.Repl where
 import qualified Util.Log as Log
 import qualified Cmd.Cmd as Cmd
-import qualified Cmd.Repl.LInst as LInst
 
 
 test_cmd :: Cmd.CmdL ()
 test_cmd = Log.notice "test command"
-
-
-underwater = do
-    let a = LInst.add
-    a "marim" "pianoteq/" "loop1" [0]
-    a "bass" "pianoteq/" "loop1" [1]
-    a "lead" "pianoteq/" "loop1" [2]
-    a "synb" "pianoteq/" "loop1" [3]
-    a "pan" "pianoteq/" "loop1" [4]
-    a "wood" "pianoteq/" "loop1" [5]
-    a "pizz" "pianoteq/" "loop1" [6]
-
-    -- chords
-    a "maj" "pianoteq/" "loop3" []
-    a "min" "pianoteq/" "loop3" []
-    a "fmaj" "pianoteq/" "loop3" []
-    -- sfx
-    a "rush1" "pianoteq/" "loop3" []
-    a "rush2" "pianoteq/" "loop3" []
-    -- percussion
-    a "shake" "pianoteq/" "loop2" []
-    a "bd" "pianoteq/" "loop2" []
-
-piano = do
-    let a name = LInst.add name "pianoteq/" "loop1" [0]
-    a "pno"
-    a "bass"
-    a "glock"
-    a "pipe"
-    a "string"
-    a "bell"
-
-elektrodes = do
-    let a = LInst.add
-    a "elec" "pianoteq/" "loop1" [0]
-    a "bass" "pianoteq/" "loop1" [1]
-    a "syn" "pianoteq/" "loop1" [2]
-    a "pno" "pianoteq/" "loop1" [3]
-    a "hit" "pianoteq/" "loop1" [4]
-
-    -- percussion
-    a "bd" "pianoteq/" "loop2" []
-    a "sn" "pianoteq/" "loop2" []
-    a "hh-c" "pianoteq/" "loop2" []
-    a "hh-o" "pianoteq/" "loop2" []
-    a "hh-m" "pianoteq/" "loop2" []
-    a "tom" "pianoteq/" "loop2" []
